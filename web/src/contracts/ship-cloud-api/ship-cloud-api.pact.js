787ccf2c3169e0e0765d14ffb5649536a8306afe
GraysonNull <graysonnull@me.com>
2019-05-08 00:28:28 +0000

    pact for image batch watch

--- web/src/contracts/ship-cloud-api/ship-cloud-api.pact.js
+++ web/src/contracts/ship-cloud-api/ship-cloud-api.pact.js
@@ -5,6 +5,7 @@ import createMidstreamWatch from "./contracts/solo-account/create-midstream-watc
 import listWatchesShipCluster from "./contracts/ship-cluster-only/list-watches/list-watches";
 import createInitSession from "./contracts/solo-account/create-init-session/create-init-session";
 import getWatchVersion from "./contracts/solo-account/get-watch-version/get-watch-version";
+import getImageWatchItems from "./contracts/solo-account/list-image-watch-items/list-image-watch-items";
 import getWatchVersionGitOps from "./contracts/gitops-cluster-only/get-watch-version/get-watch-version";
 
 describe("ShipAPI GraphQL Pact", () => {
@@ -16,6 +17,7 @@ describe("ShipAPI GraphQL Pact", () => {
   describe("solo-account:createMidstreamWatch", () => createMidstreamWatch() );
   describe("solo-account:createInitSession", () => createInitSession() );
   describe("solo-account:getWatchVersion", () => getWatchVersion() );
+  describe("solo-account:getImageWatchItems", () => getImageWatchItems() );
 
   describe("ship-cluster-account:listWatches", () => listWatchesShipCluster() );
 
