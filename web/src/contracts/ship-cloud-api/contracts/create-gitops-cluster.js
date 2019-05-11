3024b2adea2e4210e75b359f12c1e51d7fa5f93a
Marc Campbell <marc.e.campbell@gmail.com>
2019-05-11 05:12:32 +0000

    Refactor contracts to be single-file

---
+++ web/src/contracts/ship-cloud-api/contracts/create-gitops-cluster.js
@@ -0,0 +1,66 @@
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import * as _ from "lodash";
+import { getShipClient, createSessionToken } from "../utils";
+import { createGitOpsCluster } from "../../../mutations/ClusterMutations";
+import * as Pact from "@pact-foundation/pact";
+import { Matchers } from "@pact-foundation/pact";
+import { createGitOpsClusterRaw } from "../../../mutations/ClusterMutations";
+
+chai.use(chaiAsPromised);
+
+export default () => {
+  it("creates a gitops cluster for solo dev", async (done) => {
+    await global.provider.addInteraction(createGitOpsClusterInteraction);
+    const result = await getShipClient("solo-account-session-1").mutate({
+      mutation: createGitOpsCluster,
+      variables: {
+        title: "FooBarGit Cluster",
+        installationId: 987654,
+        gitOpsRef: {
+          owner: "me",
+          repo: "myself",
+          branch: "i",
+        },
+      },
+    });
+    // expect(result.data.createGitOpsCluster).to.deep.equal({"id": "generated", "slug": "foobargit-cluster"})
+    // createdClusterId = result.data.createGitOpsCluster.id;
+    global.provider.verify().then(() => done());
+  });
+}
+
+const createGitOpsClusterInteraction = new Pact.GraphQLInteraction()
+  .uponReceiving("a mutation to create a gitops cluster for solo dev")
+  .withRequest({
+    path: "/graphql",
+    method: "POST",
+    headers: {
+      "Authorization": createSessionToken("solo-account-session-1"),
+      "Content-Type": "application/json",
+    }
+  })
+  .withOperation("createGitOpsCluster")
+  .withQuery(createGitOpsClusterRaw)
+  .withVariables({
+    title: "FooBarGit Cluster",
+    installationId: 987654,
+    gitOpsRef: {
+      owner: "me",
+      repo: "myself",
+      branch: "i",
+    },
+  })
+  .willRespondWith({
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: {
+      data: {
+        createGitOpsCluster: {
+          id: Matchers.like("generated"),
+          slug: "foobargit-cluster",
+        },
+      },
+    },
+  });
+
--- web/src/contracts/ship-cloud-api/contracts/update-watch.js
+++ web/src/contracts/ship-cloud-api/contracts/update-watch.js
@@ -1,9 +1,33 @@
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import * as _ from "lodash";
+import { getShipClient, createSessionToken } from "../utils";
+import { updateWatch, updateWatchRaw } from "../../../mutations/WatchMutations";
 import * as Pact from "@pact-foundation/pact";
 import { Matchers } from "@pact-foundation/pact";
-import { createSessionToken } from "../../../utils";
-import { updateWatchRaw } from "../../../../../mutations/WatchMutations";
 
-export const updateWatchInteraction = new Pact.GraphQLInteraction()
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+export default () => {
+  it("updates a watch for single user", async (done) => {
+    await global.provider.addInteraction(updateWatchInteraction);
+    const result = await getShipClient("single-user-account-session-1").mutate({
+      mutation: updateWatch,
+      variables: {
+          watchId: "single-user-watch-update-1",
+          watchName: "Updated Single User Watch Update",
+          iconUri: "http://ccsuppliersource.com/wp-content/uploads/2018/12/bigstock_online_update_11303201.jpg"
+      },
+    });
+    expect(result.data.updateWatch.id).to.equal("single-user-watch-update-1");
+    expect(result.data.updateWatch.watchName).to.equal("Updated Single User Watch Update");
+    expect(result.data.updateWatch.watchIcon).to.equal("http://ccsuppliersource.com/wp-content/uploads/2018/12/bigstock_online_update_11303201.jpg");
+    global.provider.verify().then(() => done());
+  });
+}
+
+const updateWatchInteraction = new Pact.GraphQLInteraction()
   .uponReceiving("a mutation to update a watch for single user")
   .withRequest({
     path: "/graphql",
