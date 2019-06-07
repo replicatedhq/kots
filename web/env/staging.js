5d699cc7fbb08d0c46387729194065b60bb2b3fd
GraysonNull <graysonnull@me.com>
2019-06-07 19:42:10 +0000

    proper staging app url in env

--- web/env/staging.js
+++ web/env/staging.js
@@ -9,7 +9,7 @@ module.exports = {
   SHIPEDUT_ENDPOINT: "https://www.staging.replicated.com/api/v1/edit/",
   GITHUB_CLIENT_ID: "Iv1.b16ae32898661e1d",
   GITHUB_REDIRECT_URI: "https://www.staging.replicated.com/auth/github/callback",
-  GITHUB_INSTALL_URL: "https://github.com/apps/ship-cloud-development-only",
+  GITHUB_INSTALL_URL: "https://github.com/apps/ship-cloud-staging",
   BUGSNAG_API_KEY: "a7504e4a1632ad844b789721e8982b19",
   SHOW_SCM_LEADS: true,
   AVALIABLE_LOGIN_TYPES: ["github"],
