e28358f7f95386de01434255021884731e128677
Ben Junya <ben@replicated.com>
2019-06-10 21:57:19 +0000

    SideBar polish

--- web/src/components/shared/SideBar/SideBar.jsx
+++ web/src/components/shared/SideBar/SideBar.jsx
@@ -5,15 +5,20 @@ import { Link } from "react-router-dom";
 
 import "@src/scss/components/shared/SideBar.scss";
 
-export default function SideBar(props) {
+function SideBar(props) {
   const { className, watches, currentWatch } = props;
 
   return (
     <div className={classNames("sidebar u-minHeight--full", className)}>
       <div className="flex-column u-width--full">
         {watches?.map( (watch, idx) => {
-          const { watchIcon, watchName } = watch;
+          const { watchIcon, watchName, pendingVersions } = watch;
           const [ owner, slug ] = watch.slug.split("/");
+
+          const isBehind = pendingVersions.length && pendingVersions.length > 2
+              ? "2+"
+              : pendingVersions.length;
+
           return (
             <div key={idx} className={classNames("sidebar-link", {
               selected: currentWatch === watchName
@@ -22,11 +27,23 @@ export default function SideBar(props) {
                 className="flex"
                 to={`/watch/${owner}/${slug}`}>
                   <img className="sidebar-link-icon" src={watchIcon} />
-                  <div className="flex-column u-lineHeight--medium">
-                    <p className="u-color--tuna u-fontWeight--bold">{watch.watchName}</p>
-                    <div className="flex">
-                      <img src="http://placekitten.com/10/10" />
-                      <span className="u-marginLeft--10 u-color--dustyGray u-fontSize--small">Up to date</span>
+                  <div className="flex-column">
+                    <p className="u-color--tuna u-fontWeight--bold u-marginBottom--5">{watch.watchName}</p>
+                    <div className="flex alignItems--center">
+                      <div className={classNames("icon", {
+                        "checkmark-icon": !isBehind,
+                        "exclamationMark-icon": isBehind
+                      })}
+                      />
+                    <span className={classNames("u-marginLeft--5 u-fontSize--small u-fontWeight--bold", {
+                      "u-color--dustyGray": !isBehind,
+                      "u-color--orange": isBehind
+                    })}>
+                        { isBehind
+                            ? `${isBehind} versions behind`
+                            : "Up to date"
+                        }
+                      </span>
                     </div>
                   </div>
               </Link>
@@ -44,3 +61,4 @@ SideBar.propTypes = {
   className: PropTypes.string,
   currentWatch: PropTypes.string
 };
+export default SideBar;
