925c20ca27f9c4379d9495e83e9fb2f5fefdcd71
Ben Junya <ben@replicated.com>
2019-06-12 21:54:14 +0000

    Remove accidental duplicate functions during rebase

--- web/src/utilities/utilities.js
+++ web/src/utilities/utilities.js
@@ -57,18 +57,12 @@ export function getApplicationType(watch) {
     console.error(error);
     return "Error fetching applicationType";
   }
- /** 
- * @param {String} - 
- * @return {Object} - 
- */
-export function getAppData(metadata) {
-  return JSON.parse(metadata);
 }
 
 /**
- * 
- * @param {String} - 
- * @return {String} - 
+ *
+ * @param {String} -
+ * @return {String} -
  */
 export function getReadableLicenseType(type) {
   let readableType = "Development";
@@ -86,21 +80,6 @@ export function getReadableLicenseType(type) {
  */
 export function getAppData(metadata) {
   return JSON.parse(metadata);
-}
-
-/**
- *
- * @param {String} -
- * @return {String} -
- */
-export function getReadableLicenseType(type) {
-  let readableType = "Development";
-  if (type === "paid") {
-    readableType = "Production"
-  } else if (type === "trial") {
-    readableType = "Trial"
-  }
-  return readableType;
 }
 
 export const Utilities = {
