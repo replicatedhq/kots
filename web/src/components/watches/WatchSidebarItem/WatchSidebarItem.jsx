2ac82a4a0fb77442eebf2e4a30d8f113578f3001
GraysonNull <graysonnull@me.com>
2019-06-18 23:54:24 +0000

    [ch20243] some updates from design feedback ticket

--- web/src/components/watches/WatchSidebarItem/WatchSidebarItem.jsx
+++ web/src/components/watches/WatchSidebarItem/WatchSidebarItem.jsx
@@ -15,18 +15,18 @@ export default function WatchSidebarItem(props) {
   return (
     <div className={classNames('sidebar-link', className)}>
       <Link
-        className="flex"
+        className="flex alignItems--center"
         to={`/watch/${owner}/${slug}`}>
           <span className="sidebar-link-icon" style={{ backgroundImage: `url(${watchIcon})` }}></span>
           <div className="flex-column">
-            <p className="u-color--tuna u-fontWeight--bold u-marginBottom--5">{watchName}</p>
+            <p className="u-color--tuna u-fontWeight--bold u-marginBottom--10">{watchName}</p>
             <div className="flex alignItems--center">
               <div className={classNames("icon", {
                 "checkmark-icon": !isBehind,
                 "exclamationMark-icon": isBehind
               })}
               />
-              <span className={classNames("u-marginLeft--5 u-fontSize--small u-fontWeight--bold", {
+              <span className={classNames("u-marginLeft--5 u-fontSize--normal u-fontWeight--medium", {
                 "u-color--dustyGray": !isBehind,
                 "u-color--orange": isBehind
               })}>
