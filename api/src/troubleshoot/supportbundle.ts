2925302bfd632136eafb088466af7ca694cb1c42
Marc Campbell <marc.e.campbell@gmail.com>
2019-06-28 22:10:34 +0000

    Insights

--- api/src/troubleshoot/supportbundle.ts
+++ api/src/troubleshoot/supportbundle.ts
@@ -1,4 +1,6 @@
 
+import * as _ from "lodash";
+
 export type SupportBundleStatus = "pending" | "uploaded" | "analyzing" | "analyzed" | "analysis_error";
 
 export class SupportBundle {
@@ -27,30 +29,50 @@ export class SupportBundle {
       createdAt: this.createdAt.toISOString(),
       uploadedAt: this.uploadedAt.toISOString(),
       isArchived: this.isArchived,
+      analysis: this.analysis.toSchema(),
     };
   }
 };
 
-export interface SupportBundleAnalysis {
+export class SupportBundleAnalysis {
   id: string;
   error: string;
   maxSeverity: string;
   insights: SupportBundleInsight[];
-  createdAt: string;
+  createdAt: Date;
+
+  public toSchema() {
+    return {
+      id: this.id,
+      error: this.error,
+      maxSeverity: this.maxSeverity,
+      insights: _.map(this.insights, (insight) => {
+        return insight.toSchema();
+      }),
+      createdAt: this.createdAt.toISOString(),
+    };
+  }
 };
 
-export interface SupportBundleInsight {
+export class SupportBundleInsight {
   key: string;
   severity: string;
   primary: string;
   detail: string;
   icon: string;
-  icon_key: string;
+  iconKey: string;
   desiredPosition: number;
-  labels: Label[];
+
+  public toSchema() {
+    return {
+      key: this.key,
+      severity: this.severity,
+      primary: this.primary,
+      detail: this.detail,
+      icon: this.icon,
+      icon_key: this.iconKey,
+      desiredPosition: this.desiredPosition,
+    };
+  }
 }
 
-export interface Label {
-  key: string;
-  value: string;
-}
