8ff96161421f30d559f1112ec7f6ba7184287c44
Ben Junya <ben@replicated.com>
2019-07-16 22:45:07 +0000

    use build version as sourcemap appVersion

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -7,7 +7,8 @@ import Root from "./Root";
 if (window.env.BUGSNAG_API_KEY && window.env.ENVIRONMENT !== "development") {
   const bugsnagClient = bugsnag({
     apiKey: window.env.BUGSNAG_API_KEY,
-    releaseStage: window.env.ENVIRONMENT
+    releaseStage: window.env.ENVIRONMENT,
+    appVersion: window.env.SHIP_CLUSTER_BUILD_VERSION
   });
   bugsnagClient.use(bugsnagReact, React);
 
