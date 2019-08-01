5dc1fc08b3a14ed6b375d6445a99cb20790fc1b8
Ben Junya <ben@replicated.com>
2019-08-01 18:25:43 +0000

    Stop polling after receiving new results

--- web/src/components/PreflightCheckPage.jsx
+++ web/src/components/PreflightCheckPage.jsx
@@ -24,86 +24,130 @@ class PreflightChecksPage extends Component {
     });
   }
 
-  onTabChange = (/* name */ ) => {
+  onTabChange = (/* name */) => {
+
+  }
+
+  componentDidUpdate (/* lastProps */) {
+    const { listPreflightResultsQuery } = this.props;
+    const { initialResultCount, hasInitialResults, showPreflightResults } = this.state;
+    const hasResults = !listPreflightResultsQuery.loading;
+
+    if (showPreflightResults) {
+      return;
+    }
+
+    if (hasResults) {
+      if (!hasInitialResults) {
+        this.setState({
+          hasInitialResults: true,
+          initialResultCount: listPreflightResultsQuery.listPreflightResults.length
+        });
+      } else if (initialResultCount < listPreflightResultsQuery.listPreflightResults.length) {
+        console.log("trigger update!");
+        listPreflightResultsQuery.stopPolling();
+        this.setState({
+          showPreflightResults: true
+        });
+      }
+    }
 
   }
 
   render() {
-    const { showPreflightInstructions } = this.state;
+    const { showPreflightInstructions, showPreflightResults } = this.state;
+    const { listPreflightResultsQuery } = this.props;
+    if (!listPreflightResultsQuery.loading) {
+      console.log(listPreflightResultsQuery?.listPreflightResults[0]);
+    }
+
     return (
       <div className="flex-column flex1">
-        <div className="flex1 u-overflow--auto">
-          <div className="PreflightChecks--wrapper u-paddingTop--30 u-overflow--hidden">
-            <div className="u-minWidth--full u-minHeight--full">
-              <p className="u-fontSize--header u-color--tuna u-fontWeight--bold">
-                Preflight checks
-            </p>
-              <p className="u-fontWeight--medium u-lineHeight--more u-marginTop--5 u-marginBottom--10">
-                Preflight checks are designed to be run against a target cluster before installing an application. Preflights are simply a different set of collectors + analyzers. These checks are optional but are recommended to ensure that the application you install will work properly.
-            </p>
-              <p className="u-fontSize--jumbo u-color--tuna u-fontWeight--bold u-marginTop--30">
-                Run this command from your workstation
-            </p>
-              <p className="u-marginTop--10 u-marginBottom-10">
-                You will be able to see the results in your terminal window as well as in this UI.
-            </p>
-              <CodeSnippet className="u-marginTop--10" language="bash" canCopy={true}>
-                {`kubectl prefight ${location.href}`}
-            </CodeSnippet>
-              <div className="section-border flex justifyContent--center u-position--relative u-marginTop--20">
-                <p
-                  className="preflight-button flex-auto u-fontSize--small u-color--astral u-fontWeight--medium u-cursor--pointer"
-                  onClick={this.togglePreflightInstructions}
-                >
-                  {showPreflightInstructions
-                    ? "I already have the preflight tool"
-                    : "I need to install the preflight tool"
-                  }
-                </p>
+        {showPreflightResults
+          ? (
+            <div className="flex1 u-overflow--auto">
+              <div className="PreflightChecks--wrapper u-paddingTop--30 u-overflow--hidden">
+                <div className="u-minWidth--full u-minHeight--full">
+                  Render the results in here
+                </div>
               </div>
-              {showPreflightInstructions && (
-                <Fragment>
-                  <p className="u-fontSize--jumbo u-color--tuna u-fontWeight--bold u-marginTop--20">
-                    Install the preflight tool
-                </p>
-                  <CodeSnippet
-                    className="u-marginTop--10"
-                    preText={(
-                      <span className="u-fontSize--small u-fontWeight--bold">
-                        The best way to install the preflight tool is using Kubernetes package manager
-                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/kubernetes-sigs/krew/"> krew</a>.
-                    </span>
-                    )}
-                    language="bash"
-                    canCopy={true}
-                  >
-                    kubectl krew install preflight
-                  </CodeSnippet>
-                  <TabView
-                    className="u-marginTop--10"
-                    onTabChange={this.onTabChange}
-                  >
-                    <Tab name="mac" displayText="MacOS">
-                      <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
-                        {"brew tap replicatedhq/troubleshoot\nbrew install replicatedhq/preflight"}
-                      </CodeSnippet>
-                    </Tab>
-                    <Tab name="win" displayText="Windows">
-                      <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
-                        choco install replicatedhq/preflight
-                      </CodeSnippet>
-                    </Tab>
-                    <Tab name="linux" displayText="Linux">
-                      <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
-                        sudo apt-get install replicatedhq/troubleshoot
-                      </CodeSnippet>
-                    </Tab>
-                  </TabView>
-                </Fragment>
-              )}
             </div>
-          </div>
-        </div>
+          )
+          : (
+              <div className = "flex1 u-overflow--auto">
+                <div className = "PreflightChecks--wrapper u-paddingTop--30 u-overflow--hidden">
+                  <div className = "u-minWidth--full u-minHeight--full">
+                    <p className = "u-fontSize--header u-color--tuna u-fontWeight--bold">
+                      Preflight checks
+                    </p>
+                    <p className="u-fontWeight--medium u-lineHeight--more u-marginTop--5 u-marginBottom--10">
+                      Preflight checks are designed to be run against a target cluster before installing an application. Preflights are simply a different set of collectors + analyzers. These checks are optional but are recommended to ensure that the application you install will work properly.
+                    </p>
+                    <p className="u-fontSize--jumbo u-color--tuna u-fontWeight--bold u-marginTop--30">
+                      Run this command from your workstation
+                    </p>
+                    <p className="u-marginTop--10 u-marginBottom-10">
+                      You will be able to see the results in your terminal window as well as in this UI.
+                    </p>
+                    <CodeSnippet className="u-marginTop--10" language="bash" canCopy={true}>
+                      {`kubectl prefight ${location.href}`}
+                    </CodeSnippet>
+                    <div className="section-border flex justifyContent--center u-position--relative u-marginTop--20">
+                    <p
+                      className="preflight-button flex-auto u-fontSize--small u-color--astral u-fontWeight--medium u-cursor--pointer"
+                      onClick={this.togglePreflightInstructions}
+                    >
+                      {showPreflightInstructions
+                        ? "I already have the preflight tool"
+                        : "I need to install the preflight tool"
+                      }
+                    </p>
+                  </div>
+                  {showPreflightInstructions && (
+                    <Fragment>
+                      <p className="u-fontSize--jumbo u-color--tuna u-fontWeight--bold u-marginTop--20">
+                        Install the preflight tool
+                      </p>
+                      <CodeSnippet
+                        className="u-marginTop--10"
+                        preText={(
+                          <span className="u-fontSize--small u-fontWeight--bold">
+                            The best way to install the preflight tool is using Kubernetes package manager
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/kubernetes-sigs/krew/"> krew</a>.
+                          </span>
+                        )}
+                        language="bash"
+                        canCopy={true}
+                      >
+                        kubectl krew install preflight
+                      </CodeSnippet>
+                      <TabView
+                        className="u-marginTop--10 hidden"
+                        onTabChange={this.onTabChange}
+                      >
+                        <Tab name="mac" displayText="MacOS">
+                          <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
+                            {"brew tap replicatedhq/troubleshoot\nbrew install replicatedhq/preflight"}
+                          </CodeSnippet>
+                        </Tab>
+                        <Tab name="win" displayText="Windows">
+                          <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
+                            choco install replicatedhq/preflight
+                          </CodeSnippet>
+                        </Tab>
+                        <Tab name="linux" displayText="Linux">
+                          <CodeSnippet className="u-marginTop--10" language="bash" canCopy={false}>
+                            sudo apt-get install replicatedhq/troubleshoot
+                          </CodeSnippet>
+                        </Tab>
+                      </TabView>
+                    </Fragment>
+                  )}
+              </div>
+                </div >
+            </div >
+          )
+        }
         <div className="flex-auto flex justifyContent--flexEnd">
           <button type="button" className="btn primary red u-marginRight--30 u-marginBottom--15">Skip this step</button>
         </div>
@@ -117,12 +161,6 @@ export default compose(
   withApollo,
   graphql(listPreflightResults, {
     name: "listPreflightResultsQuery",
-    props: (props, lastProps) => {
-      console.log(props, lastProps);
-      return {
-        ...props,
-      }
-    },
     options: props => {
       const { match } = props;
       return {
