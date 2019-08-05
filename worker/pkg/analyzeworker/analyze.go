209cfcf03cef5dbb8c32a60fd5454846fa5b9342
divolgin <dmitriy@replicated.com>
2019-08-05 16:14:48 +0000

    removing the analyzer for non-troubleshoot support bundles

--- worker/pkg/analyzeworker/analyze.go
+++ worker/pkg/analyzeworker/analyze.go
@@ -4,67 +4,10 @@ import (
 	"context"
 	"os"
 
-	"github.com/replicatedhq/ship-cluster/worker/pkg/analyze"
 	"github.com/replicatedhq/ship-cluster/worker/pkg/troubleshoot"
 	"github.com/replicatedhq/ship-cluster/worker/pkg/types"
 	"go.uber.org/zap"
 )
-
-func (w *Worker) deployAnalyzer(supportBundle *types.SupportBundle) error {
-	namespace := analyze.GetNamespace(context.TODO(), supportBundle)
-	if err := w.ensureNamespace(context.TODO(), namespace); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create namespace", zap.Error(err))
-		return err
-	}
-
-	networkPolicy := analyze.GetNetworkPolicySpec(context.TODO(), supportBundle)
-	if err := w.ensureNetworkPolicy(context.TODO(), networkPolicy); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create networkpolicy", zap.Error(err))
-		return err
-	}
-
-	serviceAccount := analyze.GetServiceAccountSpec(context.TODO(), supportBundle)
-	if err := w.ensureServiceAccount(context.TODO(), serviceAccount); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create serviceaccount", zap.Error(err))
-		return err
-	}
-
-	role := analyze.GetRoleSpec(context.TODO(), supportBundle)
-	if err := w.ensureRole(context.TODO(), role); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create role", zap.Error(err))
-		return err
-	}
-
-	rolebinding := analyze.GetRoleBindingSpec(context.TODO(), supportBundle)
-	if err := w.ensureRoleBinding(context.TODO(), rolebinding); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create rolebinding", zap.Error(err))
-		return err
-	}
-
-	analyzeSpec, err := w.Store.GetAnalyzeSpec(context.TODO(), supportBundle.WatchID)
-	if err != nil {
-		return err
-	}
-
-	configMap := analyze.GetConfigMapSpec(context.TODO(), supportBundle, analyzeSpec)
-	if err := w.ensureConfigMap(context.TODO(), configMap); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create configMap", zap.Error(err))
-		return err
-	}
-
-	getBundlePresignedURI, err := w.Store.GetSupportBundleURL(supportBundle)
-	if err != nil {
-		return err
-	}
-
-	pod := analyze.GetPodSpec(context.TODO(), w.Config.LogLevel, w.Config.AnalyzeImage, w.Config.AnalyzeTag, w.Config.AnalyzePullPolicy, serviceAccount.Name, supportBundle, getBundlePresignedURI, os.Getenv("ANALYZE_NODE_SELECTOR"))
-	if err := w.ensurePod(context.TODO(), pod); err != nil {
-		w.Logger.Errorw("analyzeworker failed to create pod", zap.Error(err))
-		return err
-	}
-
-	return nil
-}
 
 func (w *Worker) deployTroubleshoot(supportBundle *types.SupportBundle) error {
 	namespace := troubleshoot.GetNamespace(context.TODO(), supportBundle)
