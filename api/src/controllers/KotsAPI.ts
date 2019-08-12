b5e59a978b0d7cf94d17c7d6e6ffea5cdd2f9ed0
Marc Campbell <marc.e.campbell@gmail.com>
2019-08-12 23:17:08 +0000

    Up

--- api/src/controllers/KotsAPI.ts
+++ api/src/controllers/KotsAPI.ts
@@ -1,14 +1,16 @@
-import { Controller, Put } from "@tsed/common";
+import { Controller, Put, BodyParams } from "@tsed/common";
 import { MultipartFile } from "@tsed/multipartfiles";
 
 @Controller("/api/v1/kots")
 export class KotsAPI {
   @Put("/")
   async kotsPut(
-    @MultipartFile("kotsapp.tar.gz") file: Express.Multer.File
+    @MultipartFile("file") file: Express.Multer.File,
+    @BodyParams("") body: any,
   ): Promise<any> {
     console.log("Luke i am father")
-    console.log(typeof file)
+    console.log(file);
+    console.log(body);
     return {};
   }
 }
