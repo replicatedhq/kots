5a94d352cd152d29ccf53dd3bf974d34e379d296
Marc Campbell <marc.e.campbell@gmail.com>
2019-08-12 23:21:53 +0000

    Upload

--- api/src/controllers/KotsAPI.ts
+++ api/src/controllers/KotsAPI.ts
@@ -1,16 +1,55 @@
-import { Controller, Put, BodyParams } from "@tsed/common";
+import { Controller, Put, Post, BodyParams } from "@tsed/common";
 import { MultipartFile } from "@tsed/multipartfiles";
+
+interface CreateAppBody {
+  name: string
+}
+
+interface UpdateAppBody {
+  slug: string;
+}
 
 @Controller("/api/v1/kots")
 export class KotsAPI {
-  @Put("/")
-  async kotsPut(
+  @Post("/")
+  async kotsUploadCreate(
     @MultipartFile("file") file: Express.Multer.File,
-    @BodyParams("") body: any,
+    @BodyParams("") body: CreateAppBody,
   ): Promise<any> {
-    console.log("Luke i am father")
-    console.log(file);
-    console.log(body);
-    return {};
+
+    // body.name is the name of the app
+
+    // file.filename is a locally-stored (need to read it) copy of the archive
+
+    // this should create the application in pg
+    // upload the version to s3
+    // create a version in pg
+
+    // return the url for the app
+
+    return {
+      uri: "https://www.google.com",
+    };
+  }
+
+  @Put("/")
+  async kotsUploadUpdate(
+    @MultipartFile("file") file: Express.Multer.File,
+    @BodyParams("") body: UpdateAppBody,
+  ): Promise<any> {
+
+    // body.slug is the slug of the app to update
+
+    // file.filename is a locally-stored (need to read it) copy of the archive
+
+    // this should create the application in pg
+    // upload the version to s3
+    // create a version in pg
+
+    // return the url for the app
+
+    return {
+      uri: "https://www.google.com",
+    };
   }
 }
