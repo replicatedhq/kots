443d44aacfd3c9b447f7188a98c7bfa8ec970142
Ben Junya <ben@replicated.com>
2019-08-14 01:44:51 +0000

    Add middlewares to pkg script

--- api/src/middlewares/ErrorHandler.ts
+++ api/src/middlewares/ErrorHandler.ts
@@ -3,13 +3,12 @@ import { Err, GlobalErrorHandlerMiddleware, OverrideProvider, Req, Res } from "@
 @OverrideProvider(GlobalErrorHandlerMiddleware)
 export class ErrorHandler extends GlobalErrorHandlerMiddleware {
 
-  use(@Err() error: any,
+  use(
+    @Err() error: any,
     @Req() request: Req,
-    @Res() response: Res): any {
-
-    console.log("ERROR!");
-    console.log(error);
-
-    return super.use(error, request, response);
+    @Res() response: Res
+  ): any {
+    console.error(error);
+    super.use(error, request, response);
   }
 }
