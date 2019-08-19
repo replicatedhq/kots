3d06ef7c1a2f296688faeb0cdf385646ad085421
Ben Junya <ben@replicated.com>
2019-08-19 22:35:42 +0000

    Docstrings for bugsnag client tool

--- api/src/server/bugsnagClient.ts
+++ api/src/server/bugsnagClient.ts
@@ -2,6 +2,12 @@ import bugsnag from "@bugsnag/js";
 import * as BugsnagCore from "@bugsnag/browser/dist/types/bugsnag-core";
 let bugsnagClient;
 
+/**
+ * Creates a brand new Bugsnag client and stores it in the global namespace of bugsnagClient.ts.
+ * If no options.apiKey is sent in, this method will return `undefined`.
+ * @param bugsnagOptions {Object} - Options for Bugsnag
+ * @return {undefined|BugsnagCore.Client}
+ */
 export function createBugsnagClient(bugsnagOptions: BugsnagCore.IConfig): BugsnagCore.Client | void {
   if (bugsnagOptions.apiKey) {
     bugsnagClient = bugsnag({
@@ -12,6 +18,10 @@ export function createBugsnagClient(bugsnagOptions: BugsnagCore.IConfig): Bugsna
   return bugsnagClient;
 }
 
+/**
+ * Grabs a previously initialized BugsnagClient. If the client is unavailable, this method returns `undefined`
+ * @return {undefined|BugsnagCore.Client}
+ */
 export function getBugsnagClient(): BugsnagCore.Client | void {
   return bugsnagClient;
 }
