809a16460f6fc8f0cf411877188c9c1a2737d0d1
Ben Junya <ben@replicated.com>
2019-08-19 18:37:37 +0000

    Angry whitespace cause pino

--- api/src/server/logger.ts
+++ api/src/server/logger.ts
@@ -9,7 +9,7 @@ function initLoggerFromEnv(): pino.Logger {
   return pino({
     name: "kotsadm-api",
     timestamp: () => {
-      return `,"time":${(new Date()).toISOString()}`;
+      return `,"time": ${(new Date()).toISOString()}`;
     },
     level: pinoLevel,
     prettyPrint: {
