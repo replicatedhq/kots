02fe20d3b6c0e06cef3463897f350e1b8818eb54
Marc Campbell <marc.e.campbell@gmail.com>
2019-09-01 23:48:01 +0000

    Add an exported C function for update checks

--- ffi/main.go
+++ ffi/main.go
@@ -3,21 +3,112 @@ package main
 import "C"
 
 import (
-	"os"
 	"fmt"
-	"path"
 	"io/ioutil"
+	"os"
+	"path"
 
-	"github.com/replicatedhq/kots/pkg/pull"
 	"github.com/mholt/archiver"
 	kotsv1beta1 "github.com/replicatedhq/kots/kotskinds/apis/kots/v1beta1"
 	kotsscheme "github.com/replicatedhq/kots/kotskinds/client/kotsclientset/scheme"
+	"github.com/replicatedhq/kots/pkg/pull"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
+//export UpdateCheck
+func UpdateCheck(fromArchivePath string) int {
+	tmpRoot, err := ioutil.TempDir("", "kots")
+	if err != nil {
+		fmt.Printf("failed to create temp path: %s\n", err.Error())
+		return -1
+	}
+	defer os.RemoveAll(tmpRoot)
+
+	// extract the current archive to this root
+	tarGz := archiver.TarGz{
+		Tar: &archiver.Tar{
+			ImplicitTopLevelFolder: false,
+		},
+	}
+	if err := tarGz.Unarchive(fromArchivePath, tmpRoot); err != nil {
+		fmt.Printf("failed to unarchive: %s\n", err.Error())
+		return -1
+	}
+
+	kotsCursorFile := path.Join(tmpRoot, "base", ".kotsCursor")
+	_, err = os.Stat(kotsCursorFile)
+	if err != nil {
+		fmt.Printf("failed to find kots cursor file in archive\n")
+		return -1
+	}
+	beforeCursor, err := ioutil.ReadFile(kotsCursorFile)
+	if err != nil {
+		fmt.Printf("failed to read cursor file: %s\n", err.Error())
+		return -1
+	}
+
+	expectedLicenseFile := path.Join(tmpRoot, "upstream", "userdata", "license.yaml")
+	_, err = os.Stat(expectedLicenseFile)
+	if err != nil {
+		fmt.Printf("failed to find license file in archive\n")
+		return -1
+	}
+	licenseData, err := ioutil.ReadFile(expectedLicenseFile)
+	if err != nil {
+		fmt.Printf("failed to read license file: %s\n", err.Error())
+		return -1
+	}
+
+	kotsscheme.AddToScheme(scheme.Scheme)
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode([]byte(licenseData), nil, nil)
+	if err != nil {
+		fmt.Printf("failed to decode license data: %s\n", err.Error())
+		return -1
+	}
+	license := obj.(*kotsv1beta1.License)
+
+	pullOptions := pull.PullOptions{
+		LicenseFile:         expectedLicenseFile,
+		RootDir:             tmpRoot,
+		ExcludeKotsKinds:    true,
+		ExcludeAdminConsole: true,
+	}
+
+	if _, err := pull.Pull(fmt.Sprintf("replicated://%s", license.Spec.AppSlug), pullOptions); err != nil {
+		fmt.Printf("failed to pull upstream: %s\n", err.Error())
+		return 1
+	}
+
+	afterCursor, err := ioutil.ReadFile(kotsCursorFile)
+	if err != nil {
+		fmt.Printf("failed to read cursor file after update: %s\n", err.Error())
+		return -1
+	}
+
+	fmt.Printf("Result of checking for updates for %s: Before: %s, After %s\n", license.Spec.AppSlug, beforeCursor, afterCursor)
+
+	isUpdateAvailable := string(beforeCursor) != string(afterCursor)
+	if !isUpdateAvailable {
+		return 0
+	}
+
+	paths := []string{
+		path.Join(tmpRoot, "upstream"),
+		path.Join(tmpRoot, "base"),
+		path.Join(tmpRoot, "overlays"),
+	}
+
+	if err := tarGz.Archive(paths, fromArchivePath); err != nil {
+		fmt.Printf("failed to write archive: %s", err.Error())
+		return -1
+	}
+
+	return 0
+}
 
 //export PullFromLicense
-func PullFromLicense(licenseData string, downstream string, outputFile string) int { 
+func PullFromLicense(licenseData string, downstream string, outputFile string) int {
 	kotsscheme.AddToScheme(scheme.Scheme)
 	decode := scheme.Codecs.UniversalDeserializer().Decode
 	obj, _, err := decode([]byte(licenseData), nil, nil)
@@ -46,16 +137,16 @@ func PullFromLicense(licenseData string, downstream string, outputFile string) i
 		return 1
 	}
 	defer os.RemoveAll(tmpRoot)
-	
+
 	pullOptions := pull.PullOptions{
-		Downstreams: []string{downstream},
-		LicenseFile: licenseFile.Name(),
-		ExcludeKotsKinds: true,
-		RootDir: tmpRoot,
+		Downstreams:         []string{downstream},
+		LicenseFile:         licenseFile.Name(),
+		ExcludeKotsKinds:    true,
+		RootDir:             tmpRoot,
 		ExcludeAdminConsole: true,
 	}
 
-	if _, err := pull.Pull(fmt.Sprintf("replicated://%", license.Spec.AppSlug), pullOptions); err != nil {
+	if _, err := pull.Pull(fmt.Sprintf("replicated://%s", license.Spec.AppSlug), pullOptions); err != nil {
 		fmt.Printf("failed to pull upstream: %s\n", err.Error())
 		return 1
 	}
