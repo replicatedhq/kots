55f98944d41b8e240c24669877e4886babfa85d4
Ben Junya <ben@replicated.com>
2019-09-16 22:38:27 +0000

    Add error states to airgap upload progress page

--- web/src/components/AirgapUploadProgress.jsx
+++ web/src/components/AirgapUploadProgress.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import classNames from "classnames";
 import { withRouter } from "react-router-dom";
 import { compose, withApollo, graphql} from "react-apollo";
 import { getAirgapInstallStatus } from "../queries/AppsQueries";
@@ -11,13 +12,20 @@ function AirgapUploadProgress(props) {
   if (getAirgapInstallStatus?.installStatus === "installed") {
     history.replace("/");
   }
+  const hasError = getAirgapInstallStatus?.installStatus === "airgap_upload_in_error";
+
+  if (hasError) {
+    getAirgapInstallStatus.stopPolling();
+  }
 
   return (
     <div className="flex1 flex-column alignItems--center justifyContent--center">
       <div className="flex1 flex-column alignItems--center justifyContent--center u-color--tuna">
         <Loader size={60} color="#326DE6"/>
         Checking in on your airgap bundle right now...<br />
-        <div className="u-marginTop--20">
+        <div className={classNames("u-marginTop--20", {
+          "u-color--chestnut": hasError
+        })}>
           {getAirgapInstallStatus?.currentMessage}
         </div>
       </div>
