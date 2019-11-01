d97af8f8131a96bf1429353542683006be0a3c77
Salah Al Saleh <salahalsaleh1993@gmail.com>
2019-10-31 17:21:38 -0700

    Fix config screen scrolling (#645)

--- web/src/components/apps/AppConfig.jsx
+++ web/src/components/apps/AppConfig.jsx
@@ -142,9 +142,9 @@ class AppConfig extends Component {
     }
 
     return (
-      <div className={classNames("flex1 flex-column u-padding--20 justifyContent--flexStart alignItems--center", { "justifyContent--center": fromLicenseFlow })}>
+      <div className={classNames("flex1 flex-column u-padding--20 justifyContent--flexStart alignItems--center u-overflow--auto", { "justifyContent--center": fromLicenseFlow })}>
         {fromLicenseFlow && getKotsApp?.getKotsApp && <span className="u-fontSize--larger u-color--tuna u-fontWeight--bold">Configure {getKotsApp.getKotsApp.name}</span>}
-        <div className={classNames("ConfigOuterWrapper flex u-padding--15 u-overflow--auto", { "u-marginTop--20": fromLicenseFlow })}>
+        <div className={classNames("ConfigOuterWrapper flex u-padding--15", { "u-marginTop--20": fromLicenseFlow })}>
           <div className="ConfigInnerWrapper flex1 u-padding--15">
             <div className="flex1">
               <ShipConfigRenderer groups={configGroups} getData={this.handleConfigChange} />
