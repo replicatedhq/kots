1822b56786f86f985eb4f20b7c6f7d46ec435d09
Marc Campbell <marc.e.campbell@gmail.com>
2019-11-11 15:32:38 +0000

    Remove minio job because this is handled in the kotsadm-api startup now

--- pkg/kotsadm/minio.go
+++ pkg/kotsadm/minio.go
@@ -15,12 +15,6 @@ func getMinioYAML(namespace string) (map[string][]byte, error) {
 	docs := map[string][]byte{}
 	s := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
 
-	var configMap bytes.Buffer
-	if err := s.Encode(minioConfigMap(namespace), &configMap); err != nil {
-		return nil, errors.Wrap(err, "failed to marshal minio config map")
-	}
-	docs["minio-configmap.yaml"] = configMap.Bytes()
-
 	var statefulset bytes.Buffer
 	if err := s.Encode(minioStatefulset(namespace), &statefulset); err != nil {
 		return nil, errors.Wrap(err, "failed to marshal minio statefulset")
@@ -33,12 +27,6 @@ func getMinioYAML(namespace string) (map[string][]byte, error) {
 	}
 	docs["minio-service.yaml"] = service.Bytes()
 
-	var job bytes.Buffer
-	if err := s.Encode(minioJob(namespace), &job); err != nil {
-		return nil, errors.Wrap(err, "failed to marshal minio job")
-	}
-	docs["minio-job.yaml"] = job.Bytes()
-
 	return docs, nil
 }
 
@@ -47,36 +35,12 @@ func ensureMinio(deployOptions DeployOptions, clientset *kubernetes.Clientset) e
 		return errors.Wrap(err, "failed to ensure minio secret")
 	}
 
-	if err := ensureMinioConfigMap(deployOptions.Namespace, clientset); err != nil {
-		return errors.Wrap(err, "failed to ensure minio configmap")
-	}
-
 	if err := ensureMinioStatefulset(deployOptions.Namespace, clientset); err != nil {
 		return errors.Wrap(err, "failed to ensure minio statefulset")
 	}
 
 	if err := ensureMinioService(deployOptions.Namespace, clientset); err != nil {
 		return errors.Wrap(err, "failed to ensure minio service")
-	}
-
-	if err := ensureMinioJob(deployOptions.Namespace, clientset); err != nil {
-		return errors.Wrap(err, "failed to ensure minio job")
-	}
-
-	return nil
-}
-
-func ensureMinioConfigMap(namespace string, clientset *kubernetes.Clientset) error {
-	_, err := clientset.CoreV1().ConfigMaps(namespace).Get("kotsadm-minio", metav1.GetOptions{})
-	if err != nil {
-		if !kuberneteserrors.IsNotFound(err) {
-			return errors.Wrap(err, "failed to get existing config map")
-		}
-
-		_, err := clientset.CoreV1().ConfigMaps(namespace).Create(minioConfigMap(namespace))
-		if err != nil {
-			return errors.Wrap(err, "failed to create configmap")
-		}
 	}
 
 	return nil
@@ -108,22 +72,6 @@ func ensureMinioService(namespace string, clientset *kubernetes.Clientset) error
 		_, err := clientset.CoreV1().Services(namespace).Create(minioService(namespace))
 		if err != nil {
 			return errors.Wrap(err, "failed to create service")
-		}
-	}
-
-	return nil
-}
-
-func ensureMinioJob(namespace string, clientset *kubernetes.Clientset) error {
-	_, err := clientset.BatchV1().Jobs(namespace).Get("kotsadm-minio", metav1.GetOptions{})
-	if err != nil {
-		if !kuberneteserrors.IsNotFound(err) {
-			return errors.Wrap(err, "failed to get existing job")
-		}
-
-		_, err := clientset.BatchV1().Jobs(namespace).Create(minioJob(namespace))
-		if err != nil {
-			return errors.Wrap(err, "failed to create job")
 		}
 	}
 
