052db67611df27375c2b785068c27a934b302230
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2019-11-11 23:50:53 +0000

    Support hidden entitlement fields

--- kotskinds/apis/kots/v1beta1/license_types.go
+++ kotskinds/apis/kots/v1beta1/license_types.go
@@ -94,6 +94,7 @@ type EntitlementField struct {
 	Title       string           `json:"title,omitempty"`
 	Description string           `json:"description,omitempty"`
 	Value       EntitlementValue `json:"value,omitempty"`
+	Hidden      bool             `json:"hidden,omitempty"`
 }
 
 // LicenseSpec defines the desired state of LicenseSpec
