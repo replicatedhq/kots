47e8a0bd618a521520dcc019de455f48ddc85e87
Marc Campbell <marc.e.campbell@gmail.com>
2020-01-15 23:55:48 +0000

    wtih path

--- web/env/enterprise.js
+++ web/env/enterprise.js
@@ -1,7 +1,7 @@
 module.exports = {
   ENVIRONMENT: "production",
   SECURE_ADMIN_CONSOLE: true,
-  API_ENDPOINT: "//",
+  API_ENDPOINT: "//api/v1",
   SHIP_CLUSTER_BUILD_VERSION: (function () {
     return String(Date.now());
   }()),
