2a2e7add9350d8bd5767c0e7e19c78bea6ca741c
Marc Campbell <marc.e.campbell@gmail.com>
2020-01-16 14:56:13 +0000

    Some additional service names

--- pkg/kotsadm/api_objects.go
+++ pkg/kotsadm/api_objects.go
@@ -176,7 +176,7 @@ func apiDeployment(deployOptions types.DeployOptions) *appsv1.Deployment {
 								},
 								{
 									Name:  "SHIP_API_ENDPOINT",
-									Value: fmt.Sprintf("http://kotsadm-api.%s.svc.cluster.local:3000", deployOptions.Namespace),
+									Value: fmt.Sprintf("http://kotsadm.%s.svc.cluster.local:3000", deployOptions.Namespace),
 								},
 								{
 									Name:  "SHIP_API_ADVERTISE_ENDPOINT",
