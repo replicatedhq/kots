5700e69445a4f03c0729cc8f6c6657fcd2a798e8
Salah Al Saleh <salahalsaleh1993@gmail.com>
2020-01-21 16:16:35 -0800

    Add auto create cluster token back (#275)

--- kotskinds/client/kotsclientset/clientset.go
+++ kotskinds/client/kotsclientset/clientset.go
@@ -18,8 +18,6 @@ limitations under the License.
 package kotsclientset
 
 import (
-	"fmt"
-
 	kotsv1beta1 "github.com/replicatedhq/kots/kotskinds/client/kotsclientset/typed/kots/v1beta1"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
@@ -29,6 +27,8 @@ import (
 type Interface interface {
 	Discovery() discovery.DiscoveryInterface
 	KotsV1beta1() kotsv1beta1.KotsV1beta1Interface
+	// Deprecated: please explicitly pick a version if possible.
+	Kots() kotsv1beta1.KotsV1beta1Interface
 }
 
 // Clientset contains the clients for groups. Each group has exactly one
@@ -43,6 +43,12 @@ func (c *Clientset) KotsV1beta1() kotsv1beta1.KotsV1beta1Interface {
 	return c.kotsV1beta1
 }
 
+// Deprecated: Kots retrieves the default version of KotsClient.
+// Please explicitly pick a version.
+func (c *Clientset) Kots() kotsv1beta1.KotsV1beta1Interface {
+	return c.kotsV1beta1
+}
+
 // Discovery retrieves the DiscoveryClient
 func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 	if c == nil {
@@ -52,14 +58,9 @@ func (c *Clientset) Discovery() discovery.DiscoveryInterface {
 }
 
 // NewForConfig creates a new Clientset for the given config.
-// If config's RateLimiter is not set and QPS and Burst are acceptable,
-// NewForConfig will generate a rate-limiter in configShallowCopy.
 func NewForConfig(c *rest.Config) (*Clientset, error) {
 	configShallowCopy := *c
 	if configShallowCopy.RateLimiter == nil && configShallowCopy.QPS > 0 {
-		if configShallowCopy.Burst <= 0 {
-			return nil, fmt.Errorf("Burst is required to be greater than 0 when RateLimiter is not set and QPS is set to greater than 0")
-		}
 		configShallowCopy.RateLimiter = flowcontrol.NewTokenBucketRateLimiter(configShallowCopy.QPS, configShallowCopy.Burst)
 	}
 	var cs Clientset
