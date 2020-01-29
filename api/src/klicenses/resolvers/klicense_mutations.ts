a8663bbed6794fa98b917bbf52ec29701dfcdc30
divolgin <dmitriy@replicated.com>
2020-01-29 23:45:32 +0000

    Apply new license to app yaml when license changes

--- api/src/klicenses/resolvers/klicense_mutations.ts
+++ api/src/klicenses/resolvers/klicense_mutations.ts
@@ -6,9 +6,10 @@ import { KLicense } from "../klicense";
 export function KotsLicenseMutations(stores: Stores) {
   return {
     async syncAppLicense(root: any, args: any, context: Context): Promise<KLicense> {
-      const { appSlug, airgapLicense } = args;
+      const { appSlug, airgapLicense = "" } = args;
       const appId = await stores.kotsAppStore.getIdFromSlug(appSlug);
       const app = await context.getApp(appId)
+
       return await syncLicense(stores, app, airgapLicense);
     },
   }
