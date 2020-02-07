39e2bba127c1702644bcbfd0e27f5e92b112463c
Salah Al Saleh <salahalsaleh1993@gmail.com>
2020-02-07 09:41:06 -0800

    Fix overriding default config values (#318)
    * Fix overriding default config values

--- pkg/upstream/write.go
+++ pkg/upstream/write.go
@@ -32,21 +32,9 @@ func WriteUpstream(u *types.Upstream, options types.WriteOptions) error {
 		u.Files = append(u.Files, adminConsoleFiles...)
 	}
 
-	var previousValuesContent []byte
 	var previousInstallationContent []byte
 	_, err := os.Stat(renderDir)
 	if err == nil {
-		// if there's already a config values yaml, we need to save
-		_, err := os.Stat(path.Join(renderDir, "userdata", "config.yaml"))
-		if err == nil {
-			c, err := ioutil.ReadFile(path.Join(renderDir, "userdata", "config.yaml"))
-			if err != nil {
-				return errors.Wrap(err, "failed to read existing config values")
-			}
-
-			previousValuesContent = c
-		}
-
 		_, err = os.Stat(path.Join(renderDir, "userdata", "installation.yaml"))
 		if err == nil {
 			c, err := ioutil.ReadFile(path.Join(renderDir, "userdata", "installation.yaml"))
@@ -84,29 +72,6 @@ func WriteUpstream(u *types.Upstream, options types.WriteOptions) error {
 
 		if err := ioutil.WriteFile(fileRenderPath, file.Content, 0644); err != nil {
 			return errors.Wrap(err, "failed to write upstream file")
-		}
-	}
-
-	if previousValuesContent != nil {
-		for i, f := range u.Files {
-			if f.Path == path.Join("userdata", "config.yaml") {
-				mergedValues, err := mergeValues(previousValuesContent, f.Content)
-				if err != nil {
-					return errors.Wrap(err, "failed to merge config values")
-				}
-
-				err = ioutil.WriteFile(path.Join(renderDir, "userdata", "config.yaml"), mergedValues, 0644)
-				if err != nil {
-					return errors.Wrap(err, "failed to replace configg values with previous config values")
-				}
-
-				updatedValues := types.UpstreamFile{
-					Path:    f.Path,
-					Content: mergedValues,
-				}
-
-				u.Files[i] = updatedValues
-			}
 		}
 	}
 
@@ -164,38 +129,6 @@ func getEncryptionKey(prevInstallation *kotsv1beta1.Installation) (string, error
 	}
 
 	return prevInstallation.Spec.EncryptionKey, nil
-}
-
-func mergeValues(previousValues []byte, applicationDeliveredValues []byte) ([]byte, error) {
-	decode := scheme.Codecs.UniversalDeserializer().Decode
-
-	prevObj, _, err := decode(previousValues, nil, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to decode previous values")
-	}
-	prevValues := prevObj.(*kotsv1beta1.ConfigValues)
-
-	applicationValuesObj, _, err := decode(applicationDeliveredValues, nil, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to decode application delivered values")
-	}
-	applicationValues := applicationValuesObj.(*kotsv1beta1.ConfigValues)
-
-	for name, value := range applicationValues.Spec.Values {
-		_, ok := prevValues.Spec.Values[name]
-		if !ok {
-			prevValues.Spec.Values[name] = value
-		}
-	}
-
-	s := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
-
-	var b bytes.Buffer
-	if err := s.Encode(prevValues, &b); err != nil {
-		return nil, errors.Wrap(err, "failed to encode merged values")
-	}
-
-	return b.Bytes(), nil
 }
 
 func mustMarshalInstallation(installation *kotsv1beta1.Installation) []byte {
