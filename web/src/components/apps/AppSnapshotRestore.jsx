2827177ed3f4a564499be566f9d2569dd8d3c10d
Jelena <jelena.gruica@gmail.com>
2020-02-11 21:06:55 +0000

    Restore UI wip

--- web/src/components/apps/AppSnapshotRestore.jsx
+++ web/src/components/apps/AppSnapshotRestore.jsx
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import { graphql, compose, withApollo } from "react-apollo";
 import { withRouter } from "react-router-dom";
 import Helmet from "react-helmet";
+import { Line } from "rc-progress";
 import Loader from "../shared/Loader";
 import { restoreDetail } from "../../queries/SnapshotQueries";
 
 class AppSnapshotRestore extends Component {
-  state = {
-  };
-
   componentDidMount() {
     this.props.restoreDetail.startPolling(2000);
   }
@@ -29,16 +27,43 @@ class AppSnapshotRestore extends Component {
         <Helmet>
           <title>{`${this.props.app.name} Snapshots Restore`}</title>
         </Helmet>
-        <div className="flex1 flex-column justifyContent--center alignItems--center">
-          <p className="u-fontWeight--bold u-color--tuna u-fontSize--larger u-lineHeight--normal u-marginBottom--10"> Application restore in progress </p>
-          <p className="u-fontSize--normal u-fontWeight--medium u-color--dustyGray u-lineHeight--normal"> After all volumes have been restored you will need to log back in to the admin console. </p>
+        <p className="u-fontWeight--bold u-color--tuna u-fontSize--larger u-lineHeight--normal u-marginBottom--10"> Application restore in progress </p>
+        <p className="u-fontSize--normal u-fontWeight--medium u-color--dustyGray u-lineHeight--normal"> After all volumes have been restored you will need to log back in to the admin console. </p>
+        <div className="flex flex-column  u-marginTop--40">
+          {restoreDetail?.restoreDetail?.volumes?.length === 0 &&
+            <div className="flex-column flex1 alignItems--center justifyContent--center">
+              <Loader size="60" />
+            </div>
+          }
+          {restoreDetail?.restoreDetail?.volumes?.map((volume, i) => {
+            const strokeColor = volume.completionPercent === 100 ? "#44BB66" : "#326DE6";
+            const minutes = Math.floor(volume.timeRemainingSeconds / 60);
+            const remainingTime = volume.timeRemainingSeconds < 60 ? `${volume.timeRemainingSeconds} seconds remaining` : `${minutes} minutes remaining`;
+            const percentage = volume.completionPercent ? volume.completionPercent : 0;
+
+            return (
+              <div className="flex flex1 u-marginTop--30" key={`${volume.name}-${i}`}>
+                <div className="flex flex1">
+                  <p className="u-fontSize--normal u-color--tuna u-fontWeight--bold u-lineHeight--bold u-marginRight--10">Restoring volume: {volume.name}</p>
+                </div>
+                <div className="flex flex1 flex-column justifyContent--center">
+                  <Line percent={percentage} strokeWidth="3" strokeColor={strokeColor} />
+                  {volume.timeRemainingSeconds !== 0 ?
+                    <div className="flex justifyContent--center u-fontSize--smaller u-fontWeight--medium u-color--silverSand u-marginTop--5"> {volume.timeRemainingSeconds ? remainingTime : null}</div>
+                    :
+                    <div className="flex justifyContent--center u-fontSize--smaller u-fontWeight--medium u-color--silverSand u-marginTop--5"> Complete </div>
+                  }
+                </div>
+                {volume.completionPercent === 100 ?
+                  <span className="icon checkmark-icon u-marginLeft--10"></span>
+                  :
+                  <span className="u-fontSize-small u-fontWeight--medium u-color--silverSand u-marginLeft--10">{volume.completionPercent ? `${volume.completionPercent}%` : null}</span>
+                }
+              </div>
+            );
+          })}
         </div>
         <div>
-          {restoreDetail?.volumes?.map((volume, i) => (
-            <div className="flex flex1" key={`${volume.name}-${i}`}>
-              <p className="flex1 u-fontSize--large u-color--tuna u-fontWeight--bold u-lineHeight--bold">Restoring volume: {volume.name}</p>
-            </div>
-          ))}
         </div>
       </div>
     );
