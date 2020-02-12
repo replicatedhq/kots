054adff424981c71c301c27eaa7f6ba555b1c7b5
divolgin <dmitriy@replicated.com>
2020-02-12 21:18:04 +0000

    Don't allow upgrade command on kurl clusters

--- pkg/kotsadm/types/upgradeoptions.go
+++ pkg/kotsadm/types/upgradeoptions.go
@@ -5,4 +5,5 @@ import "k8s.io/cli-runtime/pkg/genericclioptions"
 type UpgradeOptions struct {
 	Namespace             string
 	KubernetesConfigFlags *genericclioptions.ConfigFlags
+	ForceUpgradeKurl      bool
 }
