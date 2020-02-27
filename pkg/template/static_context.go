a291f5ee46b7c678f91811b75524e2011d2c2e96
Michael <michael@replicated.com>
2020-02-27 00:25:47 +0000

    TEST template function

--- pkg/template/static_context.go
+++ pkg/template/static_context.go
@@ -70,6 +70,10 @@ func (ctx StaticCtx) FuncMap() template.FuncMap {
 	funcMap["KubeSeal"] = ctx.kubeSeal
 	funcMap["Namespace"] = ctx.namespace
 
+	funcMap["Mike"] = func() string {
+		return "mike"
+	}
+
 	funcMap["TLSCert"] = func(certName string, cn string, ips []interface{}, alternateDNS []interface{}, daysValid int) string {
 		if p, ok := tlsMap[certName]; ok {
 			return p.Cert
