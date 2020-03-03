29bbd263e576692ffccfd149468fe7cc7a70c577
Andrew Lavery <laverya@umich.edu>
2020-03-03 11:54:33 -0800

    move updateAppConfig code into function to make looping easier

--- pkg/handlers/config.go
+++ pkg/handlers/config.go
@@ -78,27 +78,43 @@ func UpdateAppConfig(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	archiveDir, err := app.GetAppVersionArchive(foundApp.ID, updateAppConfigRequest.Sequence)
+	resp, err := updateAppConfig(foundApp, updateAppConfigRequest.Sequence, updateAppConfigRequest)
 	if err != nil {
 		logger.Error(err)
-		updateAppConfigResponse.Error = "failed to get app version archive"
-		JSON(w, 500, updateAppConfigResponse)
+		JSON(w, 500, resp)
 		return
+	}
+
+	if len(resp.RequiredItems) > 0 {
+		JSON(w, 400, resp)
+		return
+	}
+
+	JSON(w, 200, resp)
+}
+
+func updateAppConfig(updateApp *app.App, seq int, req UpdateAppConfigRequest) (UpdateAppConfigResponse, error) {
+	updateAppConfigResponse := UpdateAppConfigResponse{
+		Success: false,
+	}
+
+	archiveDir, err := app.GetAppVersionArchive(updateApp.ID, req.Sequence)
+	if err != nil {
+		updateAppConfigResponse.Error = "failed to get app version archive"
+		return updateAppConfigResponse, err
 	}
 	defer os.RemoveAll(archiveDir)
 
 	kotsKinds, err := kotsutil.LoadKotsKindsFromPath(archiveDir)
 	if err != nil {
-		logger.Error(err)
 		updateAppConfigResponse.Error = "failed to load kots kinds from path"
-		JSON(w, 500, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, err
 	}
 
 	// check for unset required items
 	requiredItems := make([]string, 0, 0)
 	requiredItemsTitles := make([]string, 0, 0)
-	for _, group := range updateAppConfigRequest.ConfigGroups {
+	for _, group := range req.ConfigGroups {
 		for _, item := range group.Items {
 			if app.IsRequiredItem(item) && app.IsUnsetItem(item) {
 				requiredItems = append(requiredItems, item.Name)
@@ -117,14 +133,13 @@ func UpdateAppConfig(w http.ResponseWriter, r *http.Request) {
 			Error:         fmt.Sprintf("The following fields are required: %s", strings.Join(requiredItemsTitles, ", ")),
 			RequiredItems: requiredItems,
 		}
-		JSON(w, 400, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, nil
 	}
 
 	// we don't merge, this is a wholesale replacement of the config values
 	// so we don't need the complex logic in kots, we can just write
 	values := kotsKinds.ConfigValues.Spec.Values
-	for _, group := range updateAppConfigRequest.ConfigGroups {
+	for _, group := range req.ConfigGroups {
 		for _, item := range group.Items {
 			if item.Value.Type == multitype.Bool {
 				updatedValue := item.Value.BoolVal
@@ -137,10 +152,8 @@ func UpdateAppConfig(w http.ResponseWriter, r *http.Request) {
 					// encrypt using the key
 					cipher, err := crypto.AESCipherFromString(kotsKinds.Installation.Spec.EncryptionKey)
 					if err != nil {
-						logger.Error(err)
 						updateAppConfigResponse.Error = "failed to get encryption cipher"
-						JSON(w, 500, updateAppConfigResponse)
-						return
+						return updateAppConfigResponse, err
 					}
 
 					// if the decryption succeeds, don't encrypt again
@@ -158,87 +171,66 @@ func UpdateAppConfig(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if kotsKinds.ConfigValues == nil {
-		logger.Error(errors.New("no config values found"))
 		updateAppConfigResponse.Error = "no config values found"
-		JSON(w, 500, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, errors.New("no config values found")
 	}
 
 	kotsKinds.ConfigValues.Spec.Values = values
 
 	configValuesSpec, err := kotsKinds.Marshal("kots.io", "v1beta1", "ConfigValues")
 	if err != nil {
-		logger.Error(err)
 		updateAppConfigResponse.Error = "failed to marshal config values spec"
-		JSON(w, 500, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, err
 	}
 
 	if err := ioutil.WriteFile(filepath.Join(archiveDir, "upstream", "userdata", "config.yaml"), []byte(configValuesSpec), 0644); err != nil {
-		logger.Error(err)
 		updateAppConfigResponse.Error = "failed to write config.yaml to upstream/userdata"
-		JSON(w, 500, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, err
 	}
 
-	err = foundApp.RenderDir(archiveDir)
+	err = updateApp.RenderDir(archiveDir)
 	if err != nil {
-		logger.Error(err)
 		updateAppConfigResponse.Error = "failed to render archive directory"
-		JSON(w, 500, updateAppConfigResponse)
-		return
+		return updateAppConfigResponse, err
 	}
 
-	if updateAppConfigRequest.CreateNewVersion {
-		newSequence, err := foundApp.CreateVersion(archiveDir, "Config Change")
+	if req.CreateNewVersion {
+		newSequence, err := updateApp.CreateVersion(archiveDir, "Config Change")
 		if err != nil {
-			logger.Error(err)
 			updateAppConfigResponse.Error = "failed to create an app version"
-			JSON(w, 500, updateAppConfigResponse)
-			return
+			return updateAppConfigResponse, err
 		}
 
-		if err := app.CreateAppVersionArchive(foundApp.ID, newSequence, archiveDir); err != nil {
-			logger.Error(err)
+		if err := app.CreateAppVersionArchive(updateApp.ID, newSequence, archiveDir); err != nil {
 			updateAppConfigResponse.Error = "failed to create an app version archive"
-			JSON(w, 500, updateAppConfigResponse)
-			return
+			return updateAppConfigResponse, err
 		}
 	} else {
-		if err := app.UpdateConfigValuesInDB(archiveDir, foundApp.ID, int64(updateAppConfigRequest.Sequence)); err != nil {
-			logger.Error(err)
+		if err := app.UpdateConfigValuesInDB(archiveDir, updateApp.ID, int64(req.Sequence)); err != nil {
 			updateAppConfigResponse.Error = "failed to update config values in db"
-			JSON(w, 500, updateAppConfigResponse)
-			return
+			return updateAppConfigResponse, err
 		}
 
 		if kotsKinds.Preflight != nil {
-			if err := app.SetDownstreamVersionPendingPreflight(foundApp.ID, int64(updateAppConfigRequest.Sequence)); err != nil {
-				logger.Error(err)
+			if err := app.SetDownstreamVersionPendingPreflight(updateApp.ID, int64(req.Sequence)); err != nil {
 				updateAppConfigResponse.Error = "failed to set downstream version pending preflight"
-				JSON(w, 500, updateAppConfigResponse)
-				return
+				return updateAppConfigResponse, err
 			}
 		} else {
-			if err := app.SetDownstreamVersionReady(foundApp.ID, int64(updateAppConfigRequest.Sequence)); err != nil {
-				logger.Error(err)
+			if err := app.SetDownstreamVersionReady(updateApp.ID, int64(req.Sequence)); err != nil {
 				updateAppConfigResponse.Error = "failed to set downstream version ready"
-				JSON(w, 500, updateAppConfigResponse)
-				return
+				return updateAppConfigResponse, err
 			}
 		}
 
-		if err := app.CreateAppVersionArchive(foundApp.ID, int64(updateAppConfigRequest.Sequence), archiveDir); err != nil {
-			logger.Error(err)
+		if err := app.CreateAppVersionArchive(updateApp.ID, int64(req.Sequence), archiveDir); err != nil {
 			updateAppConfigResponse.Error = "failed to create app version archive"
-			JSON(w, 500, updateAppConfigResponse)
-			return
+			return updateAppConfigResponse, err
 		}
 	}
 
 	updateAppConfigResponse.Success = true
-
-	JSON(w, 200, updateAppConfigResponse)
+	return updateAppConfigResponse, nil
 }
 
 func decrypt(input string, cipher *crypto.AESCipher) (string, error) {
