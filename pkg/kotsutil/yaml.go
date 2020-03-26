58765d0a1c3b3dc070e3dd2dee7b2a84bffc4fd5
divolgin <dmitriy@replicated.com>
2020-03-26 21:05:33 +0000

    preserve multi-doc yaml files

--- pkg/kotsutil/yaml.go
+++ pkg/kotsutil/yaml.go
@@ -1,6 +1,8 @@
 package kotsutil
 
 import (
+	"bytes"
+
 	"github.com/pkg/errors"
 	"github.com/replicatedhq/kots/pkg/util"
 	yaml "github.com/replicatedhq/yaml/v3"
@@ -9,17 +11,25 @@ import (
 // FixUpYAML is a general purpose function that will ensure that YAML is copmatible with KOTS
 // This ensures that lines aren't wrapped at 80 chars which breaks template functions
 func FixUpYAML(inputContent []byte) ([]byte, error) {
-	yamlObj := map[string]interface{}{}
+	docs := bytes.Split(inputContent, []byte("\n---\n"))
 
-	err := yaml.Unmarshal(inputContent, &yamlObj)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to unmarshal yaml")
+	fixedUpDocs := make([][]byte, 0)
+	for _, doc := range docs {
+		yamlObj := map[string]interface{}{}
+
+		err := yaml.Unmarshal(doc, &yamlObj)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal yaml")
+		}
+
+		fixedUpDoc, err := util.MarshalIndent(2, yamlObj)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to marshal yaml")
+		}
+
+		fixedUpDocs = append(fixedUpDocs, fixedUpDoc)
 	}
 
-	inputContent, err = util.MarshalIndent(2, yamlObj)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to marshal yaml")
-	}
-
-	return inputContent, nil
+	// MarshalIndent add a line break at the end of each file
+	return bytes.Join(fixedUpDocs, []byte("---\n")), nil
 }
