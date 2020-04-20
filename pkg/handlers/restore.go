a48cd8175113f473a6e7444e80e85ede356b0c9c
divolgin <dmitriy@replicated.com>
2020-04-20 20:29:26 +0000

    Initiate a restore

--- pkg/handlers/restore.go
+++ pkg/handlers/restore.go
@@ -2,8 +2,13 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+
+	"github.com/replicatedhq/kotsadm/pkg/app"
 
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/kotsadm/pkg/downstream"
 	"github.com/replicatedhq/kotsadm/pkg/logger"
 	"github.com/replicatedhq/kotsadm/pkg/session"
 	"github.com/replicatedhq/kotsadm/pkg/snapshot"
@@ -45,10 +50,59 @@ func CreateRestore(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = snapshot.CreateRestore(mux.Vars(r)["snapshotName"])
+	backup, err := snapshot.GetBackup(mux.Vars(r)["snapshotName"])
 	if err != nil {
 		logger.Error(err)
-		createRestoreResponse.Error = "failed to create restore"
+		createRestoreResponse.Error = "failed to find backup"
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	appID := backup.Annotations["kots.io/app-id"]
+	sequence, err := strconv.ParseInt(backup.Annotations["kots.io/app-sequence"], 10, 64)
+	if err != nil {
+		logger.Error(err)
+		createRestoreResponse.Error = "failed to parse sequence label"
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	status, err := downstream.GetDownstreamVersionStatus(appID, sequence)
+	if err != nil {
+		logger.Error(err)
+		createRestoreResponse.Error = "failed to find downstream version"
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	if status != "deployed" {
+		err := errors.Errorf("sequence %d of app %s was never deployed to this cluster", sequence, appID)
+		logger.Error(err)
+		createRestoreResponse.Error = err.Error()
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	kotsApp, err := app.Get(appID)
+	if err != nil {
+		logger.Error(err)
+		createRestoreResponse.Error = "failed to get app"
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	if kotsApp.RestoreInProgressName != "" {
+		err := errors.Errorf("restore is already in progress")
+		logger.Error(err)
+		createRestoreResponse.Error = err.Error()
+		JSON(w, 500, createRestoreResponse)
+		return
+	}
+
+	err = app.InitiateRestore(mux.Vars(r)["snapshotName"], appID)
+	if err != nil {
+		logger.Error(err)
+		createRestoreResponse.Error = "failed to initiate restore"
 		JSON(w, 500, createRestoreResponse)
 		return
 	}
