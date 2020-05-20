9d2887351005afcd02ea065c46f27d9b906b27d8
Andrew Lavery <laverya@umich.edu>
2020-05-20 16:17:11 -0400

    store redaction report in sql

--- kotsadm/pkg/supportbundle/redactions.go
+++ kotsadm/pkg/supportbundle/redactions.go
@@ -1,30 +1,51 @@
 package supportbundle
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/kots/kotsadm/pkg/persistence"
 	"github.com/replicatedhq/troubleshoot/pkg/redact"
 )
 
-// todo: actually persist this in db
-var redactions map[string]redact.RedactionList
-
 func GetRedactions(bundleID string) (redact.RedactionList, error) {
-	redacts, ok := redactions[bundleID]
-	if !ok {
+	db := persistence.MustGetPGSession()
+	q := `select redact_report from supportbundle where id = $1`
+
+	var redactString sql.NullString
+	row := db.QueryRow(q, bundleID)
+	err := row.Scan(&redactString)
+	if err != nil {
+		return redact.RedactionList{}, errors.Wrap(err, "select redact_report")
+	}
+
+	if !redactString.Valid || redactString.String == "" {
 		return redact.RedactionList{}, fmt.Errorf("unable to find redactions for bundle %s", bundleID)
 	}
+
+	redacts := redact.RedactionList{}
+	err = json.Unmarshal([]byte(redactString.String), &redacts)
+	if err != nil {
+		return redact.RedactionList{}, errors.Wrap(err, "unmarshal redact report")
+	}
+
 	return redacts, nil
 }
 
 func SetRedactions(bundleID string, redacts redact.RedactionList) error {
-	if redactions == nil {
-		redactions = map[string]redact.RedactionList{}
+	db := persistence.MustGetPGSession()
+
+	redactBytes, err := json.Marshal(redacts)
+	if err != nil {
+		return errors.Wrap(err, "marshal redactionlist")
 	}
-	if _, ok := redactions[bundleID]; ok {
-		// overwriting previously stored value is an error
-		return fmt.Errorf("redactions for bundle %s already present", bundleID)
+
+	query := `update supportbundle set redact_report = $1 where id = $2`
+	_, err = db.Exec(query, string(redactBytes), bundleID)
+	if err != nil {
+		return errors.Wrap(err, "failed to set support bundle redact report")
 	}
-	redactions[bundleID] = redacts
 	return nil
 }
