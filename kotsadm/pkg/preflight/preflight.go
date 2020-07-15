7f621f7ed4debe9a303e702ddecf290d3033abc5
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-07-15 13:23:23 -0700

    ability to rerun preflight checks (#807)

--- kotsadm/pkg/preflight/preflight.go
+++ kotsadm/pkg/preflight/preflight.go
@@ -87,6 +87,16 @@ func Run(appID string, sequence int64, archiveDir string) error {
 	return nil
 }
 
+func ResetPreflightResult(appID string, sequence int64) error {
+	db := persistence.MustGetPGSession()
+	query := `update app_downstream_version set preflight_result=null, preflight_result_created_at=null where app_id = $1 and parent_sequence = $2`
+	_, err := db.Exec(query, appID, sequence)
+	if err != nil {
+		return errors.Wrap(err, "failed to exec")
+	}
+	return nil
+}
+
 // this is a copy from registry.  so many import cycles to unwind here, todo
 func getRegistrySettingsForApp(appID string) (*registrytypes.RegistrySettings, error) {
 	db := persistence.MustGetPGSession()
