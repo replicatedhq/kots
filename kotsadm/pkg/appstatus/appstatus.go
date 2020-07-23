949cff67ba5fb08f9baf2c20d18f5874e9d1f904
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-07-23 00:08:53 +0000

    move get app dashboard query to go

---
+++ kotsadm/pkg/appstatus/appstatus.go
@@ -0,0 +1,101 @@
+package appstatus
+
+import (
+	"database/sql"
+	"encoding/json"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/kots/kotsadm/pkg/persistence"
+)
+
+type AppStatus struct {
+	AppID          string          `json:"appId"`
+	UpdatedAt      time.Time       `json:"updatedAt"`
+	ResourceStates []ResourceState `json:"resourceStates"`
+	State          State           `json:"state"`
+}
+
+type ResourceState struct {
+	Kind      string `json:"kind"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	State     State  `json:"state"`
+}
+
+type State string
+
+const (
+	StateReady       State = "ready"
+	StateDegraded    State = "degraded"
+	StateUnavailable State = "unavailable"
+	StateMissing     State = "missing"
+)
+
+func Get(appID string) (*AppStatus, error) {
+	db := persistence.MustGetPGSession()
+	query := `select resource_states, updated_at from app_status where app_id = $1`
+	row := db.QueryRow(query, appID)
+
+	var updatedAt sql.NullTime
+	var resourceStatesStr sql.NullString
+
+	if err := row.Scan(&resourceStatesStr, &updatedAt); err != nil {
+		if err == sql.ErrNoRows {
+			return &AppStatus{
+				AppID:          appID,
+				UpdatedAt:      time.Time{},
+				ResourceStates: []ResourceState{},
+				State:          StateMissing,
+			}, nil
+		}
+		return nil, errors.Wrap(err, "failed to scan")
+	}
+
+	appStatus := AppStatus{
+		AppID: appID,
+	}
+	
+	if updatedAt.Valid {
+		appStatus.UpdatedAt = updatedAt.Time
+	}
+
+	if resourceStatesStr.Valid {
+		var resourceStates []ResourceState
+		if err := json.Unmarshal([]byte(resourceStatesStr.String), &resourceStates); err != nil {
+			return nil, errors.Wrap(err, "failed to unmarshal resource states")
+		}
+		appStatus.ResourceStates = resourceStates
+	}
+
+	appStatus.State = getState(appStatus.ResourceStates)
+
+	return &appStatus, nil
+}
+
+func getState(resourceStates []ResourceState) State {
+	if len(resourceStates) == 0 {
+		return StateMissing
+	}
+	max := StateReady
+	for _, resourceState := range resourceStates {
+		max = minState(max, resourceState.State)
+	}
+	return max
+}
+
+func minState(a State, b State) State {
+	if a == StateMissing || b == StateMissing {
+		return StateMissing
+	}
+	if a == StateUnavailable || b == StateUnavailable {
+		return StateUnavailable
+	}
+	if a == StateDegraded || b == StateDegraded {
+		return StateDegraded
+	}
+	if a == StateReady || b == StateReady {
+		return StateReady
+	}
+	return StateMissing
+}
---
+++ kotsadm/pkg/handlers/dashboard.go
@@ -0,0 +1,76 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/replicatedhq/kots/kotsadm/pkg/app"
+	"github.com/replicatedhq/kots/kotsadm/pkg/appstatus"
+	"github.com/replicatedhq/kots/kotsadm/pkg/kotsadmparams"
+	"github.com/replicatedhq/kots/kotsadm/pkg/logger"
+	"github.com/replicatedhq/kots/kotsadm/pkg/version"
+)
+
+type GetAppDashboardResponse struct {
+	AppStatus         *appstatus.AppStatus  `json:"appStatus"`
+	Metrics           []version.MetricChart `json:"metrics"`
+	PrometheusAddress string                `json:"prometheusAddress"`
+}
+
+func GetAppDashboard(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "content-type, origin, accept, authorization")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if err := requireValidSession(w, r); err != nil {
+		logger.Error(err)
+		return
+	}
+
+	appSlug := mux.Vars(r)["appSlug"]
+
+	a, err := app.GetFromSlug(appSlug)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	appStatus, err := appstatus.Get(a.ID)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	// TODO: get downstream current sequence not app current sequence
+	metrics, err := version.GetMetricCharts(a.ID, a.CurrentSequence)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	prometheusAddress, err := kotsadmparams.Get("PROMETHEUS_ADDRESS")
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(500)
+		return
+	}
+	if prometheusAddress == "" {
+		prometheusAddress = os.Getenv("PROMETHEUS_ADDRESS")
+	}
+
+	getAppDashboardResponse := GetAppDashboardResponse{
+		AppStatus:         appStatus,
+		Metrics:           metrics,
+		PrometheusAddress: prometheusAddress,
+	}
+
+	JSON(w, 200, getAppDashboardResponse)
+}
