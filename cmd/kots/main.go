6d2892728568052853b823689b37d8ae2008501a
Ethan Mosbaugh <ethan@replicated.com>
2020-08-19 15:47:34 +0000

    Initialize all auth plugins

--- cmd/kots/main.go
+++ cmd/kots/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"github.com/replicatedhq/kots/cmd/kots/cli"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 func main() {
