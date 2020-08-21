dc11f045ef4ff3f4c75ef3b3dd5585a08b14954a
Andrew Lavery <laverya@umich.edu>
2020-08-21 19:50:06 -0400

    just remove the getClusters stuff entirely

--- kotsadm/pkg/apiserver/server.go
+++ kotsadm/pkg/apiserver/server.go
@@ -177,9 +177,6 @@ func Start() {
 	r.Path("/api/v1/gitops/app/{appId}/cluster/{clusterId}/initconnection").Methods("OPTIONS", "POST").HandlerFunc(handlers.InitGitOpsConnection)
 	r.Path("/api/v1/gitops/reset").Methods("OPTIONS", "POST").HandlerFunc(handlers.ResetGitOps)
 
-	// Clusters
-	r.Path("/api/v1/clusters/list").Methods("OPTIONS", "GET").HandlerFunc(handlers.GetClusters)
-
 	// to avoid confusion, we don't serve this in the dev env...
 	if os.Getenv("DISABLE_SPA_SERVING") != "1" {
 		spa := handlers.SPAHandler{StaticPath: filepath.Join("web", "dist"), IndexPath: "index.html"}
--- kotsadm/web/src/components/clusters/GitOps.jsx
+++ kotsadm/web/src/components/clusters/GitOps.jsx
@@ -4,7 +4,6 @@ import Helmet from "react-helmet";
 import { graphql, compose, withApollo } from "react-apollo";
 import { withRouter } from "react-router-dom";
 import GitOpsDeploymentManager from "../gitops/GitOpsDeploymentManager";
-import { Utilities } from "../../utilities/utilities";
 
 import "../../scss/components/watches/WatchedApps.scss";
 import "../../scss/components/watches/WatchCard.scss";
@@ -14,45 +13,19 @@ export class GitOps extends React.Component {
     history: PropTypes.object.isRequired,
   };
 
-  componentDidMount() {
-    this.getClusters();
-  }
-
   render() {
-    const hasClusters = this.state && this.state.clusters && this.state.clusters?.length && this.state.clusters[0];
-
     return (
       <div className="ClusterDashboard--wrapper container flex-column flex1 u-overflow--auto">
         <Helmet>
           <title>GitOps deployments</title>
         </Helmet>
         <div className="flex-column flex1">
-          {hasClusters && 
             <div className="flex-column flex-1-auto u-paddingBottom--20 u-paddingTop--30 u-marginTop--10 u-overflow--auto">
               <GitOpsDeploymentManager appName={this.props.appName} />
             </div>
-          }
         </div>
       </div>
     );
-  }
-
-  getClusters = async () => {
-    fetch(`${window.env.API_ENDPOINT}/clusters/list`, {
-      method: "GET",
-      headers: {
-        "Authorization": Utilities.getToken(),
-        "Content-Type": "application/json",
-      }
-    }).then(async (response) => {
-      const data = await response.json();
-      this.setState({
-          clusters: data,
-      })
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
   }
 }
 
