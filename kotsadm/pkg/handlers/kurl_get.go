d532554880d2b95b21500ababa81db9532a9c117
Andrew Lavery <laverya@umich.edu>
2020-08-26 17:48:38 -0400

    golang api for kurl nodes list endpoint

--- kotsadm/pkg/handlers/kurl_get.go
+++ kotsadm/pkg/handlers/kurl_get.go
@@ -1,1 +1,39 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/replicatedhq/kots/kotsadm/pkg/k8s"
+	"github.com/replicatedhq/kots/kotsadm/pkg/kurl"
+	"github.com/replicatedhq/kots/kotsadm/pkg/logger"
+)
+
+func GetKurlNodes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "content-type, origin, accept, authorization")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if err := requireValidSession(w, r); err != nil {
+		logger.Error(err)
+		return
+	}
+
+	client, err := k8s.Clientset()
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	nodes, err := kurl.GetNodes(client)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	JSON(w, http.StatusOK, nodes)
+}
---
+++ kotsadm/pkg/kurl/kurl_nodes.go
@@ -0,0 +1,110 @@
+package kurl
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+	kuberneteserrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type KurlNodes struct {
+	Nodes         []Node `json:"nodes"`
+	HA            bool   `json:"ha"`
+	IsKurlEnabled bool   `json:"isKurlEnabled"`
+}
+
+type Node struct {
+	Name           string            `json:"name"`
+	IsConnected    bool              `json:"isConnected"`
+	CanDelete      bool              `json:"canDelete"`
+	KubeletVersion string            `json:"kubeletVersion"`
+	CPU            CapacityAvailable `json:"cpu"`
+	Memory         CapacityAvailable `json:"memory"`
+	Pods           CapacityAvailable `json:"pods"`
+	Conditions     NodeConditions    `json:"conditions"`
+}
+
+type CapacityAvailable struct {
+	Capacity  string `json:"capacity"`
+	Available string `json:"available"`
+}
+
+type NodeConditions struct {
+	MemoryPressure bool `json:"memoryPressure"`
+	DiskPressure   bool `json:"diskPressure"`
+	PidPressure    bool `json:"pidPressure"`
+	Ready          bool `json:"ready"`
+}
+
+// GetNodes will get a list of nodes with stats
+func GetNodes(client kubernetes.Interface) (KurlNodes, error) {
+	nodes, err := client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return KurlNodes{}, errors.Wrap(err, "list nodes")
+	}
+
+	toReturn := KurlNodes{}
+
+	for _, node := range nodes.Items {
+		toReturn.Nodes = append(toReturn.Nodes, Node{
+			Name:           node.Name,
+			IsConnected:    true,
+			CanDelete:      node.Spec.Unschedulable,
+			KubeletVersion: node.Status.NodeInfo.KubeletVersion,
+			CPU: CapacityAvailable{
+				Capacity: node.Status.Capacity.Cpu().String(), // TODO include available resources
+			},
+			Memory: CapacityAvailable{
+				Capacity: node.Status.Capacity.Memory().String(),
+			},
+			Pods: CapacityAvailable{
+				Capacity: node.Status.Capacity.Pods().String(),
+			},
+			Conditions: findNodeConditions(node.Status.Conditions),
+		})
+	}
+
+	kurlConf, err := ReadConfigMap(client)
+	if err != nil {
+		if kuberneteserrors.IsNotFound(err) {
+			toReturn.IsKurlEnabled = false
+			return toReturn, nil
+		}
+		return KurlNodes{}, errors.Wrap(err, "get kurl config")
+	} else {
+		toReturn.IsKurlEnabled = true
+	}
+
+	if val, ok := kurlConf.Data["ha"]; ok {
+		parsedBool, err := strconv.ParseBool(val)
+		if err != nil {
+			return KurlNodes{}, errors.Wrapf(err, "parse 'ha' entry in kurl config %q", val)
+		}
+		toReturn.HA = parsedBool
+	}
+
+	return toReturn, nil
+}
+
+func findNodeConditions(conditions []v1.NodeCondition) NodeConditions {
+	discoveredConditions := NodeConditions{}
+	for _, condition := range conditions {
+		if condition.Type == "MemoryPressure" {
+			discoveredConditions.MemoryPressure = condition.Status == v1.ConditionTrue
+		}
+		if condition.Type == "DiskPressure" {
+			discoveredConditions.DiskPressure = condition.Status == v1.ConditionTrue
+		}
+		if condition.Type == "PIDPressure" {
+			discoveredConditions.PidPressure = condition.Status == v1.ConditionTrue
+		}
+		if condition.Type == "Ready" {
+			discoveredConditions.Ready = condition.Status == v1.ConditionTrue
+		}
+	}
+	return discoveredConditions
+}
