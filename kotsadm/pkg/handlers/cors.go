d20a014feef6257b5a57b0c690faaebe2516a817
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-10-14 15:57:14 -0700

    unique support bundle names (#1175)
    * unique support bundle names

--- kotsadm/pkg/handlers/cors.go
+++ kotsadm/pkg/handlers/cors.go
@@ -8,6 +8,7 @@ func CORS(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "content-type, origin, accept, authorization")
+	w.Header().Set("Access-Control-Expose-Headers", "content-disposition")
 }
 
 func handleOptionsRequest(w http.ResponseWriter, r *http.Request) (isOptionsRequest bool) {
