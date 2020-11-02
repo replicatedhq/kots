4f7ad63e35b2126a2dd200adc2c9b6fa83fc1ae3
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-11-02 15:36:58 -0800

    kotsadm pod restore init containers (#1211)

--- pkg/snapshot/restore.go
+++ pkg/snapshot/restore.go
@@ -8,9 +8,6 @@ import (
 	kotsadmtypes "github.com/replicatedhq/kots/pkg/kotsadm/types"
 	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	veleroclientv1 "github.com/vmware-tanzu/velero/pkg/generated/clientset/versioned/typed/velero/v1"
-	"github.com/vmware-tanzu/velero/pkg/kuberesource"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
@@ -77,126 +74,6 @@ func CreateInstanceRestore(options CreateInstanceRestoreOptions) (*velerov1.Rest
 			},
 			RestorePVs:              &trueVal,
 			IncludeClusterResources: &trueVal,
-			Hooks: velerov1.RestoreHooks{
-				Resources: []velerov1.RestoreResourceHookSpec{
-					{
-						Name:               "kotsadm-restore-hook",
-						IncludedNamespaces: []string{kotsadmNamespace},
-						IncludedResources:  []string{kuberesource.Pods.Resource},
-						LabelSelector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								kotsadmtypes.KotsadmKey: kotsadmtypes.KotsadmLabelValue,
-								"app":                   "kotsadm",
-							},
-						},
-						PostHooks: []velerov1.RestoreResourceHook{
-							{
-								Init: &velerov1.InitRestoreHook{
-									InitContainers: []corev1.Container{
-										{
-											Name:            "restore-db",
-											Image:           kotsadmImage,
-											ImagePullPolicy: corev1.PullAlways,
-											Command: []string{
-												"/restore-db.sh",
-											},
-											VolumeMounts: []corev1.VolumeMount{
-												{
-													Name:      "backup",
-													MountPath: "/backup",
-												},
-											},
-											Env: []corev1.EnvVar{
-												{
-													Name: "POSTGRES_PASSWORD",
-													ValueFrom: &corev1.EnvVarSource{
-														SecretKeyRef: &corev1.SecretKeySelector{
-															LocalObjectReference: corev1.LocalObjectReference{
-																Name: "kotsadm-postgres",
-															},
-															Key: "password",
-														},
-													},
-												},
-											},
-											Resources: corev1.ResourceRequirements{
-												Limits: corev1.ResourceList{
-													"cpu":    resource.MustParse("500m"),
-													"memory": resource.MustParse("500Mi"),
-												},
-												Requests: corev1.ResourceList{
-													"cpu":    resource.MustParse("100m"),
-													"memory": resource.MustParse("100Mi"),
-												},
-											},
-										},
-										{
-											Name:            "restore-s3",
-											Image:           kotsadmImage,
-											ImagePullPolicy: corev1.PullAlways,
-											Command: []string{
-												"/restore-s3.sh",
-											},
-											VolumeMounts: []corev1.VolumeMount{
-												{
-													Name:      "backup",
-													MountPath: "/backup",
-												},
-											},
-											Env: []corev1.EnvVar{
-												{
-													Name:  "S3_ENDPOINT",
-													Value: "http://kotsadm-minio:9000",
-												},
-												{
-													Name:  "S3_BUCKET_NAME",
-													Value: "kotsadm",
-												},
-												{
-													Name: "S3_ACCESS_KEY_ID",
-													ValueFrom: &corev1.EnvVarSource{
-														SecretKeyRef: &corev1.SecretKeySelector{
-															LocalObjectReference: corev1.LocalObjectReference{
-																Name: "kotsadm-minio",
-															},
-															Key: "accesskey",
-														},
-													},
-												},
-												{
-													Name: "S3_SECRET_ACCESS_KEY",
-													ValueFrom: &corev1.EnvVarSource{
-														SecretKeyRef: &corev1.SecretKeySelector{
-															LocalObjectReference: corev1.LocalObjectReference{
-																Name: "kotsadm-minio",
-															},
-															Key: "secretkey",
-														},
-													},
-												},
-												{
-													Name:  "S3_BUCKET_ENDPOINT",
-													Value: "true",
-												},
-											},
-											Resources: corev1.ResourceRequirements{
-												Limits: corev1.ResourceList{
-													"cpu":    resource.MustParse("500m"),
-													"memory": resource.MustParse("500Mi"),
-												},
-												Requests: corev1.ResourceList{
-													"cpu":    resource.MustParse("100m"),
-													"memory": resource.MustParse("100Mi"),
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
 		},
 	}
 
