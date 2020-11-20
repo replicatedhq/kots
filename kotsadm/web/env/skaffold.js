809b2923dfea364500b59c10b1d491f44436c1e5
Ethan Mosbaugh <ethan@replicated.com>
2020-11-20 04:55:04 +0000

    relative api endpoint

--- kotsadm/web/env/skaffold.js
+++ kotsadm/web/env/skaffold.js
@@ -1,7 +1,7 @@
 module.exports = {
   ENVIRONMENT: "development",
   SECURE_ADMIN_CONSOLE: true,
-  API_ENDPOINT: "http://127.0.0.1:30000/api/v1",
+  API_ENDPOINT: "/api/v1",
   KOTSADM_BUILD_VERSION: (function () {
     return String(Date.now());
   }()),
