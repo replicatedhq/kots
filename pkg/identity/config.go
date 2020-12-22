bb1c53757274c31015e9b561bed9ca8421fcaa38
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-12-22 11:41:25 -0800

    use default http client to ping kotsadm address (#1394)

--- pkg/identity/config.go
+++ pkg/identity/config.go
@@ -9,7 +9,6 @@ import (
 	"github.com/coreos/go-oidc"
 	dexoidc "github.com/dexidp/dex/connector/oidc"
 	ghodssyaml "github.com/ghodss/yaml"
-	"github.com/hashicorp/go-cleanhttp"
 	"github.com/pkg/errors"
 	kotsv1beta1 "github.com/replicatedhq/kots/kotskinds/apis/kots/v1beta1"
 	identitydeploy "github.com/replicatedhq/kots/pkg/identity/deploy"
@@ -29,17 +28,15 @@ var (
 	ConfigSecretKeyName = "dexConnectors"
 )
 
-var insecureClient *http.Client
-
-func init() {
-	transport := cleanhttp.DefaultTransport()
-	transport.TLSClientConfig = &tls.Config{
-		InsecureSkipVerify: true,
-	}
+var (
 	insecureClient = &http.Client{
-		Transport: transport,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
 	}
-}
+)
 
 type ErrorConnection struct {
 	Message string
