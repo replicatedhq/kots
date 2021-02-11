9aff6762f3999ab0b0423bca5324842518f684ca
Jelena <jelena.gruica@gmail.com>
2021-02-11 00:49:11 +0000

    updates

--- kotsadm/web/src/components/snapshots/SnapshotRow.jsx
+++ kotsadm/web/src/components/snapshots/SnapshotRow.jsx
@@ -24,7 +24,7 @@ class SnapshotRow extends React.Component {
     const isExpired = dayjs(new Date()).isSameOrAfter(snapshot?.expiresAt);
 
     return (
-      <div className={`flex flex-auto SnapshotRow--wrapper alignItems--center u-marginTop--10 ${snapshot?.status === "Deleting" && "is-deleting"} ${isExpired && "is-expired"}`}>
+      <div className={`flex flex-auto SnapshotRow--wrapper alignItems--center u-marginTop--10 ${snapshot?.status === "Deleting" && "is-deleting"} ${snapshot?.status === "InProgress" && "in-progress"} ${isExpired && "is-expired"}`}>
         <div className="flex-column flex1" style={{ maxWidth: "700px" }}>
           <p className={`u-fontSize--largest ${isExpired || snapshot?.status === "Deleting" ? "u-color--dustyGray" : "u-color--tuna"} u-lineHeight--normal u-fontWeight--bold u-marginRight--10`}>{snapshot?.name}</p>
           <div className="flex flex1 alignItems--center u-marginTop--10">
