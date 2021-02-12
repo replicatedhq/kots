b1f5461bd338f0ca6db636212964c7aa153c30bf
GraysonNull <graysonnull@me.com>
2021-02-12 12:09:30 -0600

    use inclusive language on nodes (#1498)

--- kotsadm/pkg/kurl/types/types.go
+++ kotsadm/pkg/kurl/types/types.go
@@ -9,7 +9,7 @@ type KurlNodes struct {
 type Node struct {
 	Name           string            `json:"name"`
 	IsConnected    bool              `json:"isConnected"`
-	IsMasterNode   bool              `json:"isMasterNode"`
+	IsPrimaryNode  bool              `json:"isPrimaryNode"`
 	CanDelete      bool              `json:"canDelete"`
 	KubeletVersion string            `json:"kubeletVersion"`
 	CPU            CapacityAvailable `json:"cpu"`
