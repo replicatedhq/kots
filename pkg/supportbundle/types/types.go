3d605c414f74456db24dba5718c7c2e3580712ec
Marc Campbell <marc.e.campbell@gmail.com>
2021-02-25 00:33:25 +0000

    Review comments

--- pkg/supportbundle/types/types.go
+++ pkg/supportbundle/types/types.go
@@ -4,6 +4,12 @@ import (
 	"time"
 )
 
+type ByCreated []*SupportBundle
+
+func (a ByCreated) Len() int           { return len(a) }
+func (a ByCreated) Less(i, j int) bool { return a[i].CreatedAt.Before(a[j].CreatedAt) }
+func (a ByCreated) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
 type SupportBundle struct {
 	ID         string     `json:"id"`
 	Slug       string     `json:"slug"`
