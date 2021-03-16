02be77ae22df74db037e6621a4ed315daaed1db4
Andrew Lavery <laverya@umich.edu>
2021-03-16 15:36:46 -0400

    fix typo in description of FixUpYAML function

--- pkg/kotsutil/yaml.go
+++ pkg/kotsutil/yaml.go
@@ -8,7 +8,7 @@ import (
 	yaml "github.com/replicatedhq/yaml/v3"
 )
 
-// FixUpYAML is a general purpose function that will ensure that YAML is copmatible with KOTS
+// FixUpYAML is a general purpose function that will ensure that YAML is compatible with KOTS
 // This ensures that lines aren't wrapped at 80 chars which breaks template functions
 func FixUpYAML(inputContent []byte) ([]byte, error) {
 	docs := bytes.Split(inputContent, []byte("\n---\n"))
