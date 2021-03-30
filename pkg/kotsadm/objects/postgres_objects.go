2ac4098f64aeda4f5c3678fafea1117b6d223899
divolgin <dmitriy@replicated.com>
2021-03-30 15:38:17 +0000

    Revert "update packages in postgres image to resolve CVEs (#1664)"
    This reverts commit f93f4240b933576f11e52764192f5fb0d873e600.

--- pkg/kotsadm/objects/postgres_objects.go
+++ pkg/kotsadm/objects/postgres_objects.go
@@ -14,8 +14,10 @@ import (
 )
 
 func PostgresStatefulset(deployOptions types.DeployOptions, size resource.Quantity) *appsv1.StatefulSet {
+	image := "postgres:10.7"
 	var pullSecrets []corev1.LocalObjectReference
 	if s := kotsadmversion.KotsadmPullSecret(deployOptions.Namespace, deployOptions.KotsadmOptions); s != nil {
+		image = fmt.Sprintf("%s/postgres:%s", kotsadmversion.KotsadmRegistry(deployOptions.KotsadmOptions), kotsadmversion.KotsadmTag(deployOptions.KotsadmOptions))
 		pullSecrets = []corev1.LocalObjectReference{
 			{
 				Name: s.ObjectMeta.Name,
@@ -86,7 +88,7 @@ func PostgresStatefulset(deployOptions types.DeployOptions, size resource.Quanti
 					},
 					Containers: []corev1.Container{
 						{
-							Image:           fmt.Sprintf("%s/postgres:%s", kotsadmversion.KotsadmRegistry(deployOptions.KotsadmOptions), kotsadmversion.KotsadmTag(deployOptions.KotsadmOptions)),
+							Image:           image,
 							ImagePullPolicy: corev1.PullIfNotPresent,
 							Name:            "kotsadm-postgres",
 							Ports: []corev1.ContainerPort{
