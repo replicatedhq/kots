f0f2eba4e95a2762d8933190164f72fa4526a2a6
GraysonNull <graysonnull@me.com>
2021-05-07 09:35:17 -0500

    add progress bar while collecting a support bundle (#1822)
    * add progress bar while collecting a support bundle
    
    * remove unused code, fix brackets

--- web/src/components/troubleshoot/SupportBundleCollectProgress.jsx
+++ web/src/components/troubleshoot/SupportBundleCollectProgress.jsx
@@ -1,46 +1,48 @@
 import React from "react"
 import Loader from "../shared/Loader";
 import "@src/scss/components/AirgapUploadProgress.scss";
-import { getReadableCollectorName } from "../../utilities/utilities";
 
 let percentage;
 
-function moveBar(count) {
-  const elem = document.getElementById("preflightStatusBar");
-  percentage = count > 21 ? 96 : (count * 4.5).toFixed();
+function moveBar(progressData) {
+  const elem = document.getElementById("supportBundleStatusBar");
+  const calcPercent = (progressData.collectorsCompleted/progressData.collectorCount) * 100;
+  percentage = calcPercent > 98 ? 98 : calcPercent.toFixed();
   if (elem) {
     elem.style.width = percentage + "%";
   }
 }
 
-export default function PreflightProgress(props) {
-  const { progressData, preflightResultCheckCount } = props;
+export default function SupportBundleCollectProgress(props) {
+  const { progressData } = props;
 
   let progressBar;
 
-  if (preflightResultCheckCount > 0) {
-    moveBar(preflightResultCheckCount);
+  if (progressData.collectorsCompleted > 0) {
+    moveBar(progressData);
     progressBar = (
       <div className="progressbar">
-        <div className="progressbar-meter" id="preflightStatusBar" style={{ width: "0px" }} />
+        <div className="progressbar-meter" id="supportBundleStatusBar" style={{ width: "0px" }} />
       </div>
     );
   } else {
     percentage = "0";
     progressBar = (
       <div className="progressbar">
-        <div className="progressbar-meter" id="preflightStatusBar" style={{ width: "0px" }} />
+        <div className="progressbar-meter" id="supportBundleStatusBar" style={{ width: "0px" }} />
       </div>
     );
-    
   }
   
-  const readableName = getReadableCollectorName(progressData?.currentName);
   let statusDiv = (
     <div className="u-marginTop--20 u-fontWeight--medium u-lineHeight--medium u-textAlign--center">
       <div className="flex flex1 u-marginBottom--10 justifyContent--center alignItems--center u-textColor--secondary">
-        {progressData?.currentName && <Loader className="flex u-marginRight--5" size="24" />}
-        <p>{readableName}</p>
+        {progressData?.message && <Loader className="flex u-marginRight--5" size="24" />}
+        {percentage >= 98 ?
+          <p>Almost done, finializing your bundle...</p>
+        :
+          <p>Analyzing {progressData?.message}</p>
+        }
       </div>
     </div>
   );
@@ -49,7 +51,7 @@ export default function PreflightProgress(props) {
     <div className="PreflightProgress--wrapper flex-1-auto flex-column alignItems--center justifyContent--center u-marginTop--10">
       <div className="flex1 flex-column u-textColor--primary">
           <div className="flex1 flex-column alignItems--center justifyContent--center">
-            <h1 className="u-fontSize--larger u-fontWeight--bold u-marginBottom--10">Collecting information about your cluster</h1>
+            <h1 className="u-fontSize--larger u-fontWeight--bold u-marginBottom--10">Analyzing {props.appTitle}</h1>
             <div className="flex alignItems--center u-marginTop--20">
               <span className="u-fontWeight--bold u-fontSize--normal u-textColor--secondary u-marginRight--10">{percentage + "%"}</span>
               {progressBar}
