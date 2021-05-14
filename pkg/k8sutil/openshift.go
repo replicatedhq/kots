6f780cf0439958f5ee96ae2c4516710081598df1
divolgin <dmitriy@replicated.com>
2021-05-14 01:35:34 +0000

    Deploy alpine based postrges on OpenShift 4.2 and later

--- pkg/k8sutil/openshift.go
+++ pkg/k8sutil/openshift.go
@@ -1,8 +1,12 @@
 package k8sutil
 
 import (
+	"context"
 	"strings"
 
+	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -19,4 +23,36 @@ func IsOpenShift(clientset kubernetes.Interface) bool {
 		}
 	}
 	return false
+}
+
+func OpenShiftVersion() (string, error) {
+	// openshift-apiserver does not report version,
+	// clusteroperator/openshift-apiserver does, and only version number
+
+	cfg, err := GetClusterConfig()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get cluster config")
+	}
+
+	client, err := configv1client.NewForConfig(cfg)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to build client from config")
+	}
+
+	clusterOperator, err := client.ClusterOperators().Get(context.TODO(), "openshift-apiserver", metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get openshift apiserver")
+	}
+
+	if clusterOperator == nil {
+		return "", errors.New("no openshift apiserver found")
+	}
+
+	for _, ver := range clusterOperator.Status.Versions {
+		if ver.Name == "operator" {
+			return ver.Version, nil
+		}
+	}
+
+	return "", errors.New("no openshift operator found")
 }
