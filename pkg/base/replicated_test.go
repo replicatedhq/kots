1ec7328d17e074c2fa8ec863f80aff41c546a282
jeffreygolden <jeff@replicated.com>
2021-06-28 19:59:17 +0000

    variadic items base rendering

--- pkg/base/replicated_test.go
+++ pkg/base/replicated_test.go
@@ -8,6 +8,7 @@ import (
 	upstreamtypes "github.com/replicatedhq/kots/pkg/upstream/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 func Test_findAllKotsHelmCharts(t *testing.T) {
@@ -293,5 +294,174 @@ func Test_extractHelmBases(t *testing.T) {
 				t.Errorf("extractHelmBases() \n\n%s", fmtJSONDiff(got, tt.want))
 			}
 		})
+	}
+}
+
+func Test_renderReplicated(t *testing.T) {
+	// podName tests a normal ConfigOption, proving this process does not impact existing behavior
+	// mountPath tests defaults if no value is provided
+	// secretName-1 tests repeatable items with a string
+	// secretName-2 tests repeatable items with an int
+	// secretName-3 tests repeatable items with a bool
+	// don't touch this! tests merging repeatable items with existing items
+	tests := []struct {
+		name          string
+		upstream      *upstreamtypes.Upstream
+		renderOptions *RenderOptions
+		expectedFile  BaseFile
+	}{
+		{
+			name: "replace array with repeat values",
+			upstream: &upstreamtypes.Upstream{
+				Files: []upstreamtypes.UpstreamFile{
+					{
+						Path: "config.yaml",
+						Content: []byte(`
+apiVersion: kots.io/v1beta1 
+kind: Config 
+metadata: 
+  creationTimestamp: null 
+  name: config-sample 
+spec: 
+  groups:
+  - name: podInfo
+    description: info for pod
+    items:
+    - name: "podName"
+      type: "text"
+      default: "test"
+      value: "testPod"
+    - name: "mountPath"
+      type: "text"
+      default: /var/www/html
+  - name: secrets
+    minimumCount: 1
+    title: Secrets 
+    description: Buncha Secrets
+    items: 
+    - name: "secretName"
+      type: "text"
+      title: "Secret Name"
+      default: "onetwothree"
+      repeatable: true
+      minimumCount: 1
+      templates:
+      - apiVersion: apps/v1
+        kind: Deployment
+        name: my-deploy
+        namespace: my-app
+        yamlPath: spec.template.spec.containers[0].volumes[1].projected.sources[1]
+      valuesByGroup: 
+        secrets:
+          secretName-1: "123"
+          secretName-2: 456
+          secretName-3: true
+`,
+						),
+					},
+					{
+						Path: "deployment.yaml",
+						Content: []byte(`
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-deploy
+  namespace: my-app
+spec:
+  template:
+    spec:
+      containers:
+        - name: repl{{ ConfigOption "podName"}}
+          image: httpd
+          volumeMounts:
+          - mountPath: '{{repl ConfigOption "mountPath"}}'
+            name: secret-assets
+            readOnly: true
+          volumes:
+          - name: normalVolume
+            projected:
+              sources:
+              - name: normalSecret
+          - name: secret-assets
+            projected:
+              sources:
+              - name: "don't touch this!"
+              - name: 'repl{{ ConfigOption "secretName"}}'
+                pod: repl{{ ConfigOption "podName" }}
+                metaData:
+                  - pod: repl{{ ConfigOption "podName"}}
+              - name: "don't touch this either!"`),
+					},
+				},
+			},
+			renderOptions: &RenderOptions{},
+			expectedFile: BaseFile{
+				Path: "deployment.yaml",
+				Content: []byte(
+					`apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-deploy
+  namespace: my-app
+spec:
+  template:
+    spec:
+      containers:
+        - image: httpd
+          name: testPod
+          volumeMounts:
+            - mountPath: /var/www/html
+              name: secret-assets
+              readOnly: true
+          volumes:
+            - name: normalVolume
+              projected:
+                sources:
+                  - name: normalSecret
+            - name: secret-assets
+              projected:
+                sources:
+                  - name: "don't touch this!"
+                  - name: "don't touch this either!"
+                  - name: "123"
+                    pod: "testPod"
+                    metaData:
+                    - pod: "testPod"
+                  - name: 456
+                    pod: "testPod"
+                    metaData:
+                    - pod: "testPod"
+                  - name: true
+                    pod: "testPod"
+                    metaData:
+                    - pod: "testPod"`),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		base, err := renderReplicated(test.upstream, test.renderOptions)
+		if err != nil {
+			t.Errorf("renderReplicated(...) test %s failed with error %v", test.name, err)
+		}
+
+		for _, targetFile := range base.Files {
+			if targetFile.Path == test.expectedFile.Path {
+				var got interface{}
+				var want interface{}
+				err := yaml.Unmarshal(targetFile.Content, &got)
+				if err != nil {
+					t.Errorf("renderReplicated(...) test %s failed with error %v", test.name, err)
+				}
+				err = yaml.Unmarshal(test.expectedFile.Content, &want)
+				if err != nil {
+					t.Errorf("renderReplicated(...) test %s failed with error %v", test.name, err)
+				}
+
+				if !reflect.DeepEqual(got, want) {
+					t.Errorf("renderReplicated(...) test %s failed: wanted \n---\n%s, got \n---\n%s", test.name, want, got)
+				}
+			}
+		}
 	}
 }
