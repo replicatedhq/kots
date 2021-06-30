738d9bcf8a9dddb043895b11eab0e358069fe66f
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2021-06-30 11:28:55 -0700

    check if the bucket exists before running the migration (#1924)

--- deploy/migrate-s3.sh
+++ deploy/migrate-s3.sh
@@ -18,11 +18,17 @@ then
   exit 0
 fi
 
-echo 'object store detected, running migration ...'
-
 export S3_HOST=`echo $S3_ENDPOINT | awk -F/ '{print $3}'`
+export S3_S3CMD_FLAGS="--access_key=$S3_ACCESS_KEY_ID --secret_key=$S3_SECRET_ACCESS_KEY --host=$S3_HOST --no-ssl --host-bucket=$S3_BUCKET_NAME.$S3_HOST"
+if s3cmd $S3_S3CMD_FLAGS ls s3://$S3_BUCKET_NAME 2>&1 | grep -q 'NoSuchBucket'; then
+  echo 'bucket not found, skipping migration ...'
+  exit 0
+fi
+
+echo 'object store and bucket detected, running migration ...'
+
 mkdir -p $ARCHIVES_DIR
-s3cmd --access_key=$S3_ACCESS_KEY_ID --secret_key=$S3_SECRET_ACCESS_KEY --host=$S3_HOST --no-ssl --host-bucket=$S3_BUCKET_NAME.$S3_HOST sync s3://$S3_BUCKET_NAME $ARCHIVES_DIR
+s3cmd $S3_S3CMD_FLAGS sync s3://$S3_BUCKET_NAME $ARCHIVES_DIR
 
 echo 'migration ran successfully ...'
 echo 'recording that the migration ran ...'
