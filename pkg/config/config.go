6254ef48e6a1f4a97ebd20a5f74c623076b439b0
jeffreygolden <jeff@replicated.com>
2021-07-09 00:58:16 +0000

    repeatable should always be treated as variadic

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -118,8 +118,8 @@ func templateConfig(log *logger.CLILogger, configSpecData string, configValuesDa
 func ApplyValuesToConfig(config *kotsv1beta1.Config, values map[string]template.ItemValue) {
 	for idxG, g := range config.Spec.Groups {
 		for idxI, i := range g.Items {
-			// if the item is repeatable and we have values belonging to the item...
-			if i.Repeatable && checkForRepeatableItems(i.Name, values) {
+			// if the item is repeatable
+			if i.Repeatable {
 				if config.Spec.Groups[idxG].Items[idxI].ValuesByGroup == nil {
 					// initialize the appropriate maps
 					config.Spec.Groups[idxG].Items[idxI].ValuesByGroup = map[string]kotsv1beta1.GroupValues{}
@@ -215,13 +215,4 @@ func UnmarshalConfigValuesContent(content []byte) (map[string]template.ItemValue
 	}
 
 	return ctx, nil
-}
-
-func checkForRepeatableItems(itemName string, configValues map[string]template.ItemValue) bool {
-	for _, value := range configValues {
-		if value.RepeatableItem == itemName {
-			return true
-		}
-	}
-	return false
 }
