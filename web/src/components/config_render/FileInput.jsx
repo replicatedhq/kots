7124aed053ee417b7fed126a848883cc977aea85
GraysonNull <graysonnull@me.com>
2021-07-20 15:22:40 -0500

    Grayson/variadic config files (#1983)
    * multiple file uploads
    
    * better ui handling for multiple file uploads. ability to remove file from upload list
    
    * change trash icon

--- web/src/components/config_render/FileInput.jsx
+++ web/src/components/config_render/FileInput.jsx
@@ -25,10 +25,7 @@ export default class FileInput extends React.Component {
         this.setState({ errText: error });
       } else if (this.props.onChange) {
         this.setState({ fileAdded: true })
-        this.props.onChange(
-          map(files, "value"),
-          map(files, "filename")
-        );
+        this.props.onChange(files);
       }
     });
 
@@ -47,18 +44,31 @@ export default class FileInput extends React.Component {
     });
   }
 
+  renderFilesUploaded = (arr) => {
+    if (!arr || arr.length === 0) { return null };
+    return arr.map((item, index) => {
+      return (
+        <div key={`${item}-${index}`} className="u-marginTop--10" onClick={() => this.props.handleRemoveFile(this.props.name, item)}>
+          <span className={`icon u-smallCheckGreen u-marginRight--10 u-top--3`}></span>
+          {item}
+          {arr.length > 1 ? <span className="icon gray-trash-small clickable u-marginLeft--5 u-top--3" /> : null}
+        </div>
+      );
+    });
+  }
+
   render() {
     let label;
     this.props.label ? label = this.props.label : this.props.multiple
       ? label = "Upload files" : label = "Upload a file";
-
+    const hasFileOrValue = this.state.fileAdded || this.props.value || (this.props.multiple && this.props.filenamesText !== "");
 
     return (
       <div>
         <div className={`${this.props.readonly ? "readonly" : ""} ${this.props.disabled ? "disabled" : ""}`}>
           <p className="sub-header-color field-section-sub-header u-marginTop--10 u-marginBottom--5">{label}</p>
           <div className="flex flex-row">
-            <div className={`${this.state.fileAdded || this.props.value ? "file-uploaded" : "custom-file-upload"}`}>
+            <div className={`${hasFileOrValue ? "file-uploaded" : "custom-file-upload"}`}>
               <input
                 ref={(file) => this.file = file}
                 type="file"
@@ -70,13 +80,24 @@ export default class FileInput extends React.Component {
                 multiple={this.props.multiple}
                 disabled={this.props.disabled}
               />
-              <label htmlFor={`${this.props.name} selector`} className="u-position--relative">
-                <span className={`icon ${this.state.fileAdded || this.props.value ? "u-smallCheckGreen" : "u-ovalIcon clickable"} u-marginRight--10 u-top--3`}></span>
-                {this.state.fileAdded || this.props.value ? this.props.filenamesText : `Browse files for ${this.props.title}`}
-                {this.state.fileAdded || this.props.value ? 
-                  <p className="u-linkColor u-textDecoration--underlineOnHover u-fontSize--small u-marginLeft--30 u-marginTop--5">Select a different file</p>
-                : null }
-              </label>
+              {!this.props.multiple ?
+                <label htmlFor={`${this.props.name} selector`} className="u-position--relative">
+                  <span className={`icon ${hasFileOrValue ? "u-smallCheckGreen" : "u-ovalIcon clickable"} u-marginRight--10 u-top--3`}></span>
+                  {hasFileOrValue ? this.props.filenamesText : `Browse files for ${this.props.title}`}
+                  {hasFileOrValue ? 
+                    <p className="u-linkColor u-textDecoration--underlineOnHover u-fontSize--small u-marginLeft--30 u-marginTop--5">Select a different file</p>
+                  : null }
+                </label>
+              :
+                <div>
+                  {this.renderFilesUploaded(this.props.filenamesText)}
+                  <label htmlFor={`${this.props.name} selector`} className="u-position--relative">
+                    {hasFileOrValue ? 
+                      <p className="u-linkColor u-textDecoration--underlineOnHover u-fontSize--small u-marginLeft--30 u-marginTop--10">Select other files</p>
+                    : `Browse files for ${this.props.title}` }
+                  </label>
+                </div>
+              }
             </div>
           </div>
         </div>
