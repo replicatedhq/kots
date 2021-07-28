137433e88d653c4de04e988961e2d8717537b677
divolgin <dmitriy@replicated.com>
2021-07-28 23:32:43 +0000

    Check if at least one config group and analyzer are actually defined

--- pkg/store/kotsstore/version_store.go
+++ pkg/store/kotsstore/version_store.go
@@ -329,9 +329,9 @@ func (s *KOTSStore) createAppVersion(tx *sql.Tx, appID string, currentSequence *
 		// will support multiple downstreams, so this is cleaner here for now
 
 		downstreamStatus := types.VersionPending
-		if currentSequence == nil && kotsKinds.Config != nil { // initial version should always require configuration (if exists) even if all required items are already set and have values (except for automated installs, which can override this later)
+		if currentSequence == nil && kotsKinds.IsConfigurable() { // initial version should always require configuration (if exists) even if all required items are already set and have values (except for automated installs, which can override this later)
 			downstreamStatus = types.VersionPendingConfig
-		} else if kotsKinds.Preflight != nil && !skipPreflights {
+		} else if kotsKinds.HasPreflights() && !skipPreflights {
 			downstreamStatus = types.VersionPendingPreflight
 		}
 		if currentSequence != nil { // only check if the version needs configuration for later versions (not the initial one) since the config is always required for the initial version (except for automated installs, which can override that later)
