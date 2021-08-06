81c9b49321a35b41750fb3db896bbed272f882c5
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-08-06 13:01:46 -0700

    fix prometheus graphs and the pod namespace env var (#2059)

--- pkg/util/namespace.go
+++ pkg/util/namespace.go
@@ -1,12 +1,14 @@
 package util
 
+import "os"
+
 var (
-	PodNamespace     string
-	KotsadmNamespace string
+	PodNamespace     string = os.Getenv("POD_NAMESPACE")
+	KotsadmNamespace string = os.Getenv("KOTSADM_TARGET_NAMESPACE")
 )
 
 func AppNamespace() string {
-	if KotsadmNamespace == "" {
+	if KotsadmNamespace != "" {
 		return KotsadmNamespace
 	}
 
