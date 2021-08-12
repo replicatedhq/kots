0b1a1195d2f9c65ebed2f17da8eb6fd927fba923
Vera Harless <53271741+morningvera@users.noreply.github.com>
2021-08-12 22:33:28 +0000

    feat: add json output for backup and restore cmds

--- cmd/kots/cli/backup.go
+++ cmd/kots/cli/backup.go
@@ -1,7 +1,10 @@
 package cli
 
 import (
+	"encoding/json"
+
 	"github.com/pkg/errors"
+	"github.com/replicatedhq/kots/pkg/logger"
 	"github.com/replicatedhq/kots/pkg/print"
 	"github.com/replicatedhq/kots/pkg/snapshot"
 	"github.com/spf13/cobra"
@@ -23,12 +26,28 @@ func BackupCmd() *cobra.Command {
 
 			namespace := v.GetString("namespace")
 
+			output := v.GetString("output")
+			if output != "json" && output != "" {
+				return errors.Errorf("output format %s not supported (allowed formats are: json)", output)
+			}
+
 			options := snapshot.CreateInstanceBackupOptions{
 				Namespace: namespace,
 				Wait:      v.GetBool("wait"),
+				Silent:    output != "",
 			}
-			if err := snapshot.CreateInstanceBackup(cmd.Context(), options); err != nil {
+			backupRes, err := snapshot.CreateInstanceBackup(cmd.Context(), options)
+			if err != nil && output == "" {
 				return errors.Wrap(err, "failed to create instance backup")
+			}
+
+			log := logger.NewCLILogger()
+			if output == "json" {
+				outputJSON, err := json.Marshal(backupRes)
+				if err != nil {
+					return errors.Wrap(err, "error marshaling JSON")
+				}
+				log.Info(string(outputJSON))
 			}
 
 			return nil
@@ -37,6 +56,7 @@ func BackupCmd() *cobra.Command {
 
 	cmd.Flags().StringP("namespace", "n", "default", "namespace in which kots/kotsadm is installed")
 	cmd.Flags().Bool("wait", true, "wait for the backup to finish")
+	cmd.Flags().StringP("output", "o", "", "output format (currently supported: json)")
 
 	cmd.AddCommand(BackupListCmd())
 
