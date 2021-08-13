ef6393c0b544c066f5c17732c6bdc8f4de2a82d0
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-08-13 21:15:57 +0000

    use correct sequence for generating supportbundle dependencies

--- pkg/airgap/airgap.go
+++ pkg/airgap/airgap.go
@@ -254,7 +254,7 @@ func CreateAppFromAirgap(opts CreateAirgapAppOpts) (finalError error) {
 	troubleshootOpts := supportbundletypes.TroubleshootOptions{
 		InCluster: true,
 	}
-	_, err = supportbundle.CreateSupportBundleDependencies(a.ID, a.CurrentSequence, troubleshootOpts)
+	_, err = supportbundle.CreateSupportBundleDependencies(a.ID, 0, troubleshootOpts)
 	if err != nil {
 		return errors.Wrap(err, "failed to create support bundle dependencies")
 	}
