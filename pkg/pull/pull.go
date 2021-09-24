ccfb41cefe08e289c90c0b311e21e8c136830d40
jeffreygolden <jeff@replicated.com>
2021-09-24 19:19:39 +0000

    finding subcharts in kots pull

--- pkg/pull/pull.go
+++ pkg/pull/pull.go
@@ -420,8 +420,16 @@ func Pull(upstreamURI string, pullOptions PullOptions) (string, error) {
 	commonWriteMidstreamOptions.UseHelmInstall = map[string]bool{}
 	// this map contains chart names and useHelmInstall flag
 	// presence of chartname and the flag determines if the pullsecrets will be generated within each chart or at the top level
+	commonWriteMidstreamOptions.UseHelmInstall = map[string]bool{}
 	for _, v := range newHelmCharts {
 		commonWriteMidstreamOptions.UseHelmInstall[v.Spec.Chart.Name] = v.Spec.UseHelmInstall
+		subcharts, err := base.FindHelmSubChartsFromBase(writeBaseOptions.BaseDir, v.Spec.Chart.Name)
+		if err != nil {
+			return "", errors.Wrapf(err, "failed to find subcharts for parent chart %s", v.Spec.Chart.Name)
+		}
+		for _, subchart := range subcharts.SubCharts {
+			commonWriteMidstreamOptions.UseHelmInstall[subchart] = v.Spec.UseHelmInstall
+		}
 	}
 
 	writeMidstreamOptions := commonWriteMidstreamOptions
@@ -439,7 +447,7 @@ func Pull(upstreamURI string, pullOptions PullOptions) (string, error) {
 		writeMidstreamOptions.MidstreamDir = filepath.Join(helmBase.GetOverlaysDir(writeBaseOptions), "midstream", helmBase.Path)
 		writeMidstreamOptions.BaseDir = filepath.Join(u.GetBaseDir(writeUpstreamOptions), helmBase.Path)
 		// empty map indicates that the pullsecrets need to be generated within each chart
-		writeMidstreamOptions.UseHelmInstall = map[string]bool{}
+		//writeMidstreamOptions.UseHelmInstall = map[string]bool{}
 
 		helmMidstream, err := writeMidstream(writeMidstreamOptions, pullOptions, u, &helmBase, fetchOptions.License, identityConfig, u.GetUpstreamDir(writeUpstreamOptions), log)
 		if err != nil {
