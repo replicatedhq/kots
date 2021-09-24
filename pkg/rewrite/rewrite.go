7b6dfc0209a8ee72ae3b443a533728608bf000ff
jeffreygolden <jeff@replicated.com>
2021-09-24 20:18:30 +0000

    moving the subchart lookup logic

--- pkg/rewrite/rewrite.go
+++ pkg/rewrite/rewrite.go
@@ -226,13 +226,6 @@ func Rewrite(rewriteOptions RewriteOptions) error {
 	commonWriteMidstreamOptions.UseHelmInstall = map[string]bool{}
 	for _, v := range newHelmCharts {
 		commonWriteMidstreamOptions.UseHelmInstall[v.Spec.Chart.Name] = v.Spec.UseHelmInstall
-		subcharts, err := base.FindHelmSubChartsFromBase(writeBaseOptions.BaseDir, v.Spec.Chart.Name)
-		if err != nil {
-			return errors.Wrapf(err, "failed to find subcharts for parent chart %s", v.Spec.Chart.Name)
-		}
-		for _, subchart := range subcharts.SubCharts {
-			commonWriteMidstreamOptions.UseHelmInstall[subchart] = v.Spec.UseHelmInstall
-		}
 	}
 
 	writeMidstreamOptions := commonWriteMidstreamOptions
@@ -246,16 +239,25 @@ func Rewrite(rewriteOptions RewriteOptions) error {
 
 	commonWriteMidstreamOptions.UseHelmInstall = map[string]bool{}
 	helmMidstreams := []midstream.Midstream{}
-	for _, base := range helmBases {
+	for _, helmBase := range helmBases {
 		writeMidstreamOptions := commonWriteMidstreamOptions
-		writeMidstreamOptions.MidstreamDir = filepath.Join(base.GetOverlaysDir(writeBaseOptions), "midstream", base.Path)
-		writeMidstreamOptions.BaseDir = filepath.Join(u.GetBaseDir(writeUpstreamOptions), base.Path)
+		writeMidstreamOptions.MidstreamDir = filepath.Join(helmBase.GetOverlaysDir(writeBaseOptions), "midstream", helmBase.Path)
+		writeMidstreamOptions.BaseDir = filepath.Join(u.GetBaseDir(writeUpstreamOptions), helmBase.Path)
 
-		helmBase := base
+		for _, v := range newHelmCharts {
+			writeMidstreamOptions.UseHelmInstall[v.Spec.Chart.Name] = v.Spec.UseHelmInstall
+			subcharts, err := base.FindHelmSubChartsFromBase(writeBaseOptions.BaseDir, v.Spec.Chart.Name)
+			if err != nil {
+				return errors.Wrapf(err, "failed to find subcharts for parent chart %s", v.Spec.Chart.Name)
+			}
+			for _, subchart := range subcharts.SubCharts {
+				writeMidstreamOptions.UseHelmInstall[subchart] = v.Spec.UseHelmInstall
+			}
+		}
 
 		helmMidstream, err := writeMidstream(writeMidstreamOptions, rewriteOptions, &helmBase, fetchOptions.License, u.GetUpstreamDir(writeUpstreamOptions), log)
 		if err != nil {
-			return errors.Wrapf(err, "failed to write helm midstream %s", base.Path)
+			return errors.Wrapf(err, "failed to write helm midstream %s", helmBase.Path)
 		}
 
 		helmMidstreams = append(helmMidstreams, *helmMidstream)
