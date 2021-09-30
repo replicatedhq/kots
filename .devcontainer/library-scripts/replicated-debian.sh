98acd1f2a088680854266ffa65b87437e8e9494f
Dan Stough <dans@replicated.com>
2021-09-30 15:07:04 +0000

    optimize build

--- .devcontainer/library-scripts/replicated-debian.sh
+++ .devcontainer/library-scripts/replicated-debian.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 # k3d
-curl -s "https://raw.githubusercontent.com/rancher/k3d/main/install.sh" | bash 
+# v5 RC is needed to deterministically set the Registry port. Should be replaces with official release
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.0.0-rc.4 bash
 
 # skaffold
 curl -Lo /tmp/skaffold "https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64"
 sudo install /tmp/skaffold /usr/local/bin/
+
+# Setup Skaffold
+# TODO (dans): make this a bash variable
+mkdir -p /home/codespace/.skaffold
+cp /etc/replicated/skaffold.config /home/codespace/.skaffold/config
+chown codespace:root /home/codespace/.skaffold/config
 
 # kustomize
 pushd /tmp
