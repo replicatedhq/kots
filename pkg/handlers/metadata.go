b90391e763766e7fe97ae1ed29d2d9f8fc976d4b
Jalaja <jalaja@replicated.com>
2021-10-13 22:24:14 +0000

    [wip] kots feature flags

--- pkg/handlers/metadata.go
+++ pkg/handlers/metadata.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/pkg/errors"
@@ -10,18 +11,85 @@ import (
 	"github.com/replicatedhq/kots/pkg/kurl"
 	"github.com/replicatedhq/kots/pkg/logger"
 	"github.com/replicatedhq/kots/pkg/util"
+	v1 "k8s.io/api/core/v1"
 	kuberneteserrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
 type MetadataResponse struct {
-	IconURI       string `json:"iconUri"`
-	Name          string `json:"name"`
-	Namespace     string `json:"namespace"`
-	IsKurlEnabled bool   `json:"isKurlEnabled"`
-	UpstreamURI   string `json:"upstreamUri"`
+	IconURI             string   `json:"iconUri"`
+	Name                string   `json:"name"`
+	Namespace           string   `json:"namespace"`
+	IsKurlEnabled       bool     `json:"isKurlEnabled"`
+	UpstreamURI         string   `json:"upstreamUri"`
+	ConsoleFeatureFlags []string `json:"consoleFeatureFlags"`
 }
+
+func GetMetadataHandler(h *Handler, fn MetadataK8sFn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		brandingConfigMap, isKurl, err := fn()
+		if err != nil {
+			logger.Error(err)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+
+		}
+		metadataResponse := MetadataResponse{
+			IconURI:       "https://cdn2.iconfinder.com/data/icons/mixd/512/16_kubernetes-512.png",
+			Name:          "the john's application",
+			Namespace:     util.PodNamespace,
+			IsKurlEnabled: isKurl,
+		}
+
+		data, ok := brandingConfigMap.Data["application.yaml"]
+		if !ok {
+			logger.Error(errors.New("application.yaml not found in the configmap kotsadm-application-metadata"))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// parse data as a kotskind
+		obj, gvk, err := scheme.Codecs.UniversalDeserializer().Decode([]byte(data), nil, nil)
+		if err != nil {
+			logger.Error(fmt.Errorf("failed to decode application yaml %w", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if gvk.Group != "kots.io" || gvk.Version != "v1beta1" || gvk.Kind != "Application" {
+			logger.Error(errors.New("unexpected gvk found in metadata"))
+			w.WriteHeader(500)
+			return
+		}
+		application := obj.(*kotsv1beta1.Application)
+		metadataResponse.IconURI = application.Spec.Icon
+		metadataResponse.Name = application.Spec.Title
+		metadataResponse.UpstreamURI = brandingConfigMap.Data["upstreamUri"]
+		metadataResponse.ConsoleFeatureFlags = application.Spec.ConsoleFeatureFlags
+
+	}
+
+}
+
+func GetMetaDataConfig() (*v1.ConfigMap, bool, error) {
+	clientset, err := k8sutil.GetClientset()
+	if err != nil {
+		return nil, false, nil
+	}
+
+	isKurlEnabled := kurl.IsKurl()
+
+	brandingConfigMap, err := clientset.CoreV1().ConfigMaps(util.PodNamespace).Get(context.TODO(), "kotsadm-application-metadata", metav1.GetOptions{})
+	if err != nil && !kuberneteserrors.IsNotFound(err) {
+		return nil, false, nil
+	}
+
+	return brandingConfigMap, isKurlEnabled, nil
+}
+
+type MetadataK8sFn func() (*v1.ConfigMap, bool, error)
 
 // Metadata route is UNAUTHENTICATED
 // It is needed for branding/some cluster flags before user is logged in.
@@ -73,11 +141,13 @@ func (h *Handler) Metadata(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(500)
 			return
 		}
+		// TODO - return it here
 
 		application := obj.(*kotsv1beta1.Application)
 		metadataResponse.IconURI = application.Spec.Icon
 		metadataResponse.Name = application.Spec.Title
 		metadataResponse.UpstreamURI = brandingConfigMap.Data["upstreamUri"]
+		metadataResponse.ConsoleFeatureFlags = application.Spec.ConsoleFeatureFlags
 	}
 
 	JSON(w, 200, metadataResponse)
