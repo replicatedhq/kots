a901235151941862580f368b7c5d4d0f5e7e461b
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-11-18 15:35:18 -0800

    send app installation time when checking for updates (#2350)
    * send app installation time when checking for updates

--- pkg/upstream/types/types.go
+++ pkg/upstream/types/types.go
@@ -20,17 +20,18 @@ type UpstreamFile struct {
 }
 
 type Upstream struct {
-	URI           string
-	Name          string
-	Type          string
-	Files         []UpstreamFile
-	UpdateCursor  string
-	ChannelID     string
-	ChannelName   string
-	VersionLabel  string
-	ReleaseNotes  string
-	ReleasedAt    *time.Time
-	EncryptionKey string
+	URI             string
+	Name            string
+	Type            string
+	Files           []UpstreamFile
+	UpdateCursor    string
+	ChannelID       string
+	ChannelName     string
+	ChannelSequence int64
+	VersionLabel    string
+	ReleaseNotes    string
+	ReleasedAt      *time.Time
+	EncryptionKey   string
 }
 
 type Update struct {
@@ -68,6 +69,7 @@ type FetchOptions struct {
 	IdentityConfig         *kotsv1beta1.IdentityConfig
 	Airgap                 *kotsv1beta1.Airgap
 	EncryptionKey          string
+	InstalledAt            *time.Time
 	CurrentCursor          string
 	CurrentChannelID       string
 	CurrentChannelName     string
