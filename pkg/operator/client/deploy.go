46ccb6baaee9445e4b98f1236a26b155576555f1
jeffreygolden <jeff@replicated.com>
2022-02-10 15:09:02 -0600

    add default timeout to helm command

--- pkg/operator/client/deploy.go
+++ pkg/operator/client/deploy.go
@@ -488,7 +488,8 @@ func (c *Client) installWithHelm(helmDir string, targetNamespace string) (*comma
 			return nil, errors.Wrapf(err, "failed to unmarshal %s", chartfilePath)
 		}
 
-		args := []string{"upgrade", "-i", cname.ChartName, installDir}
+		args := []string{"upgrade", "-i", cname.ChartName, installDir, "--timeout 60m"}
+
 		if targetNamespace != "" && targetNamespace != "." {
 			args = append(args, "-n", targetNamespace)
 		}
