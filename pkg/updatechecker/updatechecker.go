331d54b5fae380956737eed1776a69ab2fc2a880
Pavan Sokke Nagaraj <pavan@replicated.com>
2022-02-25 15:37:36 -0500

    update: add logging

--- pkg/updatechecker/updatechecker.go
+++ pkg/updatechecker/updatechecker.go
@@ -549,6 +549,7 @@ func deployVersion(opts CheckForUpdatesOpts, clusterID string, appVersions *down
 		}
 
 		if err := version.DeployVersion(opts.AppID, versionToDeploy.Sequence); err != nil {
+			logger.Errorf("failed to deploy version with ID: %v, sequence: %v. error: %w", opts.AppID, versionToDeploy.Sequence, err.Error())
 			return errors.Wrap(err, "failed to deploy version")
 		}
 
