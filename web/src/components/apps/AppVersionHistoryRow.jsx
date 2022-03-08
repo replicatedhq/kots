723dddfdadf77a39446bb5db01a5fd3e224e3e93
Pavan Sokke Nagaraj <pavan@replicated.com>
2022-03-08 15:22:26 -0500

    Update AppVersionHistoryRow.jsx

--- web/src/components/apps/AppVersionHistoryRow.jsx
+++ web/src/components/apps/AppVersionHistoryRow.jsx
@@ -192,7 +192,7 @@ function renderVersionAction(version, nothingToCommitDiff, app, history, actionF
         <div className="flex alignItems--center">
           <button
             className={classNames("btn u-marginLeft--10", { "secondary dark": isRollback, "secondary blue": isSecondaryBtn, "primary blue": isPrimaryButton })}
-            disabled={version.status === "deploying"}
+            disabled={version.status === "deploying" || blockDeployment}
             onClick={() => needsConfiguration ? history.push(`/app/${app.slug}/config/${version.sequence}`) : isRollback ? actionFn(version, true) : actionFn(version)}
           >
             <span
