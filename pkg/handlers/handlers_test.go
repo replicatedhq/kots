df23958dec9b9b46b422e56dedb4a3abef21ae3c
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2022-03-25 00:43:33 +0000

    fix tests and don't disable button for pending config and pending download

--- pkg/handlers/handlers_test.go
+++ pkg/handlers/handlers_test.go
@@ -68,6 +68,7 @@ var HandlerPolicyTests = map[string][]HandlerPolicyTest{
 	},
 	"CanInstallAppVersion": {
 		{
+			Vars:         map[string]string{"appSlug": "my-app"},
 			Roles:        []rbactypes.Role{rbac.ClusterAdminRole},
 			SessionRoles: []string{rbac.ClusterAdminRoleID},
 			Calls: func(storeRecorder *mock_store.MockStoreMockRecorder, handlerRecorder *mock_handlers.MockKOTSHandlerMockRecorder) {
