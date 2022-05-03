cc52859a5dd36a065594975c797f3dd5e59109c3
Sean Rester <syntastical@users.noreply.github.com>
2022-05-03 14:02:08 -0400

    build

--- web/env/okteto.js
+++ web/env/okteto.js
@@ -1,5 +1,5 @@
 module.exports = {
   ENVIRONMENT: "development",
-  API_ENDPOINT: `https://kotsadm-${process.env.OKTETO_NAMESPACE}.replicated.okteto.dev/api/v1`,
+  API_ENDPOINT: `https://kotsadm-x-${process.env.OKTETO_NAMESPACE}-x-vcluster-${process.env.OKTETO_NAMESPACE}.replicated.okteto.dev/api/v1`,
   KOTSADM_BUILD_VERSION: Date.now()
 };
