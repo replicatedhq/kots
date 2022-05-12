80e119794cefc53f3c91d3b18426759cae0b0da3
miaawong <miawongdev@gmail.com>
2022-05-11 19:53:02 -0700

    fix env

--- web/postinstall.sh
+++ web/postinstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e 
 
-if [[ -n "$PUBLIC_ASSET_PATH" ]]; then
+if [[ -n "${PUBLIC_ASSET_PATH}" ]]; then
     mkdir -p ${PUBLIC_ASSET_PATH}/vs && cp -R node_modules/monaco-editor/min/vs/ ${PUBLIC_ASSET_PATH}/vs
 else 
     mkdir -p ./public/vs && cp -R node_modules/monaco-editor/min/vs/ ./public/vs
