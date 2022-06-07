a58346aa90ab0ff757090a98506d5726fab05a32
Star Richardson <67430892+alicenstar@users.noreply.github.com>
2022-06-07 17:43:43 -0500

    refactor: remove change password from footer, add autofocus to input field and change pass form

--- web/src/components/modals/ChangePasswordModal/ChangePasswordForm.jsx
+++ web/src/components/modals/ChangePasswordModal/ChangePasswordForm.jsx
@@ -4,33 +4,45 @@ import { Utilities } from "@src/utilities/utilities";
 
 import "@src/scss/components/modals/ChangePasswordModal/ChangePasswordForm.scss";
 
-const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) => {
+const ChangePasswordForm = ({
+  handleClose,
+  handleSetPasswordChangeSuccessful,
+}) => {
   const [err, setErr] = React.useState({
     status: false,
-    message: ""
+    message: "",
   });
   const [loading, setLoading] = React.useState(false);
   const [inputs, setInputs] = React.useState({
     currentPassword: "",
     newPassword: "",
-    confirmPassword: ""
+    confirmPassword: "",
   });
 
   const updateFormStatus = (loadingStatus, errStatus, message) => {
     setErr({
       status: errStatus,
-      message: message
+      message: message,
     });
     setLoading(loadingStatus);
-  }
+  };
 
   const validatePassword = () => {
     if (!inputs.currentPassword || inputs.currentPassword.length === "0") {
       updateFormStatus(false, true, "Current password is required.");
       return false;
     }
-    if (!inputs.newPassword || !inputs.confirmPassword || inputs.newPassword.length === "0" || inputs.confirmPassword.length === "0") {
-      updateFormStatus(false, true, "Please ensure you've filled out both new password fields.");
+    if (
+      !inputs.newPassword ||
+      !inputs.confirmPassword ||
+      inputs.newPassword.length === "0" ||
+      inputs.confirmPassword.length === "0"
+    ) {
+      updateFormStatus(
+        false,
+        true,
+        "Please ensure you've filled out both new password fields."
+      );
       return false;
     }
     if (inputs.newPassword !== inputs.confirmPassword) {
@@ -38,7 +50,7 @@ const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) 
       return false;
     }
     return true;
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -47,35 +59,42 @@ const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) 
 
       fetch(`${process.env.API_ENDPOINT}/password/change`, {
         headers: {
-          "Authorization": Utilities.getToken(),
+          Authorization: Utilities.getToken(),
           "Content-Type": "application/json",
         },
         method: "PUT",
         body: JSON.stringify({
           current_password: inputs.currentPassword,
           new_password: inputs.newPassword,
-        })
+        }),
       })
-      .then(async (res) => {
-        if (res.status >= 400) {
-          let body = await res.json();
-          let msg = body.error;
-          if (!msg) {
-            msg = res.status === 401 ? "User unauthorized. Please try again" : "There was an error changing your password. Please try again.";
+        .then(async (res) => {
+          if (res.status >= 400) {
+            let body = await res.json();
+            let msg = body.error;
+            if (!msg) {
+              msg =
+                res.status === 401
+                  ? "User unauthorized. Please try again"
+                  : "There was an error changing your password. Please try again.";
+            }
+            updateFormStatus(false, true, msg);
+            handleSetPasswordChangeSuccessful(false);
+            return;
           }
-          updateFormStatus(false, true, msg);
+          handleSetPasswordChangeSuccessful(true);
+        })
+        .catch((err) => {
+          console.log("Login failed:", err);
+          updateFormStatus(
+            false,
+            true,
+            "There was an error changing your password. Please try again."
+          );
           handleSetPasswordChangeSuccessful(false);
-          return;
-        }
-        handleSetPasswordChangeSuccessful(true);
-      })
-      .catch((err) => {
-        console.log("Login failed:", err);
-        updateFormStatus(false, true, "There was an error changing your password. Please try again.");
-        handleSetPasswordChangeSuccessful(false);
-      });
+        });
     }
-  }
+  };
 
   return (
     <form
@@ -83,12 +102,15 @@ const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) 
       onSubmit={(e) => handleSubmit(e)}
     >
       <InputField
+        autoFocus
         label="Current password"
         id="current-password"
         placeholder="current password"
         type="password"
         value={inputs.currentPassword}
-        onChange={e => setInputs({ ...inputs, currentPassword: e.target.value })}
+        onChange={(e) =>
+          setInputs({ ...inputs, currentPassword: e.target.value })
+        }
       />
       <InputField
         label="New password"
@@ -96,7 +118,7 @@ const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) 
         placeholder="new password"
         type="password"
         value={inputs.newPassword}
-        onChange={e => setInputs({ ...inputs, newPassword: e.target.value })}
+        onChange={(e) => setInputs({ ...inputs, newPassword: e.target.value })}
       />
       <InputField
         label="Confirm new password"
@@ -104,23 +126,25 @@ const ChangePasswordForm = ({ handleClose, handleSetPasswordChangeSuccessful }) 
         placeholder="confirm new password"
         value={inputs.confirmPassword}
         type="password"
-        onChange={e => setInputs({ ...inputs, confirmPassword: e.target.value })}
+        onChange={(e) =>
+          setInputs({ ...inputs, confirmPassword: e.target.value })
+        }
       />
       <div className="flex change-password-submit-section">
-        <button type="reset" className="btn secondary blue" onClick={handleClose}>
+        <button
+          type="reset"
+          className="btn secondary blue"
+          onClick={handleClose}
+        >
           Cancel
         </button>
         <button className="btn primary" type="submit">
           Change Password
         </button>
-        {err.status &&
-          <p className="change-password-error">
-            {err.message}
-          </p>
-        }
+        {err.status && <p className="change-password-error">{err.message}</p>}
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default ChangePasswordForm;
