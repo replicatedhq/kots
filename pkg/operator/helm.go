82316f8ec6a6d6e3470b1d853b1430e15e89908a
Alexander J Trelore <alextrelore@gmail.com>
2022-06-17 20:55:42 +0100

    correct the error we're checking

--- pkg/operator/helm.go
+++ pkg/operator/helm.go
@@ -89,7 +89,7 @@ func renderChartsArchive(deployedVersionArchive string, downstreamName string, k
 
 			if info.Name() == "kustomization.yaml" {
 				_, err = os.Stat(filepath.Join(sourceChartsDir, relPath))
-				if os.IsExist(err) {
+				if !os.IsNotExist(err) {
 					archiveChartOutput, err := exec.Command(kustomizeBinPath, "build", filepath.Dir(path)).Output()
 					if err != nil {
 						if ee, ok := err.(*exec.ExitError); ok {
