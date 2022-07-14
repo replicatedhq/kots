223dfd81bc6b0b45163fdef1eb98dc82dad1bc33
Craig O'Donnell <craig@replicated.com>
2022-07-14 15:30:50 +0000

    chore: automated kubectl version pr

---
+++ actions/kubectl-versions/index.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch');
+const fs = require('fs')
+
+
+async function updateVersions() {
+    // Fetch supported Kubernetes version info
+    const response = await fetch("https://endoflife.date/api/kubernetes.json");
+    const releases = await response.json();
+
+    const supported = releases.filter(release => new Date(release.eol) > new Date()).reverse();
+
+    // Create Dockerfile command text
+    let i = 0
+    for (const v of supported) {
+        const shaResponse = await fetch(`https://dl.k8s.io/release/v${v.latest}/bin/linux/amd64/kubectl.sha256`);
+        const shasum = await shaResponse.text();
+        v.install = `# Install Kubectl ${v.cycle}
+ENV KUBECTL_${v.cycle.replace(".", "_")}_VERSION=v${v.latest}
+ENV KUBECTL_${v.cycle.replace(".", "_")}_URL=https://dl.k8s.io/release/${"${KUBECTL_" + v.cycle.replace(".", "_") + "_VERSION}"}/bin/linux/amd64/kubectl
+ENV KUBECTL_${v.cycle.replace(".", "_")}_SHA256SUM=${shasum}
+RUN curl -fsSLO "${"${KUBECTL_" + v.cycle.replace(".", "_") + "_URL}"}" \\
+  && echo "${"${KUBECTL_" + v.cycle.replace(".", "_") + "_SHA256SUM}"} kubectl" | sha256sum -c - \\
+  && chmod +x kubectl \\
+  && mv kubectl "${"${KOTS_KUBECTL_BIN_DIR}"}/kubectl-v${v.cycle}"`;
+        if (i === supported.length - 1) {
+            v.install = v.install + ` \\
+  && ln -s "${"${KOTS_KUBECTL_BIN_DIR}"}/kubectl-v1.24" "${"${KOTS_KUBECTL_BIN_DIR}"}/kubectl"`
+        }
+        i++
+    }
+
+    const commandText = supported.map(s => s.install).join("\n\n");
+
+    // Insert command text into template and overwrite Dockerfile
+    fs.readFile("actions/kubectl-versions/template/Dockerfile.template", 'utf8', function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        var result = data.replace(/__KUBECTL_VERSIONS__/g, commandText);
+
+        fs.writeFile("deploy/Dockerfile", result, 'utf8', function (err) {
+            if (err) return console.error(err);
+        });
+    });
+}
+
+updateVersions();
