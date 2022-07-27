2e67bed9bc20ec2eb8799544cfcb881f0174c69d
miaawong <miawongdev@gmail.com>
2022-07-27 14:48:44 -0700

    fix logic

--- web/src/components/config_render/ConfigUtil.js
+++ web/src/components/config_render/ConfigUtil.js
@@ -1,13 +1,13 @@
 export const setOrder = (index, affix) => {
   if (affix === "left") {
-    if (index % 2 !== 0) {
+    if (index === 1) {
       return index;
     }
-    return index - 2;
+    return index - 1;
   }
   if (affix === "right") {
-    if (index % 2 === 0) {
-      return index;
+    if (index === 2) { 
+      return index
     }
     return index + 1;
   }
