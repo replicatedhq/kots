4f40ffccc29914c16c2c6365cf5ae0ab4bf11c69
Craig O'Donnell <craig@replicated.com>
2022-08-01 15:28:17 -0400

    fix: remove specific namespaces from backup if wildcard is specified (#3014)
    * fix: remove specific namespace references from backup if wildcard is specified
    
    * add test for duplicates and unordered list

---
+++ pkg/kotsadmsnapshot/backup_test.go
@@ -0,0 +1,75 @@
+package snapshot
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrepareIncludedNamespaces(t *testing.T) {
+	tests := []struct {
+		name       string
+		namespaces []string
+		want       []string
+	}{
+		{
+			name:       "empty",
+			namespaces: []string{},
+			want:       []string{},
+		},
+		{
+			name:       "single",
+			namespaces: []string{"test"},
+			want:       []string{"test"},
+		},
+		{
+			name:       "multiple",
+			namespaces: []string{"test", "test2"},
+			want:       []string{"test", "test2"},
+		},
+		{
+			name:       "multiple ignore order",
+			namespaces: []string{"test", "test2"},
+			want:       []string{"test2", "test"},
+		},
+		{
+			name:       "duplicates",
+			namespaces: []string{"test", "test2", "test"},
+			want:       []string{"test", "test2"},
+		},
+		{
+			name:       "multiple with empty string",
+			namespaces: []string{"test", "", "test2"},
+			want:       []string{"test", "test2"},
+		},
+		{
+			name:       "single wildcard",
+			namespaces: []string{"*"},
+			want:       []string{"*"},
+		},
+		{
+			name:       "wildcard with specific",
+			namespaces: []string{"*", "test"},
+			want:       []string{"*"},
+		},
+		{
+			name:       "wildcard with empty string",
+			namespaces: []string{"*", ""},
+			want:       []string{"*"},
+		},
+		{
+			name:       "wildcard with empty string and specific",
+			namespaces: []string{"*", "", "test"},
+			want:       []string{"*"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := prepareIncludedNamespaces(tt.namespaces)
+			if !assert.ElementsMatch(t, tt.want, got) {
+				t.Errorf("prepareIncludedNamespaces() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
