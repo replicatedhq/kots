271c09f4b47e0580fc261f34f486b295af929ddc
Rian Fowler <rian@replicated.com>
2022-08-06 19:11:15 -0500

    fix renderdiff

--- web/src/features/AppVersionHistory/AppVersionHistoryRow.jsx
+++ web/src/features/AppVersionHistory/AppVersionHistoryRow.jsx
@@ -43,7 +43,7 @@ const AppVersionHistoryRow = ({
   isNew,
   newPreflightResults,
   showReleaseNotes,
-  renderDiffProp,
+  renderDiff: renderDiffProp,
   toggleShowDetailsModal,
   gitopsEnabled,
   deployVersion,
