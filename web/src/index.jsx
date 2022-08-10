c8e03201bc21a87f44380f6f74b6412ca68308fe
Rian Fowler <rian@replicated.com>
2022-08-10 13:36:13 -0500

    react 18 upgrade

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,11 +1,13 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 import ReplicatedErrorBoundary from "./components/shared/ErrorBoundary";
 import Root from "./Root";
 
-ReactDOM.render(
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
   <ReplicatedErrorBoundary>
     <Root />
-  </ReplicatedErrorBoundary>,
-  document.getElementById("app")
+  </ReplicatedErrorBoundary>
 );
