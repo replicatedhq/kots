71694a65c93b7f1f3a89280d0cffc510feeabcf4
miaawong <miawongdev@gmail.com>
2022-08-26 11:54:09 -0700

    update registry settings ui

--- web/src/components/apps/AppRegistrySettings.jsx
+++ web/src/components/apps/AppRegistrySettings.jsx
@@ -11,8 +11,8 @@ export default class AppRegistrySettings extends Component {
         <Helmet>
           <title>{`${app.name} Airgap settings`}</title>
         </Helmet>
-        <div className="AirgapSettings--wrapper u-textAlign--left u-paddingRight--20 u-paddingLeft--20">
-          <p className="u-fontWeight--bold u-textColor--primary u-fontSize--larger u-marginTop--30 u-marginBottom--20 u-paddingBottom--5 u-lineHeight--normal">
+        <div className="AirgapSettings--wrapper u-textAlign--left u-marginTop--30 u-paddingRight--20 u-paddingLeft--20">
+          <p className="u-fontWeight--bold u-textColor--primary u-fontSize--larger u-marginTop--15 u-marginBottom--10 u-lineHeight--normal">
             Registry settings
           </p>
           <AirgapRegistrySettings app={app} updateCallback={updateCallback} />
