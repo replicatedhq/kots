9b91788b8c98c2d230813149c14f503a7939d2cb
Salah Al Saleh <sg.alsaleh@gmail.com>
2022-08-26 17:46:42 -0700

    Don't validate JWT tokens when checking registry access (#3109)
    * Don't validate JWT tokens when checking registry access

--- pkg/docker/registry/auth.go
+++ pkg/docker/registry/auth.go
@@ -6,19 +6,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
-	"os"
 	"strings"
 
 	"github.com/containers/image/v5/pkg/docker/config"
 	"github.com/containers/image/v5/types"
 	"github.com/distribution/distribution/v3/registry/client/auth/challenge"
 	"github.com/pkg/errors"
-	"github.com/replicatedhq/kots/pkg/logger"
 	"github.com/replicatedhq/kots/pkg/util"
 )
-
-type ScopeAction string
 
 var (
 	insecureClient = &http.Client{
@@ -31,11 +26,6 @@ var (
 	}
 )
 
-const (
-	ActionPull ScopeAction = "pull"
-	ActionPush ScopeAction = "push"
-)
-
 func LoadAuthForRegistry(endpoint string) (string, string, error) {
 	sys := &types.SystemContext{DockerDisableV1Ping: true}
 	username, password, err := config.GetAuthentication(sys, endpoint)
@@ -46,23 +36,8 @@ func LoadAuthForRegistry(endpoint string) (string, string, error) {
 	return username, password, nil
 }
 
-func CheckAccess(endpoint, username, password, org string, requestedAction ScopeAction) error {
-
+func CheckAccess(endpoint, username, password string) error {
 	endpoint = sanitizeEndpoint(endpoint)
-
-	// We need to check if we can push images to a repo.
-	// We cannot get push permission to an org alone.
-	scope := org + "/testrepo"
-	basicAuthToken := makeBasicAuthToken(username, password)
-
-	if IsECREndpoint(endpoint) && username != "AWS" {
-		token, err := GetECRBasicAuthToken(endpoint, username, password)
-		if err != nil {
-			return errors.Wrap(err, "failed to ping registry")
-		}
-		basicAuthToken = token
-		scope = org // ECR has no concept of organization and it should be an empty string
-	}
 
 	pingURL := fmt.Sprintf("https://%s/v2/", endpoint)
 	resp, err := insecureClient.Get(pingURL)
@@ -94,18 +69,21 @@ func CheckAccess(endpoint, username, password, org string, requestedAction Scope
 		return nil
 	}
 
-	host := challenges[0].Parameters["realm"]
-	v := url.Values{}
-	v.Set("service", challenges[0].Parameters["service"])
-	v.Set("scope", fmt.Sprintf("repository:%s:%s", scope, requestedAction))
+	authURL := challenges[0].Parameters["realm"]
+	basicAuthToken := makeBasicAuthToken(username, password)
 
-	authURL := host + "?" + v.Encode()
+	if IsECREndpoint(endpoint) && username != "AWS" {
+		token, err := GetECRBasicAuthToken(endpoint, username, password)
+		if err != nil {
+			return errors.Wrap(err, "failed to ping registry")
+		}
+		basicAuthToken = token
+	}
 
 	req, err := util.NewRequest("GET", authURL, nil)
 	if err != nil {
 		return errors.Wrap(err, "failed to create auth request")
 	}
-
 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", basicAuthToken))
 
 	resp, err = insecureClient.Do(req)
@@ -123,43 +101,10 @@ func CheckAccess(endpoint, username, password, org string, requestedAction Scope
 		return errors.New(errorResponseToString(resp.StatusCode, authBody))
 	}
 
-	bearerToken, err := newBearerTokenFromJSONBlob(authBody)
-	if err != nil {
-		// not a fatal error - some registries don't return JWTs
-		logger.NewCLILogger(os.Stdout).Info("failed to parse registry auth bearer token, continuing anyways: %s", err.Error())
-		return nil
-	}
+	// We don't validate JWT tokens here because some registries don't return any,
+	// and some registries return JWT tokens without access scopes.
 
-	jwtToken, err := bearerToken.getJwtToken()
-	if err != nil {
-		// not a fatal error - some registries don't return JWTs
-		logger.NewCLILogger(os.Stdout).Info("failed to parse registry auth jwt, continuing anyways: %s", err.Error())
-		return nil
-	}
-
-	claims, err := getJwtTokenClaims(jwtToken)
-	if err != nil {
-		// not a fatal error - some registries don't return JWTs
-		logger.NewCLILogger(os.Stdout).Info("failed to find registry auth claims in jwt, continuing anyways: %s", err.Error())
-		return nil
-	}
-
-	// If requested access is "push", we need to check that we actually got it
-	for _, access := range claims.Access {
-		if access.Type != "repository" {
-			continue
-		}
-		if access.Name != scope {
-			continue
-		}
-		for _, action := range access.Actions {
-			if action == string(requestedAction) {
-				return nil
-			}
-		}
-	}
-
-	return errors.Errorf("%q has no %s permission in %q", username, requestedAction, org)
+	return nil
 }
 
 func makeBasicAuthToken(username, password string) string {
