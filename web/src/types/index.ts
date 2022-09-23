45ff53cf0b3edc32144658271fe79a3cccb3f9c2
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2022-09-23 10:09:14 -0500

    Rianfowler/sc 58965/adding typechecking to dashboard component (#3212)
    * move dashboard
    
    * start adding types to dashboard;
    
    * more typechecks
    
    * more typechecks and linting fixes
    
    * fix type-o
    
    * Update web/src/components/apps/Dashboard.tsx

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -1,18 +1,42 @@
 export type App = {
   allowSnapshots: boolean;
+  autoDeploy: boolean;
   chartPath: string;
   credentials: Credentials;
   currentSequence: number;
-  downstream: DownStream;
+  downstream: Downstream;
   hasPreflight: boolean;
+  id: string;
+  iconUri: string;
+  isAirgap: boolean;
   isConfigurable: boolean;
   isGeoaxisSupported: boolean;
   isGitOpsSupported: boolean;
   isIdentityServiceSupported: boolean;
+  isSemverRequired: boolean;
   name: string;
   namespace: string;
   needsRegistry?: boolean;
   slug: string;
+  updateCheckerSpec: string;
+};
+
+export type AppLicense = {
+  assignee: string;
+  channelName: string;
+  entitlements: string[];
+  expiresAt: string;
+  id: string;
+  isAirgapSupported: boolean;
+  isGeoaxisSupported: boolean;
+  isGitOpsSupported: boolean;
+  isIdentityServiceSupported: boolean;
+  isSemverRequired: boolean;
+  isSnapshotsSupported: boolean;
+  isSupportBundleUploadSupported: boolean;
+  lastSyncedAt: string;
+  licenseSequence: number;
+  licenseType: string;
 };
 
 export type Credentials = {
@@ -20,9 +44,32 @@ export type Credentials = {
   password: string;
 };
 
-export type DownStream = {
+export type ResourceStates = {
+  kind: string;
+  name: string;
+  namespace: string;
+  // from https://github.com/replicatedhq/kots/blob/84b7e4e0e9275bb200a36be69691c4944eb8cf8f/pkg/appstate/types/types.go#L10-L14
+  state: "ready" | "updating" | "degrading" | "unavailable" | "missing";
+};
+
+type AppStatus = {
+  appId: string;
+  resourceStates: ResourceStates[];
+  sequence: number;
+  state: string;
+  updatedAt: string;
+};
+
+export type DashboardResponse = {
+  appStatus: AppStatus | null;
+  metrics: string[];
+  prometheusAddress: string;
+};
+
+export type Downstream = {
   currentVersion: Version;
   gitops: GitOps;
+  links: DashboardActionLink[];
   pendingVersions: Version[];
 };
 
@@ -34,6 +81,11 @@ export type GitOps = {
 export type KotsParams = {
   sequence: string;
   slug: string;
+};
+
+export type DashboardActionLink = {
+  title: string;
+  uri: string;
 };
 
 export type Version = {
