775c4ea8c79e9dd68097ce5d5092db0fd9a441f5
Ethan Mosbaugh <ethan@replicated.com>
2022-09-26 21:55:55 +0000

    Fix kurl-addon-publish-action

--- .github/actions/kurl-addon-kots-publisher/index.js
+++ .github/actions/kurl-addon-kots-publisher/index.js
@@ -8,6 +8,7 @@ const addonPackageUrl = getInput('ADDON_PACKAGE_URL');
 const githubToken = getInput('GITHUB_TOKEN');
 const github = getOctokit(githubToken);
 const client = new HttpClient();
+
 const latestKurlVersion = await github.rest.repos.getLatestRelease({
   owner: 'replicatedhq',
   repo: 'kurl'
@@ -15,10 +16,26 @@ const latestKurlVersion = await github.rest.repos.getLatestRelease({
 const kotsAddonVersions = await client.get('https://kots-kurl-addons-production-1658439274.s3.amazonaws.com/versions.json')
   .then(response => response.readBody())
   .then(response => JSON.parse(response));
-kotsAddonVersions.unshift({
+
+const appendVersion = (kotsAddonVersions, version) => {
+  kotsAddonVersions = kotsAddonVersions.filter(el => el.version !== version.version);
+  kotsAddonVersions.unshift(version);
+  return kotsAddonVersions.sort((a, b) => {
+    if (a.version < b.version) {
+      return -1;
+    } else if (a.version > b.version) {
+      return 1;
+    }
+    return 0;
+  }).reverse();
+};
+
+kotsAddonVersions = appendVersion(kotsAddonVersions, {
   version: addonVersion,
   url: addonPackageUrl,
+  // Be careful when regenerating the kURL add-on package as the kurlVersionCompatibilityRange
+  // will be updated to the latest kURL version.
   kurlVersionCompatibilityRange: `>= ${latestKurlVersion.data.tag_name}`,
 });
 
-fs.writeFile("./deploy/kurl/versions.json", JSON.stringify(kotsAddonVersions));
+fs.writeFile('./deploy/kurl/versions.json', JSON.stringify(kotsAddonVersions));
