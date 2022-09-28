42317152aa57c30b4c1a07cdb30b550cfa7653a0
Rian Fowler <rian@replicated.com>
2022-09-28 14:05:37 -0500

    fix linting

--- web/src/components/UploadAirgapBundle.jsx
+++ web/src/components/UploadAirgapBundle.jsx
@@ -456,10 +456,7 @@ class UploadAirgapBundle extends React.Component {
 
     return (
       <div className="UploadLicenseFile--wrapper container flex-column u-overflow--auto u-marginTop--auto u-marginBottom--auto alignItems--center">
-        <KotsPageTitle
-          pageName="Air Gap Installation"
-          showAppSlug
-        />
+        <KotsPageTitle pageName="Air Gap Installation" showAppSlug />
         <div className="LoginBox-wrapper u-flexTabletReflow flex-auto u-marginTop--20 u-marginBottom--5">
           <div className="flex-auto flex-column login-form-wrapper secure-console justifyContent--center">
             <div className="flex-column alignItems--center">
