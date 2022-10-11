f14dc58cf276ce9449a50a768dbd4c650a7bad87
Rian Fowler <rian@replicated.com>
2022-10-11 11:24:15 -0500

    fix updates available state setting

--- web/src/components/apps/AppVersionHistory.tsx
+++ web/src/components/apps/AppVersionHistory.tsx
@@ -263,16 +263,14 @@ class AppVersionHistory extends Component<Props, State> {
     ) {
       this.fetchKotsDownstreamHistory();
     }
-    if (
-      this.props.app?.downstream &&
-      this.props.app?.downstream !== lastProps.app?.downstream
-    ) {
-      if (this.props.app.downstream.pendingVersions.length > 0) {
+      if (this.props.app.downstream.pendingVersions.length > 0 &&
+        this.state.updatesAvailable === false) {
         this.setState({ updatesAvailable: true });
-      } else {
+      }
+      if (this.props.app.downstream.pendingVersions.length === 0 &&
+        this.state.updatesAvailable === true) {
         this.setState({ updatesAvailable: false });
       }
-    }
   };
 
   componentWillUnmount() {
