6375020353a26a3c0c29f8a1aaa2f6435113c9e0
Rian Fowler <rian@replicated.com>
2022-10-17 16:36:56 -0500

    make dashboard a functional component

--- web/src/features/Dashboard/index.ts
+++ web/src/features/Dashboard/index.ts
@@ -1,4 +1,4 @@
-import Dashboard from "./components/Dashboard";
+import { Dashboard } from "./components/Dashboard";
 import LicenseFields from "./components/LicenseFields";
 import DashboardGitOpsCard from "./components/DashboardGitOpsCard";
 
