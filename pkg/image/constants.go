1c79e2a959ee413b1479f6ff13b3fbcc894bbe0c
Craig O'Donnell <craig@replicated.com>
2022-10-18 16:14:48 +0000

    update lvp and minio image deps

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,10 +5,10 @@ package image
 // image name.
 
 const (
-	Minio      = "minio/minio:RELEASE.2022-10-08T20-11-00Z"
+	Minio      = "minio/minio:RELEASE.2022-10-15T19-57-03Z"
 	Postgres10 = "postgres:10.22-alpine"
 	Postgres14 = "postgres:14.5-alpine"
 	Dex        = "ghcr.io/dexidp/dex:v2.35.3"
 	Schemahero = "schemahero/schemahero:0.13.5"
-	Lvp        = "replicated/local-volume-provider:v0.3.9"
+	Lvp        = "replicated/local-volume-provider:v0.3.10"
 )
