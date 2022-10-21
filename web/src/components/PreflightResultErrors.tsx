c7268df0c7980f9aeb10b850927734fe988452f1
Rian Fowler <rian@replicated.com>
2022-10-21 10:50:16 -0500

    update result errors

--- web/src/components/PreflightResultErrors.tsx
+++ web/src/components/PreflightResultErrors.tsx
@@ -1,70 +1,57 @@
-import get from "lodash/get";
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { useEffect, useReducer } from "react";
+import { useRouteMatch } from "react-router";
 import MonacoEditor from "@monaco-editor/react";
 import CodeSnippet from "./shared/CodeSnippet";
 import ErrorModal from "./modals/ErrorModal";
 import { Utilities } from "../utilities/utilities";
 import "../scss/components/PreflightCheckPage.scss";
 
-class PreflightResultErrors extends Component {
-  state = {
-    command: null,
-    showErrorDetails: false,
-    errorTitle: "",
-    errorMsg: "",
-    displayErrorModal: false,
-  };
+import { KotsParams } from "@types";
 
-  async componentDidMount() {
-    if (!this.props.preflightResultData) {
-      return;
+type Props = {
+  ignorePermissionErrors: () => void;
+  logo: string;
+  preflightResultData: PreflightResultData | null;
+  valueFromAPI: string;
+};
+
+type State = {
+  command: string | null;
+  displayErrorModal: boolean;
+  errorTitle: string;
+  errorMsg: string;
+  showErrorDetails: boolean;
+};
+
+type PreflightResultData = {
+  appSlug: string;
+  sequence: number;
+};
+// class PreflightResultErrors extends Component {
+const PreflightResultErrors = (props: Props) => {
+  const [state, setState] = useReducer(
+    (currentState: State, newState: Partial<State>) => ({
+      ...currentState,
+      ...newState,
+    }),
+    {
+      command: null,
+      showErrorDetails: false,
+      errorTitle: "",
+      errorMsg: "",
+      displayErrorModal: false,
     }
-    this.getPreflightCommand();
-  }
+  );
+  const match = useRouteMatch<KotsParams>();
 
-  componentDidUpdate(prevProps) {
-    if (
-      !this.props.preflightResultData ||
-      (get(this.props, "preflightResultData.appSlug") ===
-        get(prevProps, "preflightResultData.appSlug") &&
-        get(this.props, "preflightResultData.sequence") ===
-          get(prevProps, "preflightResultData.sequence"))
-    ) {
-      return;
-    }
-    this.getPreflightCommand();
-  }
+  const [previousAppSlug, setPreviousAppSlug] = React.useState<
+    string | undefined
+  >(props?.preflightResultData?.appSlug);
+  const [previousSequence, setPreviousSequence] = React.useState<
+    number | undefined
+  >(props?.preflightResultData?.sequence);
 
-  toggleShowErrorDetails = () => {
-    this.setState({
-      showErrorDetails: !this.state.showErrorDetails,
-    });
-  };
-
-  getPreflightCommand = async () => {
-    const { match, preflightResultData } = this.props;
-    const sequence = match.params.sequence
-      ? parseInt(match.params.sequence, 10)
-      : 0;
-    try {
-      const command = await this.fetchPreflightCommand(
-        preflightResultData.appSlug,
-        sequence
-      );
-      this.setState({
-        command,
-      });
-    } catch (err) {
-      this.setState({
-        errorTitle: `Failed to get preflight command`,
-        errorMsg: err ? err.message : "Something went wrong, please try again.",
-        displayErrorModal: true,
-      });
-    }
-  };
-
-  fetchPreflightCommand = async (slug, sequence) => {
+  const fetchPreflightCommand = async (slug: string, sequence: number) => {
     const res = await fetch(
       `${process.env.API_ENDPOINT}/app/${slug}/sequence/${sequence}/preflightcommand`,
       {
@@ -85,110 +72,168 @@ class PreflightResultErrors extends Component {
     return response.command;
   };
 
-  toggleErrorModal = () => {
-    this.setState({ displayErrorModal: !this.state.displayErrorModal });
+  const getPreflightCommand = async () => {
+    const { preflightResultData } = props;
+    const sequence = match.params.sequence
+      ? parseInt(match.params.sequence, 10)
+      : 0;
+    try {
+      const command = await fetchPreflightCommand(
+        preflightResultData?.appSlug || "",
+        sequence
+      );
+      setState({
+        command,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        setState({
+          errorTitle: `Failed to get preflight command`,
+          errorMsg: err
+            ? err.message
+            : "Something went wrong, please try again.",
+          displayErrorModal: true,
+        });
+        return;
+      }
+      setState({
+        errorTitle: `Failed to get preflight command`,
+        errorMsg: "Something went wrong, please try again.",
+        displayErrorModal: true,
+      });
+    }
   };
 
-  render() {
-    const { valueFromAPI, logo } = this.props;
-    const { errorTitle, errorMsg, displayErrorModal, command } = this.state;
+  useEffect(() => {
+    if (!props.preflightResultData) {
+      return;
+    }
+    getPreflightCommand();
+  }, []);
 
-    return (
-      <div className="flex flex1 flex-column">
-        <div className="flex flex1 u-height--full u-width--full u-marginTop--5 u-marginBottom--20">
-          <div className="flex-column u-width--full u-overflow--hidden u-paddingTop--30 u-paddingBottom--5 alignItems--center justifyContent--center">
-            <div className="PreChecksBox-wrapper flex-column u-padding--20">
-              <div className="flex">
-                {logo && (
-                  <div className="flex-auto u-marginRight--10">
-                    <div
-                      className="watch-icon"
-                      style={{
-                        backgroundImage: `url(${logo})`,
-                        width: "36px",
-                        height: "36px",
-                      }}
-                    ></div>
-                  </div>
-                )}
-                <h2 className="u-fontSize--largest u-textColor--primary u-fontWeight--bold u-lineHeight--normal">
-                  Unable to automatically run preflight checks
-                </h2>
-              </div>
-              <p className="u-marginTop--10 u-marginBottom--10 u-fontSize--normal u-lineHeight--normal u-textColor--bodyCopy u-fontWeight--normal">
-                The Kubernetes RBAC policy that the Admin Console is running
-                with does not have access to complete the Preflight Checks. It’s
-                recommended that you run these manually before proceeding.
-              </p>
-              <p
-                className="replicated-link u-fontSize--normal u-marginBottom--10"
-                onClick={this.toggleShowErrorDetails}
-              >
-                {this.state.showErrorDetails ? "Hide details" : "Show details"}
-              </p>
-              {this.state.showErrorDetails && (
-                <div className="flex-column flex flex1 monaco-editor-wrapper u-border--gray">
-                  <MonacoEditor
-                    language="bash"
-                    value={valueFromAPI}
-                    height="300px"
-                    options={{
-                      readOnly: true,
-                      contextmenu: false,
-                      minimap: {
-                        enabled: false,
-                      },
-                      scrollBeyondLastLine: false,
+  useEffect(() => {
+    if (!props.preflightResultData) {
+      return;
+    }
+
+    // TODO: determine if it's actually necessary to track the previous props
+    if (
+      previousAppSlug !== props.preflightResultData.appSlug ||
+      previousSequence !== props.preflightResultData.sequence
+    ) {
+      getPreflightCommand();
+    }
+    setPreviousAppSlug(props.preflightResultData.appSlug);
+    setPreviousSequence(props.preflightResultData.sequence);
+  }, [props.preflightResultData]);
+
+  const toggleShowErrorDetails = () => {
+    setState({
+      showErrorDetails: !state.showErrorDetails,
+    });
+  };
+
+  const toggleErrorModal = () => {
+    setState({ displayErrorModal: !state.displayErrorModal });
+  };
+
+  const { valueFromAPI, logo } = props;
+  const { errorTitle, errorMsg, displayErrorModal, command } = state;
+
+  return (
+    <div className="flex flex1 flex-column">
+      <div className="flex flex1 u-height--full u-width--full u-marginTop--5 u-marginBottom--20">
+        <div className="flex-column u-width--full u-overflow--hidden u-paddingTop--30 u-paddingBottom--5 alignItems--center justifyContent--center">
+          <div className="PreChecksBox-wrapper flex-column u-padding--20">
+            <div className="flex">
+              {logo && (
+                <div className="flex-auto u-marginRight--10">
+                  <div
+                    className="watch-icon"
+                    style={{
+                      backgroundImage: `url(${logo})`,
+                      width: "36px",
+                      height: "36px",
                     }}
-                  />
+                  ></div>
                 </div>
               )}
-              <div className="u-marginTop--20">
-                <h2 className="u-fontSize--largest u-textColor--primary u-fontWeight--bold u-lineHeight--normal">
-                  Run Preflight Checks Manually
-                </h2>
-                <p className="u-fontSize--normal u-textColor--bodyCopy u-lineHeight--normal u-marginBottom--20">
-                  Run the commands below from your workstation to complete the
-                  Preflight Checks.
-                </p>
-                {command ? (
-                  <CodeSnippet
-                    language="bash"
-                    canCopy={true}
-                    onCopyText={
-                      <span className="u-textColor--success">
-                        Command has been copied to your clipboard
-                      </span>
-                    }
-                  >
-                    {command}
-                  </CodeSnippet>
-                ) : null}
+              <h2 className="u-fontSize--largest u-textColor--primary u-fontWeight--bold u-lineHeight--normal">
+                Unable to automatically run preflight checks
+              </h2>
+            </div>
+            <p className="u-marginTop--10 u-marginBottom--10 u-fontSize--normal u-lineHeight--normal u-textColor--bodyCopy u-fontWeight--normal">
+              The Kubernetes RBAC policy that the Admin Console is running with
+              does not have access to complete the Preflight Checks. It’s
+              recommended that you run these manually before proceeding.
+            </p>
+            <p
+              className="replicated-link u-fontSize--normal u-marginBottom--10"
+              onClick={toggleShowErrorDetails}
+            >
+              {state.showErrorDetails ? "Hide details" : "Show details"}
+            </p>
+            {state.showErrorDetails && (
+              <div className="flex-column flex flex1 monaco-editor-wrapper u-border--gray">
+                <MonacoEditor
+                  language="bash"
+                  value={valueFromAPI}
+                  height="300px"
+                  options={{
+                    readOnly: true,
+                    contextmenu: false,
+                    minimap: {
+                      enabled: false,
+                    },
+                    scrollBeyondLastLine: false,
+                  }}
+                />
               </div>
-              <div className="u-marginTop--30 flex justifyContent--flexEnd">
-                <span
-                  className="replicated-link u-marginLeft--20 u-fontSize--normal"
-                  onClick={this.props.ignorePermissionErrors}
+            )}
+            <div className="u-marginTop--20">
+              <h2 className="u-fontSize--largest u-textColor--primary u-fontWeight--bold u-lineHeight--normal">
+                Run Preflight Checks Manually
+              </h2>
+              <p className="u-fontSize--normal u-textColor--bodyCopy u-lineHeight--normal u-marginBottom--20">
+                Run the commands below from your workstation to complete the
+                Preflight Checks.
+              </p>
+              {command ? (
+                <CodeSnippet
+                  language="bash"
+                  canCopy={true}
+                  onCopyText={
+                    <span className="u-textColor--success">
+                      Command has been copied to your clipboard
+                    </span>
+                  }
                 >
-                  Proceed with limited Preflights
-                </span>
-              </div>
+                  {command}
+                </CodeSnippet>
+              ) : null}
+            </div>
+            <div className="u-marginTop--30 flex justifyContent--flexEnd">
+              <span
+                className="replicated-link u-marginLeft--20 u-fontSize--normal"
+                onClick={props.ignorePermissionErrors}
+              >
+                Proceed with limited Preflights
+              </span>
             </div>
           </div>
         </div>
-
-        {errorMsg && (
-          <ErrorModal
-            errorModal={displayErrorModal}
-            toggleErrorModal={this.toggleErrorModal}
-            err={errorTitle}
-            errMsg={errorMsg}
-            appSlug={this.props.appSlug}
-          />
-        )}
       </div>
-    );
-  }
-}
 
-export default withRouter(PreflightResultErrors);
+      {errorMsg && (
+        <ErrorModal
+          errorModal={displayErrorModal}
+          toggleErrorModal={toggleErrorModal}
+          err={errorTitle}
+          errMsg={errorMsg}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PreflightResultErrors;
