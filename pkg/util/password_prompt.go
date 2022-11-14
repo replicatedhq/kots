43add18d27431c227a6b67ccfa444ab90cdbe771
Alexander <alextrelore@gmail.com>
2022-11-14 16:17:08 +0000

    shows password complexity rules for resetting kotsadm password (#3411)

---
+++ pkg/util/password_prompt.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"os"
+
+	"github.com/manifoldco/promptui"
+	"github.com/pkg/errors"
+)
+
+func PromptForNewPassword() (string, error) {
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{ . | bold }} ",
+		Valid:   "{{ . | green }} ",
+		Invalid: "{{ . | red }} ",
+		Success: "{{ . | bold }} ",
+	}
+
+	prompt := promptui.Prompt{
+		Label:     "Enter a new password for the admin console (6+ characters):",
+		Templates: templates,
+		Mask:      rune('•'),
+		Validate: func(input string) error {
+			if len(input) < 6 {
+				return errors.New("please enter a longer password")
+			}
+
+			return nil
+		},
+	}
+
+	for {
+		result, err := prompt.Run()
+		if err != nil {
+			if err == promptui.ErrInterrupt {
+				os.Exit(-1)
+			}
+			continue
+		}
+
+		return result, nil
+	}
+}
