056a866b860fb03e0d9ae6b58f4e19c5aae31795
miaawong <miawongdev@gmail.com>
2022-11-22 17:00:04 -0800

    wip

--- web/src/components/config_render/ConfigRadio.jsx
+++ web/src/components/config_render/ConfigRadio.jsx
@@ -1,50 +1,37 @@
-import React from "react";
-import get from "lodash/get";
+import React from 'react';
+import get from 'lodash/get';
+import { RadioButton } from 'replicated-design-system';
 
 export default class ConfigRadio extends React.Component {
   handleOnChange = (e) => {
     const { group } = this.props;
     if (
       this.props.handleChange &&
-      typeof this.props.handleChange === "function"
+      typeof this.props.handleChange === 'function'
     ) {
       this.props.handleChange(group, e.target.value);
     }
   };
 
   render() {
-    let val = get(this.props, "value");
+    let val = get(this.props, 'value');
     if (!val || val.length === 0) {
       val = this.props.default;
     }
     const checked = val === this.props.name;
 
     return (
-      <div
-        id={`${this.props.name}-group`}
-        className="flex alignItems--center u-marginRight--20 u-marginTop--15"
-      >
-        <input
-          type="radio"
-          name={this.props.group}
-          id={`${this.props.group}-${this.props.name}`}
-          value={this.props.name}
-          checked={checked}
-          disabled={this.props.readOnly}
-          onChange={(e) => this.handleOnChange(e)}
-          className={`${this.props.className || ""} ${
-            this.props.readOnly ? "readonly" : ""
-          }`}
-        />
-        <label
-          htmlFor={`${this.props.group}-${this.props.name}`}
-          className={`u-marginLeft--5 header-color field-section-sub-header u-userSelect--none ${
-            this.props.readOnly ? "u-cursor--default" : "u-cursor--pointer"
-          }`}
-        >
-          {this.props.title}
-        </label>
-      </div>
+      <RadioButton
+        name={this.props.name}
+        divClassName="u-marginRight--20 u-marginTop--15"
+        group={this.props.group}
+        checked={checked}
+        title={this.props.title}
+        disabled={this.props.readOnly}
+        inputClassName={this.props.className}
+        labelClassName="u-marginLeft--5 header-color field-section-sub-header u-userSelect--none"
+        onChange={this.handleOnChange}
+      />
     );
   }
 }
