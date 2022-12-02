dc4aa3c609c96c9973136a8d6948f9421d4242cb
miaawong <miawongdev@gmail.com>
2022-12-01 16:00:08 -0800

    upload support bundle file

--- web/src/components/troubleshoot/GenerateSupportBundleModal.tsx
+++ web/src/components/troubleshoot/GenerateSupportBundleModal.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import Modal from "react-modal";
 import CodeSnippet from "@components/shared/CodeSnippet";
 import { Utilities } from "@src/utilities/utilities";
 import { useHistory } from "react-router";
-import { App } from "@types";
+import { App, LicenseFile, KotsParams } from "@types";
+// @ts-ignore
+import Dropzone from "react-dropzone";
+import Icon from "@components/Icon";
+import isEmpty from "lodash/isEmpty";
+import randomstring from "randomstring";
+import { useRouteMatch } from "react-router";
 
 type Props = {
   isOpen: boolean;
@@ -12,19 +18,42 @@ type Props = {
   updateBundleSlug: (value: string) => void;
 };
 
+type State = {
+  bundleCommand: string;
+  fileUploading: boolean;
+  generateBundleErrMsg: string;
+  showGetBundleSpec: boolean;
+  supportBundle: LicenseFile | null;
+  uploadBundleErrMsg: string;
+};
+
 const GenerateSupportBundleModal = ({
   isOpen,
   toggleModal,
   watch,
   updateBundleSlug,
 }: Props) => {
-  const [showGetBundleSpec, setShowGetBundleSpec] = React.useState(false);
-  const [bundleCommand, setBundleCommand] = React.useState("");
+  const [state, setState] = useReducer(
+    (currentState: State, newState: Partial<State>) => ({
+      ...currentState,
+      ...newState,
+    }),
+    {
+      bundleCommand: "",
+      fileUploading: false,
+      generateBundleErrMsg: "",
+      showGetBundleSpec: false,
+      supportBundle: {} as LicenseFile,
+      uploadBundleErrMsg: "",
+    }
+  );
+
   const toggleShowGetBundleSpec = () => {
-    setShowGetBundleSpec(!showGetBundleSpec);
+    setState({ showGetBundleSpec: !state.showGetBundleSpec });
   };
-  const [generateBundleErrMsg, setGenerateBundleErrMsg] = React.useState("");
+
   const history = useHistory();
+  const match = useRouteMatch<KotsParams>();
 
   const fetchSupportBundleCommand = async () => {
     const res = await fetch(
@@ -44,7 +73,7 @@ const GenerateSupportBundleModal = ({
       throw new Error(`Unexpected status code: ${res.status}`);
     }
     const response = await res.json();
-    setBundleCommand(response.command);
+    setState({ bundleCommand: response.command });
   };
 
   useEffect(() => {
@@ -67,9 +96,9 @@ const GenerateSupportBundleModal = ({
     })
       .then(async (res) => {
         if (!res.ok) {
-          setGenerateBundleErrMsg(
-            `Unable to generate bundle: Status ${res.status}`
-          );
+          setState({
+            generateBundleErrMsg: `Unable to generate bundle: Status ${res.status}`,
+          });
         }
         const response = await res.json();
         updateBundleSlug(response.slug);
@@ -77,17 +106,70 @@ const GenerateSupportBundleModal = ({
         history.push(
           `/app/${watch?.slug}/troubleshoot/analyze/${response.slug}`
         );
-
-        setGenerateBundleErrMsg("");
+        setState({ generateBundleErrMsg: "" });
       })
       .catch((err) => {
         console.log(err);
 
-        setGenerateBundleErrMsg(
-          err ? err.message : "Something went wrong, please try again."
-        );
+        setState({
+          generateBundleErrMsg: err
+            ? err.message
+            : "Something went wrong, please try again.",
+        });
       });
   };
+
+  const onDrop = (files: LicenseFile[]) => {
+    setState({ supportBundle: files[0] });
+  };
+
+  const uploadAndAnalyze = async () => {
+    try {
+      const bundleId = randomstring.generate({ capitalization: "lowercase" });
+      const uploadBundleUrl = `${process.env.API_ENDPOINT}/troubleshoot/${watch?.id}/${bundleId}`;
+
+      setState({ fileUploading: true, uploadBundleErrMsg: "" });
+
+      const response = await fetch(uploadBundleUrl, {
+        method: "PUT",
+        body: state.supportBundle,
+        headers: {
+          "Content-Type": "application/tar+gzip",
+        },
+      });
+
+      if (!response.ok) {
+        setState({
+          fileUploading: false,
+          uploadBundleErrMsg: `Unable to upload the bundle: Status ${response.status}`,
+        });
+        return;
+      }
+
+      setState({ fileUploading: false, uploadBundleErrMsg: "" });
+      toggleModal();
+
+      const url = `/app/${match.params.slug}/troubleshoot/analyze/${bundleId}`;
+      history.push(url);
+    } catch (err) {
+      setState({
+        fileUploading: false,
+        uploadBundleErrMsg: err
+          ? `Unable to upload the bundle: ${(err as Error)?.message}`
+          : "Something went wrong, please try again.",
+      });
+    }
+  };
+
+  const clearFile = () => {
+    console.log("hello");
+    setState({
+      supportBundle: null,
+    });
+  };
+
+  const hasFile = state.supportBundle && !isEmpty(state.supportBundle);
+  console.log(hasFile);
 
   return (
     <Modal
@@ -111,10 +193,9 @@ const GenerateSupportBundleModal = ({
               style={{ maxWidth: "450px" }}
               className="u-fontSize--normal u-lineHeight--normal"
             >
-              Collect logs, resources and other data from the running
-              application and analyze them against a set of known problems in
-              Sentry Enterprise. Logs, cluster info and other data will not
-              leave your cluster.
+              Collect logs, resources and other data from the application and
+              analyze them against a set of known problems in Sentry Enterprise.
+              Data will not leave your cluster.
             </p>
             <div>
               <button
@@ -124,80 +205,135 @@ const GenerateSupportBundleModal = ({
               >
                 Analyze
               </button>
-              {generateBundleErrMsg && (
+              {state.generateBundleErrMsg && (
                 <p className="u-textColor--error u-marginTop--10 u-fontSize--normal">
-                  {generateBundleErrMsg}
+                  {state.generateBundleErrMsg}
                 </p>
               )}
             </div>
           </div>
         </div>
-        <div
-          className="flex u-marginTop--30 justifyContent--center alignItems--center"
-          style={{ gap: "10px" }}
-        >
-          <div
-            style={{ borderBottom: "1px solid #BEBEBE", width: "180px" }}
-          ></div>
-          <span>or</span>
-          <div
-            style={{ borderBottom: "1px solid #BEBEBE", width: "180px" }}
-          ></div>
-        </div>
-        <div className="u-marginTop--30 command-modal">
-          <span className="u-fontWeight--bold u-textColor--primary">
-            Run a command to generate a support bundle
-          </span>
-          <p className="u-marginTop--10 u-textColor--secondary u-fontSize--normal u-lineHeight--normal">
-            Run the following command passing the correct path to your support
-            bundle spec to generate a support bundle. If your vendor did not
-            provide a support bundle spec,{" "}
-            <a href="#" onClick={toggleShowGetBundleSpec}>
-              run this command
-            </a>{" "}
-            from a machine with internet access.
-          </p>
-          <div className="u-marginTop--30">
-            {showGetBundleSpec && (
-              <>
-                <p className="u-paddingBottom--10 u-textColor--secondary u-fontSize--normal">
-                  Run this command from a machine with internet access to get a
-                  support bundle spec
-                </p>
-                <CodeSnippet
-                  language="bash"
-                  canCopy
-                  onCopyText={
-                    <span className="u-textColor--success">
-                      Command has been copied to your clipboard
-                    </span>
-                  }
-                >
-                  curl -o spec.yaml https://kots.io -H
-                  'User-agent:Replicated_Troubleshoot/v1beta1'{" "}
-                </CodeSnippet>
-              </>
-            )}
-            <CodeSnippet
-              language="bash"
-              canCopy={true}
-              onCopyText={
-                <span className="u-textColor--success">
-                  Command has been copied to your clipboard
-                </span>
-              }
+        {state.showGetBundleSpec ? (
+          <div className="u-marginTop--15 command-modal">
+            <span className="u-fontWeight--bold u-textColor--primary">
+              Run a command to generate a support bundle
+            </span>
+            <div className="u-marginTop--15">
+              {state.showGetBundleSpec && (
+                <>
+                  <p className="u-paddingBottom--10 u-textColor--secondary u-fontSize--normal">
+                    Run the following commands to generate a support bundle from
+                    the CLI. You can then upload a support bundle so that it
+                    appears in the admin console.
+                  </p>
+                  <CodeSnippet
+                    language="bash"
+                    canCopy={true}
+                    onCopyText={
+                      <span className="u-textColor--success">
+                        Command has been copied to your clipboard
+                      </span>
+                    }
+                  >
+                    {state.bundleCommand}
+                  </CodeSnippet>
+                </>
+              )}
+            </div>
+            <div
+              className="flex u-marginTop--30 justifyContent--center alignItems--center"
+              style={{ gap: "10px" }}
             >
-              {bundleCommand}
-            </CodeSnippet>
+              <div
+                style={{ borderBottom: "1px solid #BEBEBE", width: "180px" }}
+              ></div>
+              <span>or</span>
+              <div
+                style={{ borderBottom: "1px solid #BEBEBE", width: "180px" }}
+              ></div>
+            </div>
+            {state.uploadBundleErrMsg && (
+              <p className="u-textColor--error u-fontSize--normal u-fontWeight--medium u-lineHeight--normal u-marginTop--10 u-marginBottom--10">
+                {state.uploadBundleErrMsg}
+              </p>
+            )}
           </div>
-          <div className="flex u-marginTop--30">
-            <button className="btn secondary blue u-marginRight--15">
-              Upload support bundle
-            </button>{" "}
-            <button className="btn primary" onClick={toggleModal}>
-              Ok, got it!
-            </button>
+        ) : (
+          <div className="u-marginTop--15">
+            <span className="u-fontSize--normal">
+              Or{" "}
+              <a href="#" onClick={toggleShowGetBundleSpec}>
+                click here
+              </a>{" "}
+              to get a command to manually generate a support bundle. This is
+              useful if the admin console is inaccessible.
+            </span>
           </div>
+        )}
+
+        <div
+          className={`u-marginTop--30 FileUpload-wrapper ${
+            hasFile ? "has-file" : ""
+          }`}
+        >
+          <Dropzone
+            className="Dropzone-wrapper"
+            accept="application/gzip, .gz"
+            onDropAccepted={onDrop}
+            multiple={false}
+          >
+            {hasFile ? (
+              <div
+                className="flex flexDirection--column justifyContent--spaceBetween"
+                style={{ gap: "15px" }}
+              >
+                <div className={`${hasFile ? "has-file-border" : ""}`}>
+                  <p className="u-fontSize--normal u-fontWeight--medium ">
+                    {state.supportBundle?.name}
+                  </p>
+                </div>
+                <div className="flex flex-column justifyContent--center">
+                  <button
+                    type="button"
+                    className="btn secondary blue nowrap"
+                    onClick={uploadAndAnalyze}
+                    disabled={state.fileUploading || !hasFile}
+                  >
+                    {state.fileUploading
+                      ? "Uploading"
+                      : "Upload support bundle"}
+                  </button>
+                  {/* <div>
+                    <span
+                      className="replicated-link u-fontSize--small u-marginTop--10 u-textAlign--center"
+                      onClick={clearFile}
+                    >
+                      Select a different file
+                    </span>
+                  </div> */}
+                </div>
+              </div>
+            ) : (
+              <div className="u-textAlign--center">
+                <Icon
+                  icon="yaml-icon"
+                  size={40}
+                  className="u-marginBottom--10 gray-color"
+                />
+                <p className="u-fontSize--normal u-textColor--secondary u-fontWeight--medium u-lineHeight--normal">
+                  Drag your bundle here or{" "}
+                  <span className="u-linkColor u-fontWeight--medium u-textDecoration--underlineOnHover">
+                    choose a file to upload
+                  </span>
+                </p>
+              </div>
+            )}
+          </Dropzone>
+        </div>
+        <div className="flex u-marginTop--30">
+          <button className="btn primary" onClick={toggleModal}>
+            Ok, got it!
+          </button>
         </div>
       </div>
     </Modal>
