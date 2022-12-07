ce1589344432954f4dfd390d2409583d35780d11
miaawong <miawongdev@gmail.com>
2022-12-06 16:09:33 -0800

    throw an error so react-query will return an error

--- web/src/hooks/useSupportBundleCommand.ts
+++ web/src/hooks/useSupportBundleCommand.ts
@@ -10,25 +10,18 @@ const fetchSupportBundleCommand = async (watchSlug: string, origin: string) => {
       "Content-Type": "application/json",
     },
   };
-  console.log(origin, "oriign");
 
   try {
     const res = await axios.post(
-      `${process.env.API_ENDPOINT}/troubleshoot/app/${watchSlug}/supportbundlecommand`,
+      `${process.env.API_ENDPOINT}/troubleshoot/app/${watchSlug}/supportbundleommand`,
       origin,
       config
     );
 
     if (res.status === 200) {
-      console.log(res.data, "res");
       return res.data;
-    } else {
-      // TODO: more error handling
-      console.log("something went wrong");
-      throw new Error("something went wrong");
     }
   } catch (err) {
-    console.log("err");
     if (err instanceof Error) {
       throw err;
     }
@@ -36,8 +29,7 @@ const fetchSupportBundleCommand = async (watchSlug: string, origin: string) => {
 };
 
 export const useSupportBundleCommand = (watchSlug: string, origin: string) => {
-  console.log(watchSlug, origin);
-  return useQuery("supportBundleCommand", () => {
-    fetchSupportBundleCommand(watchSlug, origin);
-  });
+  return useQuery(["supportBundleCommand"], () =>
+    fetchSupportBundleCommand(watchSlug, origin)
+  );
 };
