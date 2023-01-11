875c8e40c1a07d0a6cc42144cc39f444ef6cc4e3
miaawong <miawongdev@gmail.com>
2023-01-11 11:19:21 -0800

    add ShowLogsModal to storybook

---
+++ web/src/stories/ShowLogsModal.stories.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
+import ShowLogsModal from "@src/components/modals/ShowLogsModal";
+
+export default {
+  title: "Example/ShowLogsModal",
+  component: ShowLogsModal
+} as ComponentMeta<typeof ShowLogsModal>;
+
+const Template: ComponentStory<typeof ShowLogsModal> = (args) => {
+  const [selectedTab, setSelectedTab] = React.useState("dryrunStdout");
+  const renderLogsTab = () => {
+    const isHelmManaged = false;
+    const filterNonHelmTabs = (tab: string, isHelmManaged: boolean) => {
+      if (isHelmManaged) {
+        return tab.startsWith("helm");
+      }
+      return true;
+    };
+
+    const tabs = Object.keys(args.logs);
+
+    return (
+      <div className="flex action-tab-bar u-marginTop--10">
+        {tabs
+          .filter((tab) => tab !== "renderError")
+          .filter((tab) => filterNonHelmTabs(tab, isHelmManaged))
+          .map((tab) => (
+            <div
+              className={`tab-item blue ${tab === selectedTab && "is-active"}`}
+              key={tab}
+              onClick={() => setSelectedTab(tab)}
+            >
+              {tab}
+            </div>
+          ))}
+      </div>
+    );
+  };
+
+  return (
+    <MemoryRouter>
+      <ShowLogsModal
+        {...args}
+        renderLogsTabs={renderLogsTab()}
+        selectedTab={selectedTab}
+      />
+    </MemoryRouter>
+  );
+};
+export const ShowLogsModalExample = Template.bind({});
+
+ShowLogsModalExample.args = {
+  showLogsModal: true,
+  hideLogsModal: () => {
+    alert("hide modal");
+  },
+  // if this is true, the modal will show the error msg and not the logs
+  //viewLogsErrMsg: "Error message",
+  logs: {
+    dryrunStdout:
+      "hi configmap/nginx-content created (dry run)\nsecret/kotsadm-replicated-registry created (dry run)\nsecret/snapshot3-registry created (dry run)\nservice/nginx created (dry run)\ndeployment.apps/nginx created (dry run)\n",
+    dryrunStderr: "",
+    applyStdout:
+      "hi configmap/nginx-content created\nsecret/kotsadm-replicated-registry created\nsecret/snapshot3-registry created\nservice/nginx created\ndeployment.apps/nginx created\n",
+    applyStderr: "",
+    helmStdout: "",
+    helmStderr: "",
+    renderError: ""
+  },
+  logsLoading: false,
+  versionFailing: "version failing",
+  troubleshootUrl: "troubleshoot.url"
+};
