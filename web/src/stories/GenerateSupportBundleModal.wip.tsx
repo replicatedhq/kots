44f952c72d74afee8220122ebfa2a9896422407d
Rian Fowler <rian@replicated.com>
2023-01-31 10:17:55 -0600

    comment out implementation for WIP story

--- web/src/stories/GenerateSupportBundleModal.wip.tsx
+++ web/src/stories/GenerateSupportBundleModal.wip.tsx
@@ -4,25 +4,25 @@ import { MemoryRouter } from "react-router-dom";
 import GenerateSupportBundleModal from "@src/components/troubleshoot/GenerateSupportBundleModal";
 
 // TODO: update after adding fetch mock support to storybook
-export default {
-  title: "Example/GenerateSupportBundleModal",
-  component: GenerateSupportBundleModal,
-} as ComponentMeta<typeof GenerateSupportBundleModal>;
+// export default {
+//   title: "Example/GenerateSupportBundleModal",
+//   component: GenerateSupportBundleModal,
+// } as ComponentMeta<typeof GenerateSupportBundleModal>;
 
-const Template: ComponentStory<typeof GenerateSupportBundleModal> = (args) => (
-  <MemoryRouter>
-    <GenerateSupportBundleModal {...args} />
-  </MemoryRouter>
-);
+// const Template: ComponentStory<typeof GenerateSupportBundleModal> = (args) => (
+//   <MemoryRouter>
+//     <GenerateSupportBundleModal {...args} />
+//   </MemoryRouter>
+// );
 
-export const GenerateSupportBundleModalExample = Template.bind({});
+// export const GenerateSupportBundleModalExample = Template.bind({});
 
-GenerateSupportBundleModalExample.args = {
-  isOpen: true,
-  toggleModal: () => alert("toggle modal"),
-  updateBundleSlug: () => alert("update bundle slug"),
-  watch: {
-    id: "WatchId",
-    slug: "AppSlug",
-  },
-};
+// GenerateSupportBundleModalExample.args = {
+//   isOpen: true,
+//   toggleModal: () => alert("toggle modal"),
+//   updateBundleSlug: () => alert("update bundle slug"),
+//   watch: {
+//     id: "WatchId",
+//     slug: "AppSlug",
+//   },
+// };
