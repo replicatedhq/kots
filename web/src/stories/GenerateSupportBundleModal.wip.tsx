6d4e30e3153df9f41c0d1dc29484eb5834ecebdb
Rian Fowler <rian@replicated.com>
2023-01-31 10:07:10 -0600

    add stories

---
+++ web/src/stories/GenerateSupportBundleModal.wip.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
+import GenerateSupportBundleModal from "@src/components/troubleshoot/GenerateSupportBundleModal";
+
+// TODO: update after adding fetch mock support to storybook
+export default {
+  title: "Example/GenerateSupportBundleModal",
+  component: GenerateSupportBundleModal,
+} as ComponentMeta<typeof GenerateSupportBundleModal>;
+
+const Template: ComponentStory<typeof GenerateSupportBundleModal> = (args) => (
+  <MemoryRouter>
+    <GenerateSupportBundleModal {...args} />
+  </MemoryRouter>
+);
+
+export const GenerateSupportBundleModalExample = Template.bind({});
+
+GenerateSupportBundleModalExample.args = {
+  isOpen: true,
+  toggleModal: () => alert("toggle modal"),
+  updateBundleSlug: () => alert("update bundle slug"),
+  watch: {
+    id: "WatchId",
+    slug: "AppSlug",
+  },
+};
---
+++ web/src/stories/UploadSupportBundle.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
+import UploadSupportBundleModal from "@src/components/troubleshoot/UploadSupportBundleModal";
+
+export default {
+  title: "Example/UploadSupportBundleModal",
+  component: UploadSupportBundleModal,
+} as ComponentMeta<typeof UploadSupportBundleModal>;
+
+const Template: ComponentStory<typeof UploadSupportBundleModal> = (args) => (
+  <MemoryRouter>
+    <UploadSupportBundleModal {...args} />
+  </MemoryRouter>
+);
+
+export const UploadSupportBundleModalExample = Template.bind({});
+
+UploadSupportBundleModalExample.args = {};
