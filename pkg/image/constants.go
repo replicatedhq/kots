ebf706a924449d319e132b8fc3ec49eb43ed9c95
replicated-ci-kots <91219917+replicated-ci-kots@users.noreply.github.com>
2023-03-02 07:54:01 -0600

    update kots image dependency tags (#3373)
    Co-authored-by: sgalsaleh <sgalsaleh@users.noreply.github.com>

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,9 +5,9 @@ package image
 // image name.
 
 const (
-	Minio      = "minio/minio:RELEASE.2023-02-22T18-23-45Z"
-	Mc         = "minio/mc:RELEASE.2023-02-16T19-20-11Z"
-	Rqlite     = "rqlite/rqlite:7.13.1"
+	Minio      = "minio/minio:RELEASE.2023-02-27T18-10-45Z"
+	Mc         = "minio/mc:RELEASE.2023-02-28T00-12-59Z"
+	Rqlite     = "rqlite/rqlite:7.13.2"
 	Dex        = "ghcr.io/dexidp/dex:v2.35.3"
 	Schemahero = "schemahero/schemahero:0.13.8"
 	Lvp        = "replicated/local-volume-provider:v0.5.0"
