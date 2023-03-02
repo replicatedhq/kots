2efde17c5837ca59a6c65ea677e622de84bff597
miaawong <miawongdev@gmail.com>
2023-03-02 12:41:16 -0800

    redesign preflight

--- web/src/components/PreflightResultPage.tsx
+++ web/src/components/PreflightResultPage.tsx
@@ -71,74 +71,64 @@ function PreflightResultPage(props: Props) {
   return (
     <div className="flex-column flex1 container">
       <KotsPageTitle pageName="Preflight Checks" showAppSlug />
-      <div className="flex1 flex u-overflow--auto">
-        <div className="PreflightChecks--wrapper flex1 flex-column u-paddingTop--30">
-          {history.location.pathname.includes("version-history") && (
-            <div
-              className="u-fontWeight--bold link"
-              onClick={() => history.goBack()}
-            >
-              <Icon
-                icon="prev-arrow"
-                size={12}
-                className="clickable u-marginRight--10"
-                style={{ verticalAlign: "0" }}
-              />
-              Back
+      <div className="PreflightChecks--wrapper flex-column u-paddingTop--30 flex1 flex u-overflow--auto">
+        <div
+          className={`u-maxWidth--full u-marginTop--20 flex-column u-position--relative card-bg ${
+            preflightCheck?.showPreflightCheckPending ? "flex1" : ""
+          }`}
+        >
+          {getPreflightResultsError?.message && (
+            <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
+              <div className="icon redWarningIcon u-marginRight--10" />
+              <div>
+                <p className="title">Encountered an error</p>
+                <p className="error">{getPreflightResultsError.message}</p>
+              </div>
             </div>
           )}
-          <div className="u-minWidth--full u-marginTop--20 flex-column flex1 u-position--relative">
-            {getPreflightResultsError?.message && (
-              <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
-                <div className="icon redWarningIcon u-marginRight--10" />
-                <div>
-                  <p className="title">Encountered an error</p>
-                  <p className="error">{getPreflightResultsError.message}</p>
-                </div>
+          {ignorePermissionError?.message && (
+            <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
+              <div className="icon redWarningIcon u-marginRight--10" />
+              <div>
+                <p className="title">Encountered an error</p>
+                <p className="error">{ignorePermissionError.message}</p>
               </div>
-            )}
-            {ignorePermissionError?.message && (
-              <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
-                <div className="icon redWarningIcon u-marginRight--10" />
-                <div>
-                  <p className="title">Encountered an error</p>
-                  <p className="error">{ignorePermissionError.message}</p>
-                </div>
+            </div>
+          )}
+          {rerunPreflightsError?.message && (
+            <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
+              <div className="icon redWarningIcon u-marginRight--10" />
+              <div>
+                <p className="title">Encountered an error</p>
+                <p className="error">{rerunPreflightsError.message}</p>
               </div>
-            )}
-            {rerunPreflightsError?.message && (
-              <div className="ErrorWrapper flex-auto flex alignItems--center u-marginBottom--20">
-                <div className="icon redWarningIcon u-marginRight--10" />
-                <div>
-                  <p className="title">Encountered an error</p>
-                  <p className="error">{rerunPreflightsError.message}</p>
-                </div>
-              </div>
-            )}
-            <p className="u-fontSize--jumbo2 u-textColor--primary u-fontWeight--bold">
-              Preflight checks
-            </p>
-            <p className="u-fontWeight--medium u-lineHeight--more u-marginTop--5 u-marginBottom--15">
-              Preflight checks validate that your cluster will meet the minimum
-              requirements. If your cluster does not meet the requirements your
-              application might not work properly. Some checks may be required
-              which means your application will not be able to be deployed until
-              they pass. Optional checks are recommended to ensure that the
-              application you are installing will work as intended.
-            </p>
-            {preflightCheck?.showPreflightCheckPending && (
-              <div className="flex-column justifyContent--center alignItems--center flex1 u-minWidth--full">
-                <PreflightsProgress
-                  pendingPreflightCheckName={
-                    preflightCheck?.pendingPreflightCheckName || ""
-                  }
-                  percentage={
-                    preflightCheck?.pendingPreflightChecksPercentage || 0
-                  }
-                />
-              </div>
-            )}
-            {preflightCheck?.showPreflightResultErrors && (
+            </div>
+          )}
+          <p className="u-fontSize--jumbo2 u-textColor--primary u-fontWeight--bold">
+            Preflight checks
+          </p>
+          <p className="u-fontWeight--medium u-lineHeight--more u-marginTop--5 u-marginBottom--15">
+            Preflight checks validate that your cluster meets the minimum
+            requirements. Required checks must pass in order to deploy the
+            application. Optional checks are recommended to ensure that the
+            application will work as intended.
+          </p>
+
+          {preflightCheck?.showPreflightCheckPending && (
+            <div className="flex-column justifyContent--center alignItems--center flex1 u-minWidth--full">
+              <PreflightsProgress
+                pendingPreflightCheckName={
+                  preflightCheck?.pendingPreflightCheckName || ""
+                }
+                percentage={
+                  preflightCheck?.pendingPreflightChecksPercentage || 0
+                }
+              />
+            </div>
+          )}
+
+          {preflightCheck?.showPreflightResultErrors && (
+            <>
               <PreflightResultErrors
                 errors={preflightCheck.errors}
                 ignorePermissionErrors={ignorePermissionErrors}
@@ -146,93 +136,103 @@ function PreflightResultPage(props: Props) {
                 preflightResultData={preflightCheck.preflightResults}
                 showRbacError={preflightCheck.showRbacError}
               />
-            )}
-            {preflightCheck?.showPreflightResults && (
-              <div className="dashboard-card">
-                <div className="flex flex1 justifyContent--spaceBetween alignItems--center">
-                  <p className="u-fontSize--large u-textColor--primary u-fontWeight--bold">
-                    Results from your preflight checks
-                  </p>
-                  <div className="flex alignItems--center">
-                    {props.fromLicenseFlow &&
-                    preflightCheck?.showCancelPreflight ? (
-                      <div className="flex alignItems--center">
-                        <div className="flex alignItems--center u-marginRight--20">
-                          <Link
-                            to={`/app/${slug}`}
-                            className="u-textColor--error u-textDecoration--underlineOnHover u-fontWeight--medium u-fontSize--small"
-                          >
-                            Cancel
-                          </Link>
-                        </div>
-                      </div>
-                    ) : null}
-                  </div>
-                </div>
-                <div className="flex-column">
-                  <PreflightRenderer
-                    results={preflightCheck?.preflightResults}
-                    skipped={preflightCheck?.showPreflightSkipped}
-                  />
-                </div>
+              <div className="flex justifyContent--flexEnd tw-gap-6">
+                <button
+                  className="btn secondary blue"
+                  onClick={() => ignorePermissionErrors()}
+                >
+                  Proceed with limited Preflights
+                </button>
+                {preflightCheck.shouldShowRerunPreflight && (
+                  <button
+                    type="button"
+                    className="btn primary blue"
+                    onClick={() => rerunPreflights()}
+                  >
+                    Re-run
+                  </button>
+                )}
               </div>
-            )}
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justifyContent--flexEnd tw-gap-5">
-        {preflightCheck?.shouldShowRerunPreflight &&
-          preflightCheck?.showPreflightResultErrors && (
-            <div className="flex-auto flex justifyContent--flexEnd u-marginBottom--15">
-              <button
-                type="button"
-                className="btn primary blue"
-                onClick={() => rerunPreflights()}
-              >
-                Re-run
-              </button>
+            </>
+          )}
+          {preflightCheck?.showPreflightResults && (
+            <div className="tw-mt-6">
+              <div className="flex flex1 tw-justify-between tw-items-end">
+                <p className="u-fontSize--large u-textColor--primary u-fontWeight--bold">
+                  Results
+                </p>
+                {preflightCheck?.shouldShowRerunPreflight && (
+                  <button
+                    type="button"
+                    className="btn primary blue"
+                    onClick={() => rerunPreflights()}
+                  >
+                    Re-run
+                  </button>
+                )}
+              </div>
+              <div className="flex-column">
+                <PreflightRenderer
+                  results={preflightCheck?.preflightResults}
+                  skipped={preflightCheck?.showPreflightSkipped}
+                />
+              </div>
             </div>
           )}
-        {props.fromLicenseFlow && (
-          <div className="flex-auto flex justifyContent--flexEnd u-marginBottom--15">
-            {!preflightCheck?.showPreflightCheckPending && (
-              <div>
-                <button
-                  type="button"
-                  className="btn primary blue"
-                  disabled={preflightCheck?.showDeploymentBlocked}
-                  onClick={() =>
-                    preflightCheck?.shouldShowConfirmContinueWithFailedPreflights
-                      ? setShowContinueWithFailedPreflightsModal(true)
-                      : deployKotsDownstream({
-                          continueWithFailedPreflights: true,
-                        })
-                  }
-                >
+          {props.fromLicenseFlow && preflightCheck?.showPreflightResults && (
+            <div className="flex-auto flex justifyContent--flexEnd tw-mt-6">
+              <div className="flex tw-gap-6">
+                {preflightCheck?.showCancelPreflight && (
+                  <Link to={`/app/${slug}`}>
+                    <button className="btn secondary blue">
+                      Go to dashboard
+                    </button>
+                  </Link>
+                )}
+                {!preflightCheck?.showPreflightCheckPending && (
+                  <>
+                    <button
+                      type="button"
+                      className="btn primary blue"
+                      disabled={preflightCheck?.showDeploymentBlocked}
+                      onClick={() =>
+                        preflightCheck?.shouldShowConfirmContinueWithFailedPreflights
+                          ? setShowContinueWithFailedPreflightsModal(true)
+                          : deployKotsDownstream({
+                              continueWithFailedPreflights: true,
+                            })
+                      }
+                    >
+                      <span
+                        data-tip-disable={
+                          !preflightCheck?.showDeploymentBlocked
+                        }
+                        data-tip="Deployment is disabled as a strict analyzer in this version's preflight checks has failed or has not been run"
+                        data-for="disable-deployment-tooltip"
+                      >
+                        Deploy
+                      </span>
+                    </button>
+                    <ReactTooltip
+                      effect="solid"
+                      id="disable-deployment-tooltip"
+                    />
+                  </>
+                )}
+              </div>
+              {preflightCheck?.showIgnorePreflight && (
+                <div className="flex flex1 justifyContent--center alignItems--center">
                   <span
-                    data-tip-disable={!preflightCheck?.showDeploymentBlocked}
-                    data-tip="Deployment is disabled as a strict analyzer in this version's preflight checks has failed or has not been run"
-                    data-for="disable-deployment-tooltip"
+                    className="u-fontSize--normal u-fontWeight--medium u-textDecoration--underline u-textColor--bodyCopy u-marginTop--15 u-cursor--pointer"
+                    onClick={() => setShowConfirmIgnorePreflightsModal(true)}
                   >
-                    Continue
+                    Ignore Preflights{" "}
                   </span>
-                </button>
-                <ReactTooltip effect="solid" id="disable-deployment-tooltip" />
-              </div>
-            )}
-            {preflightCheck?.showIgnorePreflight && (
-              <div className="flex flex1 justifyContent--center alignItems--center">
-                <span
-                  className="u-fontSize--normal u-fontWeight--medium u-textDecoration--underline u-textColor--bodyCopy u-marginTop--15 u-cursor--pointer"
-                  onClick={() => setShowConfirmIgnorePreflightsModal(true)}
-                >
-                  Ignore Preflights{" "}
-                </span>
-              </div>
-            )}
-          </div>
-        )}
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
 
       {showConfirmIgnorePreflightsModal && (
@@ -256,11 +256,12 @@ function PreflightResultPage(props: Props) {
         ariaHideApp={false}
         className="Modal"
       >
-        <div className="Modal-body">
-          <p className="u-fontSize--normal u-textColor--bodyCopy u-lineHeight--normal u-marginBottom--20">
-            Preflight is showing some issues, are you sure you want to continue?
+        <div className="Modal-body tw-w-[300px]">
+          <p className="u-fontSize--normal u-textColor--bodyCopy u-lineHeight--normal u-marginBottom--20 tw-text-center">
+            Some preflight checks did not pass. <br /> Are you sure you want to
+            deploy?
           </p>
-          <div className="u-marginTop--10 flex justifyContent--flexEnd">
+          <div className="u-marginTop--10 flex tw-justify-center">
             <button
               type="button"
               className="btn secondary"
@@ -276,7 +277,7 @@ function PreflightResultPage(props: Props) {
                 deployKotsDownstream({ continueWithFailedPreflights: true });
               }}
             >
-              Deploy and continue
+              Deploy anyway
             </button>
           </div>
         </div>
