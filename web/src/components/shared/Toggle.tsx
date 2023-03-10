f208c3487ba34b8e94629dee7424cd20903e493d
Rian Fowler <rian@replicated.com>
2023-03-10 09:27:13 -0600

    add type checking to Toggle. make it a FC

---
+++ web/src/components/shared/Toggle.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../scss/components/shared/Toggle.scss";
+
+interface Props {
+  items: {
+    onClick: () => void;
+    isActive: boolean;
+    title: string;
+  }[];
+}
+
+export default function Tooltip(props: Props) {
+  return (
+    <div className="Toggle flex flex-auto alignItems--center">
+      {props.items?.map((item, i) => {
+        return (
+          <div
+            key={i}
+            className={`Toggle-item ${item.isActive ? "is-active" : ""}`}
+            onClick={item.onClick}
+          >
+            {item.title}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
