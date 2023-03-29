1d16aa676c2bcc4d80a9b56974fa0ed7715d2a8b
Tim Ousborne <timo@replicated.com>
2023-03-28 18:15:54 -0600

    Revert "change kotskinds MappedValue to be public and annotated for JSON marshling"
    This reverts commit 73148d1344eec0139cfdd3727336f2a34abb1717.

--- kotskinds/apis/kots/v1beta1/helmchart_types.go
+++ kotskinds/apis/kots/v1beta1/helmchart_types.go
@@ -28,35 +28,35 @@ import (
 
 // +kubebuilder:validation:Type=""
 type MappedChartValue struct {
-	Value string `json:"value,omitempty"`
+	Value string `json:"-"`
 
-	ValueType string `json:"valueType,omitempty"`
+	valueType string `json:"-"`
 
-	StrValue   string  `json:"strValue,omitempty"`
-	BoolValue  bool    `json:"boolValue,omitempty"`
-	FloatValue float64 `json:"floatValue,omitempty"`
+	strValue   string  `json:"-"`
+	boolValue  bool    `json:"-"`
+	floatValue float64 `json:"-"`
 
-	Children map[string]*MappedChartValue `json:"children,omitempty"`
-	Array    []*MappedChartValue          `json:"array,omitempty"`
+	children map[string]*MappedChartValue `json:"-"`
+	array    []*MappedChartValue          `json:"-"`
 }
 
 func (m *MappedChartValue) getBuiltValue() (interface{}, error) {
-	if m.ValueType == "string" {
-		return m.StrValue, nil
+	if m.valueType == "string" {
+		return m.strValue, nil
 	}
-	if m.ValueType == "bool" {
-		return m.BoolValue, nil
+	if m.valueType == "bool" {
+		return m.boolValue, nil
 	}
-	if m.ValueType == "float" {
-		return m.FloatValue, nil
+	if m.valueType == "float" {
+		return m.floatValue, nil
 	}
-	if m.ValueType == "nil" {
+	if m.valueType == "nil" {
 		return nil, nil
 	}
 
-	if m.ValueType == "children" {
+	if m.valueType == "children" {
 		children := map[string]interface{}{}
-		for k, v := range m.Children {
+		for k, v := range m.children {
 			childValue, err := v.getBuiltValue()
 			if err != nil {
 				return nil, errors.Wrapf(err, "failed to get value of child %s", k)
@@ -65,9 +65,9 @@ func (m *MappedChartValue) getBuiltValue() (interface{}, error) {
 		}
 		return children, nil
 	}
-	if m.ValueType == "array" {
+	if m.valueType == "array" {
 		var elements []interface{}
-		for i, v := range m.Array {
+		for i, v := range m.array {
 			elValue, err := v.getBuiltValue()
 			if err != nil {
 				return nil, errors.Wrapf(err, "failed to get value of child %d", i)
@@ -88,30 +88,30 @@ func (m *MappedChartValue) UnmarshalJSON(value []byte) error {
 	}
 
 	if b == nil {
-		m.ValueType = "nil"
+		m.valueType = "nil"
 		return nil
 	}
 
 	if b, ok := b.(string); ok {
-		m.StrValue = b
-		m.ValueType = "string"
+		m.strValue = b
+		m.valueType = "string"
 		return nil
 	}
 
 	if b, ok := b.(bool); ok {
-		m.BoolValue = b
-		m.ValueType = "bool"
+		m.boolValue = b
+		m.valueType = "bool"
 		return nil
 	}
 
 	if b, ok := b.(float64); ok {
-		m.FloatValue = b
-		m.ValueType = "float"
+		m.floatValue = b
+		m.valueType = "float"
 		return nil
 	}
 
 	if b, ok := b.(map[string]interface{}); ok {
-		m.Children = make(map[string]*MappedChartValue)
+		m.children = make(map[string]*MappedChartValue)
 		for k, v := range b {
 			vv, err := json.Marshal(v)
 			if err != nil {
@@ -123,16 +123,16 @@ func (m *MappedChartValue) UnmarshalJSON(value []byte) error {
 				return err
 			}
 
-			m.Children[k] = m2
+			m.children[k] = m2
 		}
 
-		m.ValueType = "children"
+		m.valueType = "children"
 
 		return nil
 	}
 
 	if b, ok := b.([]interface{}); ok {
-		m.Array = []*MappedChartValue{}
+		m.array = []*MappedChartValue{}
 		for _, v := range b {
 			vv, err := json.Marshal(v)
 			if err != nil {
@@ -144,10 +144,10 @@ func (m *MappedChartValue) UnmarshalJSON(value []byte) error {
 				return err
 			}
 
-			m.Array = append(m.Array, m2)
+			m.array = append(m.array, m2)
 		}
 
-		m.ValueType = "array"
+		m.valueType = "array"
 
 		return nil
 	}
@@ -191,9 +191,9 @@ func (h *HelmChartSpec) GetReplTmplValues(values map[string]MappedChartValue) (m
 }
 
 func (h *HelmChartSpec) getReplTmplValue(value *MappedChartValue) (interface{}, error) {
-	if value.ValueType == "children" {
+	if value.valueType == "children" {
 		result := map[string]interface{}{}
-		for k, v := range value.Children {
+		for k, v := range value.children {
 			built, err := h.getReplTmplValue(v)
 			if err != nil || built == nil {
 				continue
@@ -204,9 +204,9 @@ func (h *HelmChartSpec) getReplTmplValue(value *MappedChartValue) (interface{}, 
 			return nil, nil
 		}
 		return result, nil
-	} else if value.ValueType == "array" {
+	} else if value.valueType == "array" {
 		result := []interface{}{}
-		for _, v := range value.Array {
+		for _, v := range value.array {
 			built, err := h.getReplTmplValue(v)
 			if err != nil {
 				return nil, errors.Wrap(err, "failed to render array value")
@@ -257,12 +257,12 @@ func MergeHelmChartValues(baseValues map[string]MappedChartValue,
 			result[k] = baseValues[k]
 			continue
 		}
-		if v.ValueType != "children" {
+		if v.valueType != "children" {
 			result[k] = overlayValues[k]
 		} else {
 			result[k] = MappedChartValue{
-				ValueType: "children",
-				Children:  mergeValueChildren(v.Children, overlayValues[k].Children),
+				valueType: "children",
+				children:  mergeValueChildren(v.children, overlayValues[k].children),
 			}
 		}
 	}
@@ -281,12 +281,12 @@ func mergeValueChildren(baseValues map[string]*MappedChartValue, overlayValues m
 			result[k] = baseValues[k]
 			continue
 		}
-		if v.ValueType != "children" {
+		if v.valueType != "children" {
 			result[k] = overlayValues[k]
 		} else {
 			result[k] = &MappedChartValue{
-				ValueType: "children",
-				Children:  mergeValueChildren(v.Children, overlayValues[k].Children),
+				valueType: "children",
+				children:  mergeValueChildren(v.children, overlayValues[k].children),
 			}
 		}
 	}
@@ -300,9 +300,9 @@ func mergeValueChildren(baseValues map[string]*MappedChartValue, overlayValues m
 }
 
 func (h *HelmChartSpec) renderValue(value *MappedChartValue) (interface{}, error) {
-	if value.ValueType == "children" {
+	if value.valueType == "children" {
 		result := map[string]interface{}{}
-		for k, v := range value.Children {
+		for k, v := range value.children {
 			built, err := h.renderValue(v)
 			if err != nil {
 				return nil, errors.Wrapf(err, "failed to render child value at key %s", k)
@@ -310,9 +310,9 @@ func (h *HelmChartSpec) renderValue(value *MappedChartValue) (interface{}, error
 			result[k] = built
 		}
 		return result, nil
-	} else if value.ValueType == "array" {
+	} else if value.valueType == "array" {
 		result := []interface{}{}
-		for _, v := range value.Array {
+		for _, v := range value.array {
 			built, err := h.renderValue(v)
 			if err != nil {
 				return nil, errors.Wrap(err, "failed to render array value")
