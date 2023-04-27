015c520296b6e6b6376b9c379650aa3f956daafc
Pavan Sokke Nagaraj <pavan@replicated.com>
2023-04-27 17:22:41 +0000

    fix a linter

--- pkg/operator/client/manifest_delete.go
+++ pkg/operator/client/manifest_delete.go
@@ -14,7 +14,7 @@ import (
 	kuberneteserrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	labels "k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/sets"
