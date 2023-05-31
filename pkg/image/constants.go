2450dd67d9f9c5ae5299ce7864e99feace0f6038
Pavan Sokke Nagaraj <pavan@replicated.com>
2023-05-31 15:45:31 -0400

    update dependency image tags (#3913)

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,9 +5,9 @@ package image
 // image name.
 
 const (
-	Minio      = "minio/minio:RELEASE.2023-05-18T00-05-36Z"
-	Mc         = "minio/mc:RELEASE.2023-05-18T16-59-00Z"
-	Rqlite     = "rqlite/rqlite:7.17.0"
+	Minio      = "minio/minio:RELEASE.2023-05-27T05-56-19Z"
+	Mc         = "minio/mc:RELEASE.2023-05-26T23-31-54Z"
+	Rqlite     = "rqlite/rqlite:7.19.0"
 	Dex        = "ghcr.io/dexidp/dex:v2.36.0"
 	Schemahero = "schemahero/schemahero:0.14.0"
 	Lvp        = "replicated/local-volume-provider:v0.5.3"
