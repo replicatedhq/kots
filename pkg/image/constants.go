a3c8a4a42e6f0641e6d0264bd9ce758e15e17a2a
Craig O'Donnell <craig@replicated.com>
2023-09-28 15:00:04 +0000

    resolve CVE-2023-29491 in minio deps

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,8 +5,8 @@ package image
 // image name.
 
 const (
-	Minio      = "minio/minio:RELEASE.2023-08-23T10-07-06Z"
-	Mc         = "minio/mc:RELEASE.2023-08-18T21-57-55Z"
+	Minio      = "minio/minio:RELEASE.2023-09-23T03-47-50Z"
+	Mc         = "minio/mc:RELEASE.2023-09-22T05-07-46Z"
 	Rqlite     = "rqlite/rqlite:7.21.4"
 	Dex        = "ghcr.io/dexidp/dex:v2.37.0"
 	Schemahero = "schemahero/schemahero:0.14.0"
