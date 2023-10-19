ccd51bd4c726b3a84491df0b9f44741b8625769e
Andrew Lavery <laverya@umich.edu>
2023-10-19 11:02:33 -0600

    node usage metrics not being collected is not a fatal error (#4082)

--- pkg/helmvm/helmvm_nodes.go
+++ pkg/helmvm/helmvm_nodes.go
@@ -2,10 +2,6 @@ package helmvm
 
 import (
 	"context"
-	"fmt"
-	"math"
-	"strconv"
-
 	"github.com/pkg/errors"
 	"github.com/replicatedhq/kots/pkg/helmvm/types"
 	"github.com/replicatedhq/kots/pkg/k8sutil"
@@ -34,58 +30,13 @@ func GetNodes(ctx context.Context, client kubernetes.Interface) (*types.HelmVMNo
 
 	toReturn := types.HelmVMNodes{}
 
-	nodePods, err := podsPerNode(ctx, client)
-	if err != nil {
-		return nil, errors.Wrap(err, "pods per node")
-	}
-
 	for _, node := range nodes.Items {
-		cpuCapacity := types.CapacityAvailable{}
-		memoryCapacity := types.CapacityAvailable{}
-		podCapacity := types.CapacityAvailable{}
-
-		memoryCapacity.Capacity = float64(node.Status.Capacity.Memory().Value()) / math.Pow(2, 30) // capacity in GB
-
-		cpuCapacity.Capacity, err = strconv.ParseFloat(node.Status.Capacity.Cpu().String(), 64)
+		nodeMet, err := nodeMetrics(ctx, client, metricsClient, node)
 		if err != nil {
-			return nil, errors.Wrapf(err, "parse CPU capacity %q for node %s", node.Status.Capacity.Cpu().String(), node.Name)
+			return nil, errors.Wrap(err, "node metrics")
 		}
 
-		podCapacity.Capacity = float64(node.Status.Capacity.Pods().Value())
-
-		nodeMetrics, err := metricsClient.MetricsV1beta1().NodeMetricses().Get(ctx, node.Name, metav1.GetOptions{})
-		if err != nil {
-			return nil, errors.Wrap(err, "list pod metrics")
-		}
-
-		if nodeMetrics.Usage.Memory() != nil {
-			memoryCapacity.Available = memoryCapacity.Capacity - float64(nodeMetrics.Usage.Memory().Value())/math.Pow(2, 30)
-		}
-
-		if nodeMetrics.Usage.Cpu() != nil {
-			cpuCapacity.Available = cpuCapacity.Capacity - nodeMetrics.Usage.Cpu().AsApproximateFloat64()
-		}
-
-		podCapacity.Available = podCapacity.Capacity - float64(nodePods[node.Name])
-
-		nodeLabelArray := []string{}
-		for k, v := range node.Labels {
-			nodeLabelArray = append(nodeLabelArray, fmt.Sprintf("%s:%s", k, v))
-		}
-
-		toReturn.Nodes = append(toReturn.Nodes, types.Node{
-			Name:           node.Name,
-			IsConnected:    isConnected(node),
-			IsReady:        isReady(node),
-			IsPrimaryNode:  isPrimary(node),
-			CanDelete:      node.Spec.Unschedulable && !isConnected(node),
-			KubeletVersion: node.Status.NodeInfo.KubeletVersion,
-			CPU:            cpuCapacity,
-			Memory:         memoryCapacity,
-			Pods:           podCapacity,
-			Labels:         nodeLabelArray,
-			Conditions:     findNodeConditions(node.Status.Conditions),
-		})
+		toReturn.Nodes = append(toReturn.Nodes, *nodeMet)
 	}
 
 	isHelmVM, err := IsHelmVM(client)
@@ -120,38 +71,6 @@ func findNodeConditions(conditions []corev1.NodeCondition) types.NodeConditions 
 		}
 	}
 	return discoveredConditions
-}
-
-// podsPerNode returns a map of node names to the number of pods, across all namespaces
-func podsPerNode(ctx context.Context, client kubernetes.Interface) (map[string]int, error) {
-	namespaces, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, errors.Wrap(err, "list namespaces")
-	}
-
-	toReturn := map[string]int{}
-
-	for _, ns := range namespaces.Items {
-		nsPods, err := client.CoreV1().Pods(ns.Name).List(ctx, metav1.ListOptions{})
-		if err != nil {
-			return nil, errors.Wrapf(err, "list pods in namespace %s", ns.Name)
-		}
-
-		for _, pod := range nsPods.Items {
-			pod := pod
-			if pod.Spec.NodeName == "" {
-				continue
-			}
-
-			if _, ok := toReturn[pod.Spec.NodeName]; !ok {
-				toReturn[pod.Spec.NodeName] = 0
-			}
-
-			toReturn[pod.Spec.NodeName]++
-		}
-	}
-
-	return toReturn, nil
 }
 
 func isConnected(node corev1.Node) bool {
