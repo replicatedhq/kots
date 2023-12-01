53ba7b3927be937dbf9f9d73b7d5809bf59f90d1
Craig O'Donnell <craig@replicated.com>
2023-12-01 22:17:07 +0000

    add gitops to e2e suite

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -221,6 +221,7 @@ var _ = Describe("E2E", func() {
 			Entry(nil, inventory.NewMultiAppBackupAndRestoreTest()),
 			Entry(nil, inventory.MultiAppTest()),
 			Entry(nil, inventory.NewSupportBundle()),
+			Entry(nil, inventory.NewGitOps()),
 		)
 
 	})
