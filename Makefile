2bfcbdc345a857df9ea08c6ab2a611e9543a9c3b
dydx <dydx@users.noreply.github.com>
2024-01-27 04:05:51 +0000

    Update KOTS image dependency tags

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 include Makefile.build.mk
 CURRENT_USER := $(if $(GITHUB_USER),$(GITHUB_USER),$(shell id -u -n))
 MINIO_TAG ?= 0.20231101.183725-r1
-RQLITE_TAG ?= 8.17.0-r0
-DEX_TAG ?= 2.37.0-r12
+RQLITE_TAG ?= 8.18.1-r0
+DEX_TAG ?= 2.38.0-r0
 LVP_TAG ?= v0.6.0
 
 define sendMetrics
--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -6,8 +6,8 @@ package image
 
 const (
 	Minio      = "kotsadm/minio:0.20231101.183725-r1"
-	Rqlite     = "kotsadm/rqlite:8.17.0-r0"
-	Dex        = "kotsadm/dex:2.37.0-r12"
+	Rqlite     = "kotsadm/rqlite:8.18.1-r0"
+	Dex        = "kotsadm/dex:2.38.0-r0"
 	Schemahero = "schemahero/schemahero:0.17.2"
 	Lvp        = "replicated/local-volume-provider:v0.6.0"
 )
