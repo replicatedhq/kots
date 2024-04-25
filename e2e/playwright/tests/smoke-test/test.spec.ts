78592b68b6aaaa3df30e431d185b14114dd925c4
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-04-25 22:41:57 +0000

    fix test

--- e2e/playwright/tests/smoke-test/test.spec.ts
+++ e2e/playwright/tests/smoke-test/test.spec.ts
@@ -191,6 +191,6 @@ test('smoke test', async ({ page }) => {
   await page.locator('div').filter({ hasText: /^Change passwordAdd new applicationLog out$/ }).getByRole('img').click();
   await page.getByText('Log out', { exact: true }).click();
   await expect(page.getByPlaceholder('password')).toBeVisible({ timeout: 30000 });
-  await expect(page.locator('#app')).toContainText('Enter the password to access the App Name admin consolee.');
+  await expect(page.locator('#app')).toContainText('Enter the password to access the App Name admin console.');
   await expect(page.getByRole('button')).toContainText('Log in');
 });
