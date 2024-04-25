c7c94d9766f056c1a093d21d4a8dbd4d86188ab0
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-04-25 22:40:06 +0000

    include screenshot on failure

--- e2e/playwright/playwright.config.ts
+++ e2e/playwright/playwright.config.ts
@@ -11,14 +11,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['line'],
@@ -29,8 +21,15 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: `http://localhost:${process.env.PORT || 8800}`,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /*
+      To include traces for failed tests, set this to 'retain-on-failure'.
+      This is not enabled by default because it's performance heavy.
+      See https://playwright.dev/docs/trace-viewer.
+    */
+    trace: 'off',
+
+    /* Screenshot on failure. */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
