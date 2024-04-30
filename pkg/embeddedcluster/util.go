9162cc25168207ff70b0f5ce2d81cebfd86edf2b
Andrew Lavery <laverya@umich.edu>
2024-04-30 18:39:55 +0800

    tag installation kind with 'infrastructure' tag

--- pkg/embeddedcluster/util.go
+++ pkg/embeddedcluster/util.go
@@ -137,7 +137,8 @@ func startClusterUpgrade(ctx context.Context, newcfg embeddedclusterv1beta1.Conf
 		ObjectMeta: metav1.ObjectMeta{
 			Name: time.Now().Format("20060102150405"),
 			Labels: map[string]string{
-				kotsadmtypes.BackupLabel: kotsadmtypes.BackupLabelValue,
+				kotsadmtypes.BackupLabel:        "infrastructure",
+				"kots.io/embedded-cluster-type": "installation",
 			},
 		},
 		Spec: embeddedclusterv1beta1.InstallationSpec{
