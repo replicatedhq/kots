570e92d1e6036de23328b2c6b289b299adb26018
replicated-ci-kots <91219917+replicated-ci-kots@users.noreply.github.com>
2024-05-29 12:09:12 +0000

    Update KOTS image dependency tags (#4646)
    Co-authored-by: dydx <25685+dydx@users.noreply.github.com>

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,7 +5,7 @@ package image
 // image name.
 
 const (
-	Minio      = "kotsadm/minio:0.20240510.014138-r0"
+	Minio      = "kotsadm/minio:0.20240527.191746-r0"
 	Rqlite     = "kotsadm/rqlite:8.24.7-r1"
 	Dex        = "kotsadm/dex:2.39.1-r2"
 	Schemahero = "schemahero/schemahero:0.17.8"
