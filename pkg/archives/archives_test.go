ff277f86abf2a0b28a5dcff1d540559b94040ff5
Craig O'Donnell <craig@replicated.com>
2024-06-20 18:18:06 +0000

    unit test for CreateFilteredAirgapBundle

--- pkg/archives/archives_test.go
+++ pkg/archives/archives_test.go
@@ -2,7 +2,13 @@ package archives
 
 import (
 	"encoding/base64"
+	"io"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
+
+	"github.com/replicatedhq/kots/pkg/util"
 )
 
 func TestIsTGZ(t *testing.T) {
@@ -37,6 +43,74 @@ func TestIsTGZ(t *testing.T) {
 
 			if got := IsTGZ(b); got != tt.want {
 				t.Errorf("IsTGZ() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCreateFilteredAirgapBundle(t *testing.T) {
+	tests := []struct {
+		name            string
+		bundleFiles     map[string]string
+		filesToInclude  []string
+		wantBundleFiles map[string]string
+	}{
+		{
+			name: "slim airgap bundle",
+			bundleFiles: map[string]string{
+				"airgap.yaml":                     "airgap-metadata",
+				"app.tar.gz":                      "application-archive",
+				"embedded-cluster/kotsadm.tar.gz": "kotsadm-binary",
+				"images":                          "image-data",
+			},
+			filesToInclude: []string{
+				"airgap.yaml",
+				"app.tar.gz",
+				"embedded-cluster/kotsadm.tar.gz",
+			},
+			wantBundleFiles: map[string]string{
+				"airgap.yaml":                     "airgap-metadata",
+				"app.tar.gz":                      "application-archive",
+				"embedded-cluster/kotsadm.tar.gz": "kotsadm-binary",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := util.FilesToTGZ(tt.bundleFiles)
+			if err != nil {
+				t.Errorf("failed to create tgz: %v", err)
+			}
+
+			airgapBundlePath := filepath.Join(t.TempDir(), "application.airgap")
+			tmpFile, err := os.Create(airgapBundlePath)
+			if err != nil {
+				t.Errorf("failed to create tmp file: %v", err)
+			}
+
+			_, err = tmpFile.Write(b)
+			if err != nil {
+				t.Errorf("failed to write to tmp file: %v", err)
+			}
+
+			got, err := CreateFilteredAirgapBundle(airgapBundlePath, tt.filesToInclude)
+			if err != nil {
+				t.Errorf("CreateFilteredAirgapBundle() error = %v", err)
+				return
+			}
+
+			b, err = io.ReadAll(got)
+			if err != nil {
+				t.Errorf("failed to read filtered airgap bundle: %v", err)
+			}
+
+			gotFiles, err := util.TGZToFiles(b)
+			if err != nil {
+				t.Errorf("failed to convert filtered airgap bundle to files: %v", err)
+			}
+
+			if !reflect.DeepEqual(gotFiles, tt.wantBundleFiles) {
+				t.Errorf("CreateFilteredAirgapBundle() = %v, want %v", gotFiles, tt.wantBundleFiles)
 			}
 		})
 	}
