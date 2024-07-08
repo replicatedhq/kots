2dae2bf18b640b2360f85e669228804f5d582a8e
replicated-ci-kots <91219917+replicated-ci-kots@users.noreply.github.com>
2024-07-08 14:31:14 +0000

    Update KOTS image dependency tags (#4748)
    Co-authored-by: dydx <25685+dydx@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 include Makefile.build.mk
 CURRENT_USER := $(if $(GITHUB_USER),$(GITHUB_USER),$(shell id -u -n))
-MINIO_TAG ?= 0.20240629.012047-r1
+MINIO_TAG ?= 0.20240704.142545-r0
 RQLITE_TAG ?= 8.26.6-r0
 DEX_TAG ?= 2.40.0-r2
 LVP_TAG ?= v0.6.6
--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -5,7 +5,7 @@ package image
 // image name.
 
 const (
-	Minio      = "kotsadm/minio:0.20240629.012047-r1"
+	Minio      = "kotsadm/minio:0.20240704.142545-r0"
 	Rqlite     = "kotsadm/rqlite:8.26.6-r0"
 	Dex        = "kotsadm/dex:2.40.0-r2"
 	Schemahero = "schemahero/schemahero:0.17.9"
