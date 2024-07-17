984a1bd6c8204bdd3d032a56043523d99dae812f
Florian Hines <syn@ronin.sh>
2024-07-17 16:56:54 +0000

    migration to app.channel_id column

--- migrations/build-ttl.sh
+++ migrations/build-ttl.sh
@@ -3,8 +3,9 @@ set -e
 
 CURRENT_USER=${GITHUB_USER:-$(id -u -n)}
 IMAGE=ttl.sh/${CURRENT_USER}/kotsadm-migrations:24h
+SCHEMAHERO_TAG=${SCHEMAHERO_TAG:-0.17.9}
 
-docker build -f deploy/Dockerfile -t ${IMAGE} .
+docker build --build-arg SCHEMAHERO_TAG=${SCHEMAHERO_TAG} -f deploy/Dockerfile -t ${IMAGE} .
 docker push ${IMAGE}
 
 GREEN='\033[0;32m'
