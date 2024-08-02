c12e8565aa5f35a66202cae5d5a71c27ac790710
pandemicsyn <florian@replicated.com>
2024-08-02 18:22:08 -0500

    Actually maybe invoke it

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -213,6 +213,7 @@ var _ = Describe("E2E", func() {
 			Entry(nil, inventory.NewBackupAndRestore()),
 			Entry(nil, inventory.NewNoRequiredConfig()),
 			Entry(nil, inventory.NewVersionHistoryPagination()),
+			Entry(nil, inventory.NewChangeChannel()),
 			Entry(nil, inventory.NewChangeLicense()),
 			Entry(nil, inventory.NewMinKotsVersion()),
 			Entry(nil, inventory.NewTargetKotsVersion()),
