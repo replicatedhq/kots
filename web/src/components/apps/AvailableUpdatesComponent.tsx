c9ecaf0e9839cbef64235cce9a4e28f034ad054d
Mia Wong <miawongdev@gmail.com>
2024-08-23 15:33:10 -0400

    Show available updates in Dashboard  (#4828)

--- web/src/components/apps/AvailableUpdatesComponent.tsx
+++ web/src/components/apps/AvailableUpdatesComponent.tsx
@@ -5,6 +5,80 @@ import { Utilities } from "@src/utilities/utilities";
 import { AvailableUpdate } from "@types";
 import ReactTooltip from "react-tooltip";
 
+export const AvailableUpdateRow = ({
+  update,
+  index,
+  showReleaseNotes,
+  children,
+  upgradeService,
+}: {
+  update: AvailableUpdate;
+  index: number;
+  showReleaseNotes: (releaseNotes: string) => void;
+  children: React.ReactNode;
+  upgradeService?: {
+    versionLabel?: string;
+    isLoading?: boolean;
+    error?: string;
+  } | null;
+}) => {
+  return (
+    <div key={index} className="available-update-row">
+      <div className="tw-h-10 tw-bg-white tw-p-4 tw-flex tw-justify-between tw-items-center tw-rounded">
+        <div className="flex-column">
+          <div className="flex alignItems--center">
+            <p className="u-fontSize--header2 u-fontWeight--bold u-lineHeight--medium card-item-title ">
+              {update.versionLabel}
+            </p>
+            {update.isRequired && (
+              <span className="status-tag required u-marginLeft--10">
+                {" "}
+                Required{" "}
+              </span>
+            )}
+          </div>
+          {update.upstreamReleasedAt && (
+            <p className="u-fontSize--small u-fontWeight--medium u-textColor--bodyCopy u-marginTop--5">
+              {" "}
+              Released{" "}
+              <span className="u-fontWeight--bold">
+                {Utilities.dateFormat(
+                  update.upstreamReleasedAt,
+                  "MM/DD/YY @ hh:mm a z"
+                )}
+              </span>
+            </p>
+          )}
+        </div>
+        <div className="flex alignItems--center">
+          {update?.releaseNotes && (
+            <>
+              <Icon
+                icon="release-notes"
+                size={24}
+                onClick={() => showReleaseNotes(update?.releaseNotes)}
+                data-tip="View release notes"
+                className="u-marginRight--5 clickable"
+              />
+              <ReactTooltip effect="solid" className="replicated-tooltip" />
+            </>
+          )}
+          {children}
+        </div>
+      </div>
+      {upgradeService?.error &&
+        upgradeService?.versionLabel === update.versionLabel && (
+          <div className="tw-my-4">
+            <span className="u-fontSize--small u-textColor--error u-fontWeight--bold">
+              {upgradeService.error}
+              error
+            </span>
+          </div>
+        )}
+    </div>
+  );
+};
+
 const AvailableUpdatesComponent = ({
   updates,
   showReleaseNotes,
@@ -71,78 +145,33 @@ const AvailableUpdatesComponent = ({
               upgradeService?.versionLabel === update.versionLabel &&
               upgradeService.isLoading;
             return (
-              <div key={index} className="available-update-row">
-                <div className="tw-h-10 tw-bg-white tw-p-4 tw-flex tw-justify-between tw-items-center tw-rounded">
-                  <div className="flex-column">
-                    <div className="flex alignItems--center">
-                      <p className="u-fontSize--header2 u-fontWeight--bold u-lineHeight--medium card-item-title ">
-                        {update.versionLabel}
-                      </p>
-                      {update.isRequired && (
-                        <span className="status-tag required u-marginLeft--10">
-                          {" "}
-                          Required{" "}
-                        </span>
-                      )}
-                    </div>
-                    {update.upstreamReleasedAt && (
-                      <p className="u-fontSize--small u-fontWeight--medium u-textColor--bodyCopy u-marginTop--5">
-                        {" "}
-                        Released{" "}
-                        <span className="u-fontWeight--bold">
-                          {Utilities.dateFormat(
-                            update.upstreamReleasedAt,
-                            "MM/DD/YY @ hh:mm a z"
-                          )}
-                        </span>
-                      </p>
-                    )}
-                  </div>
-                  <div className="flex alignItems--center">
-                    {update?.releaseNotes && (
-                      <>
-                        <Icon
-                          icon="release-notes"
-                          size={24}
-                          onClick={() => showReleaseNotes(update?.releaseNotes)}
-                          data-tip="View release notes"
-                          className="u-marginRight--5 clickable"
-                        />
-                        <ReactTooltip
-                          effect="solid"
-                          className="replicated-tooltip"
-                        />
-                      </>
-                    )}
-                    <button
-                      className={"btn tw-ml-2 primary blue"}
-                      onClick={() => startUpgradeService(update)}
-                      disabled={!update.isDeployable || isCurrentVersionLoading}
+              <AvailableUpdateRow
+                update={update}
+                index={index}
+                showReleaseNotes={showReleaseNotes}
+                upgradeService={upgradeService}
+              >
+                <>
+                  <button
+                    className={"btn tw-ml-2 primary blue"}
+                    onClick={() => startUpgradeService(update)}
+                    disabled={!update.isDeployable || isCurrentVersionLoading}
+                  >
+                    <span
+                      key={update.nonDeployableCause}
+                      data-tip-disable={update.isDeployable}
+                      data-tip={update.nonDeployableCause}
+                      data-for="disable-deployment-tooltip"
                     >
-                      <span
-                        key={update.nonDeployableCause}
-                        data-tip-disable={update.isDeployable}
-                        data-tip={update.nonDeployableCause}
-                        data-for="disable-deployment-tooltip"
-                      >
-                        {isCurrentVersionLoading ? "Preparing..." : "Deploy"}
-                      </span>
-                    </button>
-                    <ReactTooltip
-                      effect="solid"
-                      id="disable-deployment-tooltip"
-                    />
-                  </div>
-                </div>
-                {upgradeService?.error &&
-                  upgradeService?.versionLabel === update.versionLabel && (
-                    <div className="tw-my-4">
-                      <span className="u-fontSize--small u-textColor--error u-fontWeight--bold">
-                        {upgradeService.error}
-                      </span>
-                    </div>
-                  )}
-              </div>
+                      {isCurrentVersionLoading ? "Preparing..." : "Deploy"}
+                    </span>
+                  </button>
+                  <ReactTooltip
+                    effect="solid"
+                    id="disable-deployment-tooltip"
+                  />
+                </>
+              </AvailableUpdateRow>
             );
           })}
         </div>
