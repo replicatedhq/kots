f46fa9356fc214699e0f07b093016112ef6ceddc
Mia Wong <miawongdev@gmail.com>
2024-08-30 16:35:21 +0000

    add insecure route

--- kurl_proxy/cmd/main.go
+++ kurl_proxy/cmd/main.go
@@ -274,6 +274,32 @@ func getHttpServer(fingerprint string, acceptAnonymousUploads bool, assetsDir st
 			log.Printf("No kotsadm application metadata: %v", err) // continue
 		}
 		appIcon := template.URL(app.Spec.Icon)
+		c.HTML(http.StatusOK, "welcome.html", gin.H{
+			"fingerprintSHA1": fingerprint,
+			"AppIcon":         appIcon,
+			"AppTitle":        app.Spec.Title,
+		})
+	})
+	r.GET("/insecure", func(c *gin.Context) {
+		if !acceptAnonymousUploads {
+			log.Println("TLS certs already uploaded, redirecting to https")
+			target := url.URL{
+				Scheme:   "https",
+				Host:     c.Request.Host,
+				Path:     c.Request.URL.Path,
+				RawQuery: c.Request.URL.RawQuery,
+			}
+			// Returns StatusFound (302) to avoid browser caching
+			c.Redirect(http.StatusFound, target.String())
+			return
+		}
+
+		app, err := kotsadmApplication()
+
+		if err != nil {
+			log.Printf("No kotsadm application metadata: %v", err) // continue
+		}
+		appIcon := template.URL(app.Spec.Icon)
 		c.HTML(http.StatusOK, "insecure.html", gin.H{
 			"fingerprintSHA1": fingerprint,
 			"AppIcon":         appIcon,
