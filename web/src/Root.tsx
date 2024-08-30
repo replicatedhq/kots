b5fcd3b754b23b46d7a3a813ae0753115321070a
Mia Wong <miawongdev@gmail.com>
2024-08-30 14:52:39 -0400

    Show join command during restore in Embedded Cluster (#4826)

--- web/src/Root.tsx
+++ web/src/Root.tsx
@@ -501,6 +501,9 @@ const Root = () => {
             isEmbeddedClusterEnabled={Boolean(
               state.adminConsoleMetadata?.isEmbeddedCluster
             )}
+            isEmbeddedClusterWaitingForNodes={
+              state.isEmbeddedClusterWaitingForNodes
+            }
             isGitOpsSupported={isGitOpsSupported()}
             isIdentityServiceSupported={isIdentityServiceSupported()}
             appsList={state.appsList}
@@ -636,7 +639,11 @@ const Root = () => {
                     state.adminConsoleMetadata?.isKurl ? (
                       <KurlClusterManagement />
                     ) : (
-                      <EmbeddedClusterManagement />
+                      <EmbeddedClusterManagement
+                        isEmbeddedClusterWaitingForNodes={
+                          state.isEmbeddedClusterWaitingForNodes
+                        }
+                      />
                     )
                   }
                 />
