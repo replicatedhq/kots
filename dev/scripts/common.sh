2b804a752415e781bbac57a5d0b46bfceb794602
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-09-12 13:11:10 -0700

    Move Dev Environment to MacOS (#4879)
    * Move Dev Environment to MacOS

---
+++ dev/scripts/common.sh
@@ -0,0 +1,127 @@
+# Get component image name
+function image() {
+  jq -r ".\"$1\".image" dev/metadata.json
+}
+
+# Get component dockerfile path
+function dockerfile() {
+  jq -r ".\"$1\".dockerfile" dev/metadata.json
+}
+
+# Get component dockercontext
+function dockercontext() {
+  jq -r ".\"$1\".dockercontext" dev/metadata.json
+}
+
+# Get component deployment name
+function deployment() {
+  jq -r ".\"$1\".deployment" dev/metadata.json
+}
+
+# Populates local caches and binaries for faster (re)builds
+function populate() {
+  case "$1" in
+    "kotsadm")
+      docker run --rm \
+        -v "$(pwd):/replicatedhq/kots" \
+        -e GOCACHE=/replicatedhq/kots/dev/.gocache \
+        -e GOMODCACHE=/replicatedhq/kots/dev/.gomodcache \
+        -w /replicatedhq/kots \
+        golang:1.23-alpine \
+        /bin/sh -c "apk add make bash git && make kots build"
+      ;;
+    "kotsadm-web")
+      docker run --rm \
+        -v "$(pwd):/replicatedhq/kots" \
+        -w /replicatedhq/kots/web \
+        node:18-alpine \
+        /bin/sh -c "apk add make bash git && make deps"
+      ;;
+    "kurl-proxy")
+      docker run --rm \
+        -v "$(pwd):/replicatedhq/kots" \
+        -e GOCACHE=/replicatedhq/kots/dev/.gocache \
+        -e GOMODCACHE=/replicatedhq/kots/dev/.gomodcache \
+        -w /replicatedhq/kots/kurl_proxy \
+        golang:1.23-alpine \
+        /bin/sh -c "apk add make bash git && make build"
+      ;;
+  esac
+}
+
+# Builds a component's image
+function build() {
+  docker build -t $(image $1) -f $(dockerfile $1) $(dockercontext $1)
+}
+
+# Restarts a component
+function restart() {
+  if [ "$1" == "kotsadm-migrations" ]; then
+    kubectl delete job $1 --ignore-not-found
+  elif kubectl get deployment $(deployment $1) &>/dev/null; then
+    kubectl rollout restart deployment/$(deployment $1)
+  fi
+}
+
+# The /host_mnt directory on Docker Desktop for macOS is a virtualized path that represents
+# the mounted directories from the macOS host filesystem into the Docker Desktop VM.
+# This is required for using HostPath volumes in Kubernetes.
+function render() {
+  sed "s|__PROJECT_DIR__|/host_mnt$(pwd)|g" "$1"
+}
+
+# The embedded-cluster container mounts the KOTS project at /replicatedhq/kots
+function ec_render() {
+  sed "s|__PROJECT_DIR__|/replicatedhq/kots|g" "$1"
+}
+
+# Get the embedded cluster node name
+function ec_node() {
+  echo "${EC_NODE:-node0}"
+}
+
+# Executes a command in the embedded cluster container
+function ec_exec() {
+  docker exec -it -w /replicatedhq/kots $(ec_node) $@
+}
+
+# Patches a component deployment in the embedded cluster
+function ec_patch() {
+  ec_render dev/patches/$1-up.yaml > dev/patches/$1-up-ec.yaml.tmp
+  ec_exec k0s kubectl patch deployment $(deployment $1) -n kotsadm --patch-file dev/patches/$1-up-ec.yaml.tmp
+  rm dev/patches/$1-up-ec.yaml.tmp
+}
+
+function ec_build_and_load() {
+  # Build the image
+  if docker images | grep -q "$(image $1)"; then
+    echo "$(image $1) image already exists, skipping build..."
+  else
+    echo "Building $1..."
+    populate $1
+    build $1
+  fi
+
+  # Load the image into the embedded cluster
+  if docker exec $(ec_node) k0s ctr images ls | grep -q "$(image $1)"; then
+    echo "$(image $1) image already loaded in embedded cluster, skipping import..."
+  else
+    echo "Loading "$(image $1)" image into embedded cluster..."
+    docker save "$(image $1)" | docker exec -i $(ec_node) k0s ctr images import -
+  fi
+}
+
+function up() {
+  if [ "$1" == "kotsadm-web" ]; then
+    # Tail the logs of the new pod
+    newpod=$(kubectl get pods -l app=kotsadm-web --no-headers --sort-by=.metadata.creationTimestamp | awk 'END {print $1}')
+    kubectl logs -f $newpod --tail=100
+  else
+    # Exec into the deployment
+    kubectl exec -it deployment/$(deployment $1) -- bash
+  fi
+}
+
+function ec_up() {
+  ec_exec k0s kubectl exec -it deployment/$(deployment $1) -n kotsadm -- bash
+}
---
+++ dev/scripts/up-ec.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+. dev/scripts/common.sh
+
+component=$1
+
+# Check if a component name was provided
+if [ -z "$component" ]; then
+	echo "Error: No component name provided."
+	exit 1
+fi
+
+# Check if already up
+if [ -f "dev/patches/$component-down-ec.yaml.tmp" ]; then
+  ec_up $component
+  exit 0
+fi
+
+# Build and load the image into the embedded cluster
+ec_build_and_load "$component"
+
+# The kotsadm dev image does not have a web component, and kotsadm-web service does not exist in embedded cluster.
+# Deploy kotsadm-web service instead of shipping a web component in the kotsadm dev image so
+# we can achieve a faster dev experience with hot reloading.
+if [ "$component" == "kotsadm" ]; then
+  ec_build_and_load "kotsadm-web"
+  ec_exec k0s kubectl apply -f dev/manifests/kotsadm-web -n kotsadm
+  ec_patch "kotsadm-web"
+fi
+
+# Save current deployment state
+ec_exec k0s kubectl get deployment $(deployment $component) -n kotsadm -oyaml > dev/patches/$component-down-ec.yaml.tmp
+
+# Patch the deployment
+ec_patch $component
+
+# Wait for rollout to complete
+ec_exec k0s kubectl rollout status deployment/$(deployment $component) -n kotsadm
+
+# Up into the updated deployment
+ec_up $component
