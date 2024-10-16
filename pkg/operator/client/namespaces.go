e96074ea12a4acf77a048b46cb6adc64f95ed57b
divolgin <dmitriy@replicated.com>
2024-10-16 11:36:14 -0700

    Copy custom CA config map to additional namespaces

--- pkg/operator/client/namespaces.go
+++ pkg/operator/client/namespaces.go
@@ -45,6 +45,11 @@ func (c *Client) runNamespacesInformer() error {
 					log.Printf("error ensuring image pull secrets for namespace %s: %s", addedNamespace.Name, err.Error())
 				}
 
+				if err := c.ensureEmbeddedClusterCAPresent(addedNamespace.Name); err != nil {
+					// we don't fail here...
+					log.Printf("error ensuring cluster ca present for namespace %s: %s", addedNamespace.Name, err.Error())
+				}
+
 				c.ApplyHooksInformer([]string{addedNamespace.Name})
 			}
 		},
