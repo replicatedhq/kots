0589dd2ff354b3d2c0a688663abafc54b397476f
Ethan Mosbaugh <ethan@replicated.com>
2024-10-29 17:26:31 -0500

    feat(ec): allow rollbacks for embedded cluster (#4972)
    * feat(ec): allow rollbacks for embedded cluster
    
    * f
    
    * f
    
    * f
    
    * f
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * feedback
    
    * f
    
    * assert interface

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -1,38 +1,27 @@
 package store
 
 import (
-	"github.com/replicatedhq/kots/pkg/store/kotsstore"
 	"github.com/replicatedhq/kots/pkg/util"
 )
 
 var (
-	hasStore    = false
 	globalStore Store
 )
-
-var _ Store = (*kotsstore.KOTSStore)(nil)
 
 func GetStore() Store {
 	if util.IsUpgradeService() {
 		panic("store cannot not be used in the upgrade service")
 	}
-	if !hasStore {
-		globalStore = storeFromEnv()
-		hasStore = true
+	if globalStore == nil {
+		panic("store not initialized")
 	}
 	return globalStore
 }
 
-func storeFromEnv() Store {
-	return kotsstore.StoreFromEnv()
-}
-
 func SetStore(s Store) {
 	if s == nil {
-		hasStore = false
 		globalStore = nil
 		return
 	}
-	hasStore = true
 	globalStore = s
 }
