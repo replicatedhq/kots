b8cd4db4cbcb64379e2176a08e33ebde10ac5b76
dydx <25685+dydx@users.noreply.github.com>
2024-11-05 04:07:11 +0000

    Update KOTS image dependency tags

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -6,7 +6,7 @@ package image
 
 const (
 	Minio      = "kotsadm/minio:0.20241029.160148-r0"
-	Rqlite     = "kotsadm/rqlite:8.32.5-r0"
+	Rqlite     = "kotsadm/rqlite:8.32.6-r0"
 	Dex        = "kotsadm/dex:2.41.1-r1"
 	Schemahero = "schemahero/schemahero:0.17.10"
 	Lvp        = "replicated/local-volume-provider:v0.6.7"
