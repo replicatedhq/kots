7460124a93880f033e49549ab0da318d8bdf6798
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-03-01 16:12:13 -0800

    base url

--- e2e/playwright/playwright.config.ts
+++ e2e/playwright/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://localhost:${process.env.PORT || 8800}`,
+    baseURL: process.env.BASE_URL || `http://localhost:${process.env.PORT || 8800}`,
 
     /*
       To include traces for failed tests, set this to 'retain-on-failure'.
