b3a5e64f8a39243cb237fe07c82204896ecf1860
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-03-06 09:10:18 -0800

    fail quickly

--- e2e/playwright/tests/@target-kots-version-airgap/test.spec.ts
+++ e2e/playwright/tests/@target-kots-version-airgap/test.spec.ts
@@ -28,7 +28,7 @@ const validateAirgapInstallRestrictive = async (page: Page, expect: Expect) => {
     '/tmp/app.airgap'
   );
 
-  await airgapInstall(page, expect, 'ttl.sh', 'admin', 'password', 'test', '/tmp/app.airgap', 1 * 60 * 1000); // 1 minute
+  await airgapInstall(page, expect, 'ttl.sh', 'admin', 'password', 'test', '/tmp/app.airgap', 15 * 1000); // 15 seconds (should fail quickly)
 
   const errorMessage = airgapInstallErrorMessage(page);
   await expect(errorMessage).toContainText("requires");
