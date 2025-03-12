e468f7f2f827ff64f085689f51e8c84e3fa2f1c5
Salah Al Saleh <sg.alsaleh@gmail.com>
2025-03-11 18:02:36 -0700

    Move existing-online-upgrade tests to Playwright (#5194)

---
+++ e2e/playwright/regression/@existing-online-upgrade-minimal/constants.ts
@@ -0,0 +1,16 @@
+export const NAMESPACE = "qakotsregression";
+export const VENDOR_INITIAL_CHANNEL_SEQUENCE = 74;
+export const VENDOR_UPDATE_CHANNEL_SEQUENCE = 75;
+export const LICENSE_ID = "1tMWrUR7pLWGrkkCZH836acYzTT";
+export const CUSTOMER_ID = "1tMWrYwxYdsx1J3JALhLQLFHJog";
+export const CUSTOMER_NAME = "type=existing cluster, env=online, phase=upgraded install, rbac=minimal rbac";
+export const CHANNEL_NAME = "type=existing cluster, env=online, phase=upgraded install, rbac=minimal rbac";
+export const CHANNEL_ID = "1tMWX0EwStDm7mJlVYL1FpWZJzh";
+export const CHANNEL_SLUG = "type-existing-cluster-env-on-3";
+export const VELERO_VERSION = "v1.12.1";
+export const VELERO_AWS_PLUGIN_VERSION = "v1.8.1";
+export const IS_EC = false;
+export const IS_EXISTING_CLUSTER = true;
+export const IS_AIRGAPPED = false;
+export const IS_AIRGAP_SUPPORTED = false;
+export const IS_MINIMAL_RBAC = true;
