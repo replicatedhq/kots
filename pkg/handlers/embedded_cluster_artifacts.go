f00fe629f85b68fdbc0b30b6193c9b31cf0f7f3c
Salah Al Saleh <sg.alsaleh@gmail.com>
2025-04-25 10:03:23 -0700

    Add endpoint that returns the EC infra images from the k0s directory (#5293)
    * Add endpoint that returns the EC infra images from the k0s directory

--- pkg/handlers/embedded_cluster_artifacts.go
+++ pkg/handlers/embedded_cluster_artifacts.go
@@ -23,6 +23,14 @@ func (h *Handler) GetEmbeddedClusterBinary(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	// Get data directory path from env var
+	dataDir := util.EmbeddedClusterDataDir()
+	if dataDir == "" {
+		logger.Error(errors.New("environment variable EMBEDDED_CLUSTER_DATA_DIR not set"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	// Get kubeclient
 	kbClient, err := h.GetKubeClient(r.Context())
 	if err != nil {
@@ -35,17 +43,6 @@ func (h *Handler) GetEmbeddedClusterBinary(w http.ResponseWriter, r *http.Reques
 	installation, err := embeddedcluster.GetCurrentInstallation(r.Context(), kbClient)
 	if err != nil {
 		logger.Error(errors.Wrap(err, "failed to get current installation"))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Get data directory path from runtime config
-	dataDir := ""
-	if installation.Spec.RuntimeConfig != nil {
-		dataDir = installation.Spec.RuntimeConfig.DataDir
-	}
-	if dataDir == "" {
-		logger.Error(errors.New("data directory not found in runtime config"))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -111,6 +108,77 @@ func (h *Handler) GetEmbeddedClusterBinary(w http.ResponseWriter, r *http.Reques
 	// Copy binary content to tar archive
 	if _, err := io.Copy(tarWriter, binaryFile); err != nil {
 		logger.Error(errors.Wrap(err, "failed to write binary to tar archive"))
+		return
+	}
+}
+
+// GetEmbeddedClusterInfraImages returns the infrastructure images as a .tgz file
+func (h *Handler) GetEmbeddedClusterInfraImages(w http.ResponseWriter, r *http.Request) {
+	if !util.IsEmbeddedCluster() {
+		logger.Error(errors.New("not an embedded cluster"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Get k0s directory path from env var
+	k0sDir := util.EmbeddedClusterK0sDir()
+	if k0sDir == "" {
+		logger.Error(errors.New("environment variable EMBEDDED_CLUSTER_K0S_DIR not set"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Path to images directory
+	imagesDir := filepath.Join(k0sDir, "images")
+
+	// Note: The directory can contain different names:
+	// - images-amd64.tar: written on initial installation
+	// - ec-images-amd64.tar: written on upgrades (takes precedence)
+	// TODO: consolidate the two names to avoid confusion
+	imagesPaths := []string{
+		filepath.Join(imagesDir, "ec-images-amd64.tar"),
+		filepath.Join(imagesDir, "images-amd64.tar"),
+	}
+
+	var imagesPath string
+	for _, path := range imagesPaths {
+		if _, err := os.Stat(path); err == nil {
+			imagesPath = path
+			break
+		}
+	}
+
+	if imagesPath == "" {
+		logger.Error(errors.New("no infrastructure image files found"))
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// Open images file
+	imagesFile, err := os.Open(imagesPath)
+	if err != nil {
+		logger.Error(errors.Wrap(err, "failed to open images file"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer imagesFile.Close()
+
+	// Get file information
+	fileInfo, err := imagesFile.Stat()
+	if err != nil {
+		logger.Error(errors.Wrap(err, "failed to get file info"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set response headers
+	w.Header().Set("Content-Disposition", "attachment; filename=ec-images-amd64.tar")
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
+
+	// Copy file content to response
+	if _, err := io.Copy(w, imagesFile); err != nil {
+		logger.Error(errors.Wrap(err, "failed to write images file to response"))
 		return
 	}
 }
