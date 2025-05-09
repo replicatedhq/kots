32395f54f1f6c34846988ce4ea84db07c882286b
João Antunes <joao@replicated.com>
2025-05-09 10:59:55 +0100

    Update pkg/upgradeservice/bootstrap.go
    Co-authored-by: Alex Parker <7272359+ajp-io@users.noreply.github.com>

--- pkg/upgradeservice/bootstrap.go
+++ pkg/upgradeservice/bootstrap.go
@@ -61,7 +61,7 @@ func updateWithinKubeRange(params types.UpgradeServiceParams) error {
 		return errors.Errorf("major version mismatch: current %s, update %s", currentVersion, updateVersion)
 	}
 	if currentVersion.GreaterThan(updateVersion) {
-		return errors.Errorf("cannot downgrade the version: current %s, update %s", currentVersion, updateVersion)
+		return errors.Errorf("cannot downgrade the kubernetes version: current %s, update %s", currentVersion, updateVersion)
 	}
 	if updateVersion.Minor() > currentVersion.Minor()+1 {
 		return errors.Errorf("cannot update more than one minor version: current %s, update %s", currentVersion, updateVersion)
