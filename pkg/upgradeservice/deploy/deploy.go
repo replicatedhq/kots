82508e12bdcfc66834c7e37aa2a9ef674077148f
Salah Al Saleh <sg.alsaleh@gmail.com>
2025-05-12 14:15:23 -0700

    Surface embedded cluster upgrade failures on panics (#5321)

--- pkg/upgradeservice/deploy/deploy.go
+++ pkg/upgradeservice/deploy/deploy.go
@@ -119,6 +119,9 @@ func Deploy(opts DeployOptions) error {
 
 	go func() (finalError error) {
 		defer func() {
+			if r := recover(); r != nil {
+				finalError = fmt.Errorf("recovered from panic during cluster upgrade: %v", r)
+			}
 			if finalError != nil {
 				logger.Error(errors.Wrap(finalError, "failed to deploy"))
 				if err := task.SetStatusUpgradeFailed(opts.Params.AppSlug, finalError.Error()); err != nil {
