d1cd96012e643a76eef962c61bc6a9b87601fc1a
Ethan Mosbaugh <ethan@replicated.com>
2025-07-07 09:39:09 -0700

    f

--- e2e/playwright/tests/@config-validation/test.spec.ts
+++ e2e/playwright/tests/@config-validation/test.spec.ts
@@ -68,7 +68,7 @@ test('config validation', async ({ page }) => {
   await expect(page.getByText('A valid JWT file must be in the format header.payload.signature.')).not.toBeVisible();
   await expect(page.getByText('Error detected. Please use config nav to the left to locate and fix issues.')).not.toBeVisible();
   await page.getByRole('button', { name: 'Save config' }).click();
-  await expect(page.locator('.Modal-body')).toContainText('The config for Config Validation has been updated.', { timeout: 10000 });
+  await expect(page.locator('.Modal-body')).toContainText('The config for Config Validation has been updated.', { timeout: 30000 });
 
   // validate the cli
   var invalidTestInput = [
