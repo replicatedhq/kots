fd9414d7d5f7986f8a4ac7adf2376c7af7368d20
Ethan Mosbaugh <ethan@replicated.com>
2025-08-05 12:01:31 -0700

    f

---
+++ web/eslint.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  }
+);
