ee90c719f01b5a42b7b9d7b264148f251cf06cc4
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-09-10 10:43:53 -0700

    Include channel ID and sequence in get versions command

--- pkg/print/versions.go
+++ pkg/print/versions.go
@@ -7,12 +7,14 @@ import (
 )
 
 type AppVersionResponse struct {
-	VersionLabel string     `json:"versionLabel"`
-	Sequence     int64      `json:"sequence"`
-	CreatedOn    time.Time  `json:"createdOn"`
-	Status       string     `json:"status"`
-	DeployedAt   *time.Time `json:"deployedAt"`
-	Source       string     `json:"source"`
+	VersionLabel    string     `json:"versionLabel"`
+	Sequence        int64      `json:"sequence"`
+	CreatedOn       time.Time  `json:"createdOn"`
+	Status          string     `json:"status"`
+	DeployedAt      *time.Time `json:"deployedAt"`
+	Source          string     `json:"source"`
+	ChannelID       string     `json:"channelId"`
+	ChannelSequence int64      `json:"channelSequence"`
 }
 
 func Versions(versions []AppVersionResponse, format string) {
@@ -34,8 +36,8 @@ func printVersionsTable(versions []AppVersionResponse) {
 	defer w.Flush()
 
 	fmtColumns := "%s\t%v\t%s\t%s\n"
-	fmt.Fprintf(w, fmtColumns, "VERSION", "SEQUENCE", "STATUS", "SOURCE")
+	fmt.Fprintf(w, fmtColumns, "VERSION", "SEQUENCE", "STATUS", "SOURCE", "CHANNEL ID", "CHANNEL SEQUENCE")
 	for _, version := range versions {
-		fmt.Fprintf(w, fmtColumns, version.VersionLabel, version.Sequence, version.Status, version.Source)
+		fmt.Fprintf(w, fmtColumns, version.VersionLabel, version.Sequence, version.Status, version.Source, version.ChannelID, version.ChannelSequence)
 	}
 }
