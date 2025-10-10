8f165e7f049f3709d1c6542e4794e673ca0e0e6a
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2025-10-10 09:04:12 -0700

    EC: do not process airgap bundle as part of the deploy command

--- cmd/kots/cli/deploy.go
+++ cmd/kots/cli/deploy.go
@@ -20,7 +20,6 @@ import (
 	"github.com/replicatedhq/kots/pkg/upstream"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"k8s.io/client-go/kubernetes"
 )
 
 func DeployCmd() *cobra.Command {
@@ -112,19 +111,12 @@ func DeployCmd() *cobra.Command {
 				return errors.Wrap(err, "failed to sync license")
 			}
 
-			// Step 2: If airgap bundle, push images first
-			if airgapBundle != "" {
-				if err := handleAirgapImagePush(v, clientset, appSlug, log); err != nil {
-					return errors.Wrap(err, "failed to push airgap images")
-				}
-			}
-
-			// Step 3: Upstream Update (both online and airgap)
+			// Step 2: Upstream Update (both online and airgap)
 			if err := handleUpstreamUpdate(v, appSlug, localPort, authSlug, log); err != nil {
 				return errors.Wrap(err, "failed to process upstream update")
 			}
 
-			// Step 4: Set Config + Deploy
+			// Step 3: Set Config + Deploy
 			if err := handleSetConfigAndDeploy(v, appSlug, localPort, authSlug, log); err != nil {
 				return errors.Wrap(err, "failed to set config and deploy")
 			}
@@ -287,25 +279,6 @@ func handleUpstreamUpdate(v *viper.Viper, appSlug string, localPort int, authSlu
 
 	log.ActionWithoutSpinner("Upstream update processed successfully")
 
-	return nil
-}
-
-func handleAirgapImagePush(v *viper.Viper, clientset kubernetes.Interface, appSlug string, log *logger.CLILogger) error {
-	log.ActionWithoutSpinner("Pushing airgap images...")
-
-	airgapBundle := v.GetString("airgap-bundle")
-
-	registryConfig, err := getRegistryConfig(v, clientset, appSlug)
-	if err != nil {
-		return errors.Wrap(err, "failed to get registry config")
-	}
-
-	err = upstream.PushImagesFromAirgapBundle(airgapBundle, *registryConfig)
-	if err != nil {
-		return err
-	}
-
-	log.ActionWithoutSpinner("Images pushed successfully")
 	return nil
 }
 
