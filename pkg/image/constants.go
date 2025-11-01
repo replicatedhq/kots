a9de1043b34089cf7e4c40492f2bd3ff54dce7c6
replicated-ci-kots <91219917+replicated-ci-kots@users.noreply.github.com>
2025-11-01 06:10:21 +0000

    Update KOTS image dependency tags (#5672)
    Co-authored-by: dydx <25685+dydx@users.noreply.github.com>

--- pkg/image/constants.go
+++ pkg/image/constants.go
@@ -6,7 +6,7 @@ package image
 
 const (
 	Minio      = "docker.io/kotsadm/minio:0.20251015.172955-r0"
-	Rqlite     = "docker.io/kotsadm/rqlite:9.1.3-r0"
+	Rqlite     = "docker.io/kotsadm/rqlite:9.2.0-r0"
 	Dex        = "docker.io/kotsadm/dex:2.44.0-r3"
 	Schemahero = "docker.io/schemahero/schemahero:0.22.1"
 	Lvp        = "docker.io/replicated/local-volume-provider:0.6.10"
