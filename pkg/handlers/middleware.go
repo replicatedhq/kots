960f83a377d729e9cf4656dfb2d723f6469c1257
Xav Paice <xav@replicated.com>
2026-06-09 16:42:43 +1200

    add X-Content-Type-Options header

--- pkg/handlers/middleware.go
+++ pkg/handlers/middleware.go
@@ -108,3 +108,13 @@ func RequireValidSessionQuietMiddleware(kotsStore store.Store) mux.MiddlewareFun
 		})
 	}
 }
+
+func SecurityHeadersMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		next.ServeHTTP(w, r)
+	})
+}
